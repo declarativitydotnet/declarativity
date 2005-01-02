7eeeb9696bc394f64d2eee6ab0e1bc81630ea515
Timothy Roscoe <timothyroscoe>
2005-01-02 23:43:20 +0000

    First-cut networking code

---
+++ trunk/net/udp.C
@@ -0,0 +1,145 @@
+// -*- c-basic-offset: 2; related-file-name: "element.C" -*-
+/*
+ * @(#)$Id$
+ * 
+ * This file is distributed under the terms in the attached INTEL-LICENSE file.
+ * If you do not find these files, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ * 
+ * DESCRIPTION: Element-pair for a UDP socket
+ */
+
+#include "udp.h"
+#include "udpsuio.h"
+#include "tuple.h"
+#include <async.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+/////////////////////////////////////////////////////////////////////
+//
+// Receive element
+//
+Udp::Rx::Rx(Udp &udp) 
+  : u(&udp), push_pending(true) 
+{
+  socket_on();
+}
+
+//
+// Socket callback for receive element
+//
+void Udp::Rx::socket_cb()
+{
+  // If push is not enabled, turn off the callback and return. 
+  if (!push_pending) {
+    socket_off();
+    return;
+  }
+
+  // Read packet. 
+  UdpSuio *uio = New UdpSuio();
+  struct sockaddr sa;
+  socklen_t sa_len;
+  if ( uio->inputfrom(u->sd, &sa, &sa_len) <= 0) {
+    // Error! 
+    if (errno != EAGAIN) {
+      // Make an error tuple... 
+      TupleRef t = Tuple::mk();
+      t->append(New refcounted<TupleField>(errno));
+      push(1,t,cbv_null);
+    }
+    delete uio;
+  } else {
+    // Success! We've got a packet.  Package it up...
+    TupleRef t = Tuple::mk();
+    t->append(New refcounted<TupleField>(str(*uio)));
+    t->append(New refcounted<TupleField>(str( (const char *)&sa, sa_len)));
+    // Push it. 
+    push_pending = push(0,t,wrap(this,&Udp::Rx::element_cb));
+  }
+  socket_on();
+}
+
+//
+// Element callback: called when push is re-enabled. 
+//
+void Udp::Rx::element_cb()
+{
+  push_pending = true;
+  socket_on();
+}
+
+
+/////////////////////////////////////////////////////////////////////
+//
+// Transmit element
+//
+Udp::Tx::Tx(Udp &udp) 
+  : u(&udp), pull_pending(true) 
+{
+  socket_on();
+}
+
+//
+// Socket callback for receive element
+//
+void Udp::Tx::socket_cb()
+{
+  // If pull is not enabled, turn off the callback and return. 
+  if (!pull_pending) {
+    socket_off();
+    return;
+  }
+
+  // Try to pull a packet. 
+  TuplePtr t = input(0)->pull(wrap(this,&Udp::Tx::element_cb));
+  if (!t) {
+    pull_pending = false;
+    socket_off();
+    return;
+  }
+  
+  // We've now go a packet...
+  str payload = (*t)[0]->as_s();
+  str addr = (*t)[1]->as_s();
+  ssize_t s = sendto(u->sd, 
+		     payload.cstr(), payload.len(),
+		     0, 
+		     (struct sockaddr *)(addr.cstr()), addr.len());
+  // 's' is signed, whereas the payload.len() isn't. Hence the following:
+  if (s <= 0 || (unsigned)s <= payload.len() ) {
+    // Error!  Technically, this can happen if the payload is larger
+    //  than the socket buffer (in which case errno=EAGAIN).  We treat
+    //  this as an error, nevertheless, and leave it up to the
+    //  segmentation and reassembly elements upstream to not make us
+    //  send anything bigger than the MTU, which should fit into the
+    //  socket buffers. 
+    // Make an error tuple... 
+    TupleRef t = Tuple::mk();
+    t->append(New refcounted<TupleField>(errno));
+    push(1,t,cbv_null);
+  }
+  socket_on();
+}
+
+//
+// Element callback: called when push is re-enabled. 
+//
+void Udp::Tx::element_cb()
+{
+  pull_pending = true;
+  socket_on();
+}
+
+////////////////////////////////////////////////////////////////////
+//
+// The main object itself
+//
+Udp::Udp(u_int16_t port, u_int32_t addr) 
+  : rx(*this), tx(*this)
+{
+  sd = inetsocket(SOCK_DGRAM, port, addr);
+  make_async(sd);
+}
---
+++ trunk/net/udp.h
@@ -0,0 +1,82 @@
+// -*- c-basic-offset: 2; related-file-name: "element.C" -*-
+/*
+ * @(#)$Id$
+ * 
+ * This file is distributed under the terms in the attached INTEL-LICENSE file.
+ * If you do not find these files, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ * 
+ * DESCRIPTION: Element-pair for a UDP socket
+ */
+
+#ifndef __UDP_H__
+#define __UDP_H__
+
+#include "element.h"
+
+
+class Udp { 
+public:
+
+  // 
+  // First, the Rx element: pushes tuples when packets arrive
+  //
+  class Rx : public Element {
+  public:
+    Rx(Udp &udp);
+    const char *class_name() const		{ return "Udp::Rx";};
+    const char *processing() const		{ return "/hh"; };
+    const char *flow_code() const		{ return "/-"; };
+  private:
+    void socket_cb(); // Callback for socket activity
+    void element_cb(); // Callback for element push 
+
+    void socket_on() { fdcb(u->sd, selread, wrap(this,&Udp::Rx::socket_cb)); };
+    void socket_off() { fdcb(u->sd, selread, NULL); };
+    
+    const Udp *u;
+    bool push_pending;  // == push enabled (return value of last push)
+    
+  };
+  
+  // 
+  // Second, the Tx element: pulls tuples when the socket can send
+  //
+  class Tx : public Element {
+  public:
+    Tx(Udp &udp);
+    const char *class_name() const		{ return "Udp::Tx";};
+    const char *processing() const		{ return "l/h"; };
+    const char *flow_code() const		{ return "-/"; };
+  private:
+    void socket_cb(); // Callback for socket activity
+    void element_cb(); // Callback for element pull
+
+    void socket_on() { fdcb(u->sd, selwrite, wrap(this,&Udp::Tx::socket_cb));};
+    void socket_off() { fdcb(u->sd, selwrite, NULL); };
+    
+    const Udp *u;
+    bool pull_pending;  // == pull enabled (there is a pull to do)
+  };
+  
+  //
+  // Now the Udp object itself.
+  //
+  Udp(u_int16_t port=0, u_int32_t addr = INADDR_ANY);
+
+  // Accessing the individual elements
+  Udp::Rx *get_rx() { return &rx; };
+  Udp::Tx *get_tx() { return &tx; };
+
+private:
+  // Socket
+  int sd; 
+
+  // Elements 
+  Rx	rx;
+  Tx	tx;
+};
+
+
+#endif /* __UDP_H_ */
