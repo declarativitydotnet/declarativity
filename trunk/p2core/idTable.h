92894e8f4d094139cb4ee78ccf115c2fae7d25c4
vik <vik>
2005-01-11 11:42:51 +0000

    1/10/04

--- trunk/p2core/idTable.h
+++ trunk/p2core/idTable.h
@@ -7,10 +7,10 @@
 
 #include <string>
 #include <bitset>
-#include <functional>
-#include <hash_set.h>
+#include <map>
+#include <utility>
+
 #include <assert.h>
-#include <stdint.h>
 
 
 class IdTable;
@@ -26,21 +26,21 @@ class Id /* : private virtual refcounted */ {
   friend class IdTable;
   private:
     Id (IdTable *);
-    Id (const uint32_t * num, IdTable *);
+    Id (const u_int32_t * num, IdTable *);
     Id (const std::string&, IdTable *);
 
-    static std::bitset<160> toBitWord (uint32_t num);
-    static std::string toHexWord (uint32_t num);
+    static std::bitset<160> toBitWord (u_int32_t num);
+    static std::string toHexWord (u_int32_t num);
 
-    // key represented as 5 @uint32_t words
-    uint32_t key[5];
+    // key represented as 5 @u_int32_t words
+    u_int32_t key[5];
 
   public:
     IdTable * table;
     std::bitset<160> toBitSet () const;
     std::string toHexString () const;
     inline size_t hashCode () const;
-    inline uint32_t getWord (const int index) const;
+    inline u_int32_t getWord (const int index) const;
     
     // static Id * unmarshall (const XDR *);
 };
@@ -50,19 +50,18 @@ class Id /* : private virtual refcounted */ {
  *  @DESCRIPTION: Represents a function object to compute the hash
  *                code for an @Id object; used in @IdTable
  *  -------------------------------------------------------------- */
-struct IdHashCode : public unary_function<const Id *, size_t> {
-  size_t operator () (const Id * idKey) const {
-    return idKey->hashCode ();
-  }
-};
+//struct IdHashCode : public unary_function<const Id *, size_t> {
+  //size_t operator () (const Id * idKey) const {
+    //return idKey->hashCode ();
+  //}
+//};
 
 /** --------------------------------------------------------------
  *  @STRUCT: IdComparator
  *  @DESCRIPTION: Represents a function object to compare the
  *                equality of two @Id objects
  *  -------------------------------------------------------------- */
-struct IdComparator : 
-  public binary_function<const Id *, const Id *, bool> {
+struct IdComparator {
   bool operator () (const Id * x, const Id * y) const {
     for (int i = 0; i < 5; i += 1) {
       if (x->getWord(i) == y->getWord(i))
@@ -93,7 +92,7 @@ class IdTable {
     // the ref<Id> to the existing Id
     const Id * create (const std::string&);
 
-    const Id * create (const uint32_t *);
+    const Id * create (const u_int32_t *);
 
     // Create a random @Id using /dev/urandom
     const Id * create ();
@@ -106,7 +105,7 @@ class IdTable {
 
   private:
 
-    hash_set<const Id *, IdHashCode, IdComparator> idSet;
+    std::map<const Id *, int, IdComparator> idSet;
 
     size_t maxSize;
 
