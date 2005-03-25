279741476e23abb6e88dc5b20267eb4869788b0e
Petros Maniatis <maniatis>
2005-03-25 09:26:25 +0000

    Added

---
+++ trunk/tests/scripts/buildCDF.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl 
+# Create a Chord network exponentially, with degree d.  First gateway,
+# then d nodes off of that, then d^2 nodes off of those, etc.
+
+# Once it is all set up, start running random lookups from random nodes
+
+use strict 'vars';
+use Getopt::Std;
+use FindBin;
+
+our($opt_f);
+
+if ($#ARGV == -1){
+    print "Usage buildCDF.pl [options]\n";
+    print "options:\n";
+    print "\t-f <inputFile> : File containing a lookup delay measurement per line\n";
+    exit 0;
+}
+
+if (!getopts('f:')){
+    die "Unknown option specified\n";
+}
+
+if (!defined $opt_f) {
+    die "Must have an input file name";
+}
+
+
+
+my $latencyCounts = 0;
+
+open INFILE, "<$opt_f" or die "Couldn't open $opt_f";
+while (<INFILE>) {
+    my @line = split / /;
+    my $latency = $line[0];
+    $latencyCounts++;
+    print "$latency $latencyCounts\n";
+}
+
+close INFILE;
+
+
+#End
