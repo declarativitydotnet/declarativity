2f775b4ccfb34181a6c4259e4b4e5e1640bdd063
Timothy Roscoe <timothyroscoe>
2005-05-28 00:13:22 +0000

    Not-yet-working overlog tests

---
+++ trunk/tests/ol_tests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+import string
+import tempfile
+
+separator = "OUTPUT:"
+test_dir = os.path.join(os.path.dirname(sys.argv[0]), 'ol_parser_tests')
+overlog_binary = os.path.join(os.path.dirname(sys.argv[0]), 'overlog')
+for t in [t[:-4] for t in os.listdir(test_dir) if t [-4:] == '.tst']:
+  print "Running test '%s'..." % t
+  # Strip off the trailing '\n'...
+  lines = [l[:-1] for l in file(os.path.join(test_dir,t)+".tst").readlines()]
+  sep = lines.index(separator)
+  input_stuff = string.join(lines[:sep],'\n')+'\n'
+  in_file, in_filename = tempfile.mkstemp()
+  print in_file
+  os.fdopen(in_file).write(input_stuff)
+  out_file, out_filename = tempfile.mkstemp()
+  err_file, err_filename = tempfile.mkstemp()
+  out_file.close()
+  in_file.close()
+  err_file.close()
+  os.system('%s -c %s 2>%s >%s' % ( overlog_binary,
+                                    in_filename,
+                                    err_filename,
+                                    out_filename ))
+
+  expect_stuff = string.join(lines[sep+1:],'\n')
+  error_stuff = file(err_filename).read()
+  output_stuff = file(out_filename).read()
+  os.unlink(in_filename)
+  os.unlink(out_filename)
+  os.unlink(err_filename)
+  if error_stuff.strip() != expect_stuff.strip():
+    print "FAIL: ", t
+    print "INPUT:", input_stuff
+    print "OUTPUT:", output_stuff
+    print "EXPECT:", expect_stuff
+    print "ERROR:", error_stuff
+    
+  
+  
+  
+  
--- trunk/tests/overlog.C
+++ trunk/tests/overlog.C
@@ -17,6 +17,7 @@
 #include <arpc.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include "ol_lexer.h"
 #include "ol_context.h"
@@ -29,55 +30,65 @@ extern int ol_parser_debug;
 int main(int argc, char **argv)
 {
   std::cout << "OVERLOG\n";
-
-  
   ref< OL_Context > ctxt = New refcounted< OL_Context>();
-
-  strbuf filename;
+  bool route = false;
+  bool builtin = false;
 
   for( int i=1; i<argc; i++) { 
-    str arg(argv[i]);
+    std::string arg(argv[i]);
     
     if (arg == "-d") { 
       ol_parser_debug = 1;
     } else if (arg == "-h") { 
       std::cerr << "Usage: " << argv[0] << "{option|filename}+\n"
+		<< "\t-c: canonical form (used for builtin tests)\n"
 		<< "\t-d: turn on parser debugging\n"
+		<< "\t-r: try to instantiate a router config\n"
 		<< "\t-h: print this help text\n"
 		<< "\t- : read from stdin\n";
       exit(0);
+    } else if (arg == "-c") { 
+      builtin = true;
+    } else if (arg == "-r") {
+      route = true;
+      builtin = false;
     } else if (arg == "-") {
       ctxt->parse_stream(&std::cin);
-    } else {      
-      filename << argv[i];
-      str file(filename);
-      std::ifstream istr(file);
+    } else { 
+      std::ifstream istr(argv[i]);
       ctxt->parse_stream(&istr);
     }
   }
 
+  if (builtin) {
+    std::cerr << ctxt->toString();
+    std::cerr.flush();
+    std::cout << "OK\n";
+    std::cout.flush();
+  } 
+
   std::cout << "Finish parsing (functors / tableInfos) " << ctxt->getFunctors()->size() 
 	    << " " << ctxt->getTableInfos()->size() << "\n";
 
-  
-  // test a configuration of a router
-  Router::ConfigurationRef conf = New refcounted< Router::Configuration >();
-  Rtr_ConfGen gen(ctxt, conf, true, false, filename);
-  gen.createTables("localhost");
 
-  ref< Udp > udp = New refcounted< Udp >("Udp", 10000);
-  gen.configureRouter(udp, "localhost");
-
-  LoggerI::Level level = LoggerI::NONE;
-  RouterRef router = New refcounted< Router >(conf, level);
-  if (router->initialize(router) == 0) {
-    std::cout << "Correctly initialized network of reachability flows.\n";
-  } else {
-    std::cout << "** Failed to initialize correct spec\n";
-  }
-  
+  if (route) {
+    // test a configuration of a router
+    Router::ConfigurationRef conf = New refcounted< Router::Configuration >();
+    Rtr_ConfGen gen(ctxt, conf, true, false, "test config");
+    gen.createTables("localhost");
+    
+    ref< Udp > udp = New refcounted< Udp >("Udp", 10000);
+    gen.configureRouter(udp, "localhost");
+    
+    LoggerI::Level level = LoggerI::NONE;
+    RouterRef router = New refcounted< Router >(conf, level);
+    if (router->initialize(router) == 0) {
+      std::cout << "Correctly initialized network of reachability flows.\n";
+    } else {
+      std::cout << "** Failed to initialize correct spec\n";
+    }
+  } 
+  std::cout << "Done.\n";
 
   return 0;
 }
-
-
