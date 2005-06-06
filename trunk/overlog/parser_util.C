fc3a2a71e0d5c72a1fee3fdfba0a9be4f2fa5b38
Tyson Condie <tcondie>
2005-06-06 18:21:52 +0000

    *** empty log message ***

---
+++ trunk/overlog/parser_util.C
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2005 Intel Corporation. All rights reserved.
+ *
+ * This file is distributed under the terms in the attached INTEL-LICENSE file.
+ * If you do not find these files, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ * 
+ * DESCRIPTION: Parsing environment for Overlog (the P2 dialect of datalog)
+ *
+ */
+
+#ifndef __PARSER_UTIL_C__
+#define __PARSER_UTIL_C__
+
+#include "parser_util.h"
+
+
+//=====================================
+
+Parse_Expr* Parse_Agg::DONT_CARE = New Parse_Var(Val_Str::mk("*"));
+Parse_Expr* Parse_Expr::Nil = New Parse_Var(Val_Str::mk("null"));
+
+bool Parse_Agg::operator==(const Parse_Expr &e){
+  try {
+    const Parse_Agg& a = dynamic_cast<const Parse_Agg&>(e);
+    return Parse_Expr::operator==(e) && oper == a.oper;
+  }
+  catch (std::bad_cast e) {
+    return false;
+  }
+}
+
+str Parse_Agg::toString() {
+  strbuf a;
+  switch(oper) {
+    case MIN:   a << "min< "; break;
+    case MAX:   a << "max< "; break;
+    case COUNT: a << "count< "; break;
+  }
+  a << v->toString() << " >";
+  return a;
+}
+
+Parse_Bool::Parse_Bool(Parse_Bool::Operator o, Parse_Expr *l, Parse_Expr *r) 
+  : oper(o), lhs(l), rhs(r) {
+  // TODO: if (oper != NOT && rhs == NULL) ERROR!
+}
+
+bool Parse_Bool::operator==(const Parse_Expr &e) {
+  try {
+    const Parse_Bool& b = dynamic_cast<const Parse_Bool&>(e);
+    return *lhs == *b.lhs && *rhs == *b.rhs && oper == b.oper;
+  } catch (std::bad_cast e) {
+    return false;
+  }
+} 
+
+str Parse_Bool::toString() {
+  strbuf b;
+  if (oper == NOT) {
+    b << "NOT( " << lhs->toString() << " )";
+  }
+  else if (oper == RANGE) {
+    b << lhs->toString() << " IN " << rhs->toString();
+  }
+  else {
+    b << "( " << lhs->toString() << ")";
+    switch (oper) {
+      case AND: b << " AND "; break;
+      case OR:  b << " OR "; break;
+      case EQ:  b << " == "; break;
+      case NEQ: b << " != "; break;
+      case GT:  b << " > "; break;
+      case LT:  b << " < "; break;
+      case LTE: b << " <= "; break;
+      case GTE: b << " >= "; break;
+      default: assert(0);
+    }
+    b << "( " << rhs->toString() << " )";
+  }
+
+  return b;
+}
+
+
+bool Parse_Range::operator==(const Parse_Expr &e){
+  try {
+    const Parse_Range& r = dynamic_cast<const Parse_Range&>(e);
+    return *lhs == *r.lhs && *rhs == *r.rhs && type == r.type;
+  } catch (std::bad_cast e) {
+    return false;
+  }
+}
+
+str Parse_Range::toString() {
+  strbuf r;
+  switch (type) {
+    case RANGEOO: 
+      r << "(" << lhs->toString() << ", " << rhs->toString() << ")"; break;
+    case RANGEOC: 
+      r << "(" << lhs->toString() << ", " << rhs->toString() << "]"; break;
+    case RANGECO: 
+      r << "[" << lhs->toString() << ", " << rhs->toString() << ")"; break;
+    case RANGECC: 
+      r << "[" << lhs->toString() << ", " << rhs->toString() << "]"; break;
+  }
+  return r;
+}
+
+
+bool Parse_Math::operator==(const Parse_Expr &e){
+  try {
+    const Parse_Math& m = dynamic_cast<const Parse_Math&>(e);
+    return *lhs == *m.lhs && *rhs == *m.rhs && oper == m.oper;
+  }
+  catch (std::bad_cast e) {
+    return false;
+  }
+}
+
+str Parse_Math::toString() {
+  strbuf m;
+  bool lpar = (dynamic_cast<Parse_Math*>(lhs) != NULL);
+  bool rpar = (dynamic_cast<Parse_Math*>(rhs) != NULL);
+
+  if (lpar) m << "( ";
+  m << lhs->toString(); 
+  if (lpar) m << " )";
+
+  switch (oper) {
+    case LSHIFT:  m << " << "; break;
+    case RSHIFT:  m << " >> "; break;
+    case PLUS:    m << " + "; break;
+    case MINUS:   m << " - "; break;
+    case TIMES:   m << " * "; break;
+    case DIVIDE:  m << " / "; break;
+    case MODULUS: m << " % "; break;
+    case EXP:     m << " ** "; break;
+    default: assert(0);
+  }
+  if (rpar) m << "( ";
+  m << rhs->toString();
+  if (rpar) m << " )";
+
+  return m;
+}
+
+Parse_FunctorName::Parse_FunctorName(Parse_Expr *n, Parse_Expr *l) {
+  name = n->v->toString(); delete n;
+  if (l) {
+    loc = l->v->toString();
+    delete l;
+  } else {
+    loc = "";
+  }
+}
+
+str Parse_FunctorName::toString() {
+  strbuf fn;
+  fn <<  name;
+  if (loc != "") fn << "@" << loc;
+  return fn;
+}
+
+str Parse_Functor::toString() {
+  strbuf f;
+  f << fn->toString() << "( ";
+  for (int i = 0; i < args(); i++) {
+    f << arg(i)->toString();
+    if (i+1 < args()) f << ", ";
+    else f << " )";
+  }
+  return f;
+}
+
+int Parse_Functor::find(Parse_Expr *arg) {
+  return find(arg->v->toString());
+}
+
+int Parse_Functor::find(str argname) {
+  int p = 0;
+  for ( ; p < args() && arg(p)->toString() != argname; p++);
+  return (p < args()) ? p : -1;
+}
+
+int Parse_Functor::aggregate() {
+  for (int i = 0; i < args(); i++)
+    if (dynamic_cast<Parse_Agg*>(arg(i)) != NULL) return i;
+  return -1;
+}
+
+void Parse_Functor::replace(int p, Parse_Expr *e) {
+  Parse_ExprList::iterator next = args_->erase((args_->begin()+p));
+  args_->insert(next, e);
+}
+
+str Parse_Assign::toString() {
+  strbuf a;
+  a << var->toString() << " = " << assign->toString();
+  return a;
+}
+
+str Parse_Select::toString() {
+  return select->toString();
+}
+
+str Parse_Function::toString() {
+  strbuf f;
+  f << v->toString() << "( ";
+  for (int i = 0; i < args(); i++) {
+    f << arg(i)->toString();
+    if (i+1 < args()) f << ", ";
+  }
+  f << ")";
+  return f;
+}
+
+
+#endif /* __PARSER_UTIL_C__ */
---
+++ trunk/overlog/parser_util.h
@@ -0,0 +1,227 @@
+/*
+ * Copyright (c) 2005 Intel Corporation. All rights reserved.
+ *
+ * This file is distributed under the terms in the attached INTEL-LICENSE file.
+ * If you do not find these files, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ * 
+ * DESCRIPTION: Parsing environment for Overlog (the P2 dialect of datalog)
+ *
+ */
+
+#ifndef __PARSER_UTIL_H__
+#define __PARSER_UTIL_H__
+
+#include <deque>
+#include <iostream>
+#include <map>
+#include "value.h"
+#include "val_str.h"
+
+class OL_Lexer;
+
+class Parse_Expr {
+public:
+  static Parse_Expr* Nil;
+
+  Parse_Expr(ValuePtr val=NULL) : v(val), position_(-1) {};
+  Parse_Expr(Parse_Expr *val) : v(val->v), position_(-1) {};
+
+  virtual ~Parse_Expr() {};
+
+  virtual bool operator==(const Parse_Expr &e) 
+    { return v == e.v; };
+
+  virtual str toString() = 0;
+
+  virtual void position(int p) { position_ = p; };
+  virtual int position() { return position_; }
+
+  ValuePtr v;
+  int      position_;
+};
+typedef std::deque<Parse_Expr *> Parse_ExprList;
+
+// Boxing up a ValueRef see we can pass it through the Bison parser
+// union unscathed. 
+class Parse_Val : public Parse_Expr { 
+public:
+  Parse_Val(ValuePtr val) : Parse_Expr(val), neg_(false) {};
+  virtual void neg(bool n) { neg_ = n; };
+  virtual bool neg() { return neg_; };
+  virtual str toString() { return v->toString(); };
+
+private:
+  bool neg_;
+};
+
+class Parse_Var : public Parse_Expr { 
+public:
+  Parse_Var(ValuePtr var) : Parse_Expr(var) {};
+  Parse_Var(const strbuf& var) : Parse_Expr(Val_Str::mk(var))  {};
+
+  virtual str toString() { return v->toString(); };
+};
+
+class Parse_String : public Parse_Expr {
+public:
+  Parse_String(ValuePtr s) : Parse_Expr(s) {};
+
+  virtual str toString() { return strbuf()<<"\""<<v->toString()<<"\""; };
+};
+
+class Parse_Agg : public Parse_Expr {
+public:
+  enum Operator {MAX, MIN, COUNT};
+  static Parse_Expr* DONT_CARE;
+
+  Parse_Agg(Parse_Expr *v, Operator o) 
+    : Parse_Expr(v), oper(o) {};
+
+  virtual bool operator==(const Parse_Expr &e);
+
+  virtual str toString();
+
+  Operator oper;
+};
+
+class Parse_Bool : public Parse_Expr {
+public:
+  enum Operator {NOT, AND, OR, EQ, NEQ, GT, LT, LTE, GTE, RANGE, NOP};
+
+  Parse_Bool(Operator o, Parse_Expr *l, Parse_Expr *r=NULL); 
+  virtual ~Parse_Bool() { delete lhs; if (rhs) delete rhs; };
+  virtual bool operator==(const Parse_Expr &e); 
+
+  virtual str toString();
+
+  Operator   oper;
+  Parse_Expr *lhs;
+  Parse_Expr *rhs;
+};
+
+class Parse_Range : public Parse_Expr {
+public:
+  enum Interval{RANGEOO, RANGEOC, RANGECO, RANGECC};
+
+  Parse_Range(Interval i, Parse_Expr *l, Parse_Expr *r)
+    : type(i), lhs(l), rhs(r) { };
+
+  virtual bool operator==(const Parse_Expr &e);
+
+  virtual str toString();
+
+  Interval   type;
+  Parse_Expr *lhs;
+  Parse_Expr *rhs;
+};
+
+class Parse_Math : public Parse_Expr {
+public:
+  enum Operator {LSHIFT, RSHIFT, PLUS, MINUS, TIMES, DIVIDE, MODULUS, EXP, NOP};
+  Parse_Math(Operator o, Parse_Expr *l, Parse_Expr *r=NULL) 
+    : oper(o), lhs(l), rhs(r) {
+      // TODO: if (oper != NOP && rhs == NULL) ERROR!
+  };
+  ~Parse_Math() { delete lhs; if (rhs) delete rhs; };
+
+  virtual bool operator==(const Parse_Expr &e);
+
+  virtual str toString();
+
+  Operator   oper;
+  Parse_Expr *lhs;
+  Parse_Expr *rhs;
+};
+
+class Parse_Function : public Parse_Expr {
+public:
+  Parse_Function(Parse_Expr *n, Parse_ExprList *a) 
+    : Parse_Expr(n), args_(a) { };
+
+  ~Parse_Function() { delete args_; };
+
+  virtual str toString();
+
+  void arg(Parse_Expr *arg) { args_->push_back(arg); };
+  Parse_Expr* arg(int i) { return args_->at(i); };
+  int args() { return args_->size(); };
+
+
+  Parse_ExprList *args_;
+};
+
+class Parse_Term {
+public:
+  virtual ~Parse_Term() {};
+
+  virtual str toString() = 0;
+  void position(int p) { position_ = p; };
+  int position() { return position_; };
+
+  int position_;
+};
+typedef std::deque<Parse_Term *> Parse_TermList;
+
+class Parse_FunctorName {
+public:
+  Parse_FunctorName(Parse_Expr *n, Parse_Expr *l=NULL);
+
+  str toString();
+
+  str name;
+  str loc;
+};
+
+class Parse_Functor : public Parse_Term {
+public:
+  Parse_Functor(Parse_FunctorName *f, Parse_ExprList *a) 
+    : fn(f), args_(a) {};
+  virtual ~Parse_Functor() {delete fn; delete args_; };
+
+  virtual str toString();
+
+  int aggregate();
+
+  int find(Parse_Expr *arg);
+  int find(str argname);
+
+  void arg(Parse_Expr *arg) { args_->push_back(arg); };
+  Parse_Expr* arg(int i) { return args_->at(i); };
+  int args() { return args_->size(); };
+
+  void replace(int p, Parse_Expr *e);
+
+  Parse_FunctorName *fn;
+  Parse_ExprList    *args_;
+};
+
+class Parse_Assign : public Parse_Term {
+public:
+  Parse_Assign(Parse_Expr *v, Parse_Expr *a) 
+    : var(NULL), assign(a) {
+    var = dynamic_cast<Parse_Var*>(v);
+  };
+  virtual ~Parse_Assign() { delete var; delete assign; };
+
+  virtual str toString();
+
+  Parse_Var  *var;
+  Parse_Expr *assign;
+};
+
+class Parse_Select : public Parse_Term {
+public:
+  Parse_Select(Parse_Expr *s) {
+    select = dynamic_cast<Parse_Bool*>(s);
+  };
+  virtual ~Parse_Select() { delete select; }
+
+  virtual str toString();
+
+  Parse_Bool *select;
+};
+
+
+#endif /* __PARSER_UTIL_H__ */
