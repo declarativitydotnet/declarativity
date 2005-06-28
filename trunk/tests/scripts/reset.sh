a05636b96d1e18756dca6b9406df4b14a475707a
Tyson Condie <tcondie>
2005-06-28 01:45:33 +0000

    *** empty log message ***

--- trunk/tests/scripts/reset.sh
+++ trunk/tests/scripts/reset.sh
@@ -2,9 +2,12 @@
 
 for i in `ps -ef | grep run_lookups | awk '{print $2}'` 
 do
-	kill -9 $i
+	kill -9 $i >> $1/reset.log 2>&1
 done
 for i in `ps -ef | grep run_node | awk '{print $2}'` 
 do
-	kill -9 $i
+	kill -9 $i >> $1/reset.log 2>&1
 done
+echo "\n\n============= REMAINING PROCESSES =============" >> $1/reset.log 2>&1
+ps -ef | grep `whoami`>> $1/reset.log 2>&1
+echo "===============================================\n\n" >> $1/reset.log 2>&1
