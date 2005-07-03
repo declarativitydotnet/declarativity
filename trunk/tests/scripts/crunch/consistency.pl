0916adaede120af7edeed0c428503b75e5c74cbb
Boon Thau Loo <boonloo>
2005-07-03 20:57:49 +0000

    crunch

---
+++ trunk/tests/scripts/crunch/consistency.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl 
+
+use strict 'vars';
+use Getopt::Std;
+use FindBin;
+
+our($opt_f);
+our($opt_n);
+
+if ($#ARGV == -1){
+    print "Usage buildCDF.pl [options]\n";
+    print "options:\n";
+    print "\t-f <inputFile> : File containing a lookup delay measurement per line\n";
+    print "\t-n <lookups> : Number of simultaneous lookups\n";
+    exit 0;
+}
+
+if (!getopts('f:n:')){
+    die "Unknown option specified\n";
+}
+
+if (!defined $opt_f) {
+    die "Must have an input file name";
+}
+
+if (!defined $opt_n) {
+    die "Must have number of lookups";
+}
+
+my $totalLatencyCounts = 0;
+
+open INFILE, "<$opt_f" or die "Couldn't open $opt_f";
+
+# go through the file, get <IP address, search key, located key>
+# index each search key by all located keys separated by ":"
+# go through each search key, for each key, count the size
+# pick largest, divide by N
+
+my %searchKeys;
+while (<INFILE>) {
+	chomp;
+	my @line = split /, /;
+	$searchKeys{$line[4]} = "";	
+    }
+close INFILE;
+
+open INFILE, "<$opt_f" or die "Couldn't open $opt_f";
+while (<INFILE>) {
+    chomp;
+    my @line = split /, /;
+    my $str = $searchKeys{$line[4]};
+    $str = $str . " " . $line[5];
+    $searchKeys{$line[4]} = $str;
+}
+close INFILE;
+
+# go through all keys
+my $key;
+my $totalSize = 0;
+my @consistencyArray = ();
+foreach $key (keys (%searchKeys)) {
+    #print $key . " => " . $searchKeys{$key} . "\n";
+    my @array = split(/ /, $searchKeys{$key});
+    my $x = 0;
+    my $y = 0;
+    my $size = @array;
+    my $max = -1;
+    for ($x = 0; $x < $size; $x++) {
+	my $count = 0;
+	my $str1 = $array[$x];	
+	for ($y = 0; $y < $size; $y++) {	    
+		my $str2 = $array[$y];
+		if ($str1 == $str2) {
+		    $count++;		
+		}
+	}
+	if ($count > $max) {
+	    $max = $count * 1.0;
+	}
+    }
+    if ($max > $opt_n) {
+	$max = $opt_n;
+    } 
+    $consistencyArray[$max] = $consistencyArray[$max] + 1;
+    $totalSize += 1;
+}
+
+my $x = 0;
+my $cum = 0;
+for ($x = 0; $x < @consistencyArray; $x++) {
+    $cum = $cum + $consistencyArray[$x];
+    print $x . " " . $consistencyArray[$x] . " " . $x / $opt_n . " " . $cum / $totalSize . "\n";
+}
+
--- trunk/tests/scripts/crunch/crunch.sh
+++ trunk/tests/scripts/crunch/crunch.sh
@@ -1,6 +1,7 @@
 # $1 is number of nodes
 # $2 is the location of the logs
 rm $2/*.dat
+rm $2/simple_results
 ../pout.py -e -n $1 $2
 
 ./buildCDF.pl -f simple_latency.dat > $2/simple_latency_cdf.dat
@@ -10,4 +11,5 @@ mv maintenance_latency.dat $2/.
 mv node_bw.dat $2/.
 mv simple_hop_time.dat $2/.
 mv maintenance_hop_time.dat $2/.
-
+./stripResults.sh $2/
+./consistency.pl -f $2/simple_results -n 10 > $2/consistency.dat
