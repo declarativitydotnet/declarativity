5b61cc47a8a4b01b7c547fc1fc010f9ef48d81b6
Boon Thau Loo <boonloo>
2005-07-08 17:05:26 +0000

    scripts

--- trunk/tests/scripts/crunch/crunch.sh
+++ trunk/tests/scripts/crunch/crunch.sh
@@ -2,8 +2,10 @@
 # $2 is the location of the logs
 # $3 is the number of simulteneous issues
 rm $2/*.dat
+rm $2/*.out
 rm $2/simple_results
-../pout.py -e -n $1 $2
+
+../pout.py -e -n $1 $2 
 
 ./buildCDF.pl -f simple_latency.dat > $2/simple_latency_cdf.dat
 ./hopDist.pl -f simple_latency.dat > $2/simple_latency_hopdist.dat
@@ -14,3 +16,4 @@ mv simple_hop_time.dat $2/.
 mv maintenance_hop_time.dat $2/.
 ./stripResults.sh $2/
 ./consistency.pl -f $2/simple_results -n $3 > $2/consistency.dat
+./messages.pl -f $2/ 
---
+++ trunk/tests/scripts/crunch/messages.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl 
+
+use strict 'vars';
+use Getopt::Std;
+use FindBin;
+
+our($opt_f);
+our($opt_n);
+
+if ($#ARGV == -1){
+    print "Usage buildCDF.pl [options]\n";
+    print "options:\n";
+    print "\t-f <inputFile> : Directory containing all the logs\n";
+    exit 0;
+}
+
+if (!getopts('f:n:')){
+    die "Unknown option specified\n";
+}
+
+if (!defined $opt_f) {
+    die "Must have an input file name";
+}
+
+opendir (INDIR, "$opt_f") || die "Couldn't open $opt_f";
+
+# go through each directory, look at the log file messages
+# build an in-memory data structure on the lookups 
+# 1) send <dst, lookup eventID, time>
+# 2) receive <src, lookup eventID, time>
+my $file;
+my %receiveMsgs;
+my %sendMsgs;
+my %addresses;
+my $numReceiveMsgs = 0;
+my $numSendMsgs = 0;
+while ($file = readdir(INDIR)) {
+    if (index($file, "dat") == -1 &&
+	index($file, "simple") == -1 &&
+	index($file, "out") == -1) {
+	opendir (NODEDIR, $opt_f . "/" . $file) || die "Cannot open $file";
+	my $nodeFile;
+	while ($nodeFile = readdir(NODEDIR)) {
+	    if (index($nodeFile, "out") != -1) {		
+		open INFILE, $opt_f . "/" . $file . "/" . $nodeFile  
+		    or die "Couldn't open $opt_f/$file/$nodeFile";
+		my $nextLine;
+		while ($nextLine = <INFILE>) {
+		    chomp;
+		    my @line = split(/, /, $nextLine);
+		    if (index($nextLine, "ReceiveBefore") != -1 &&
+			index($nextLine, "lookup,") != -1) {
+			my $timeReceive = $line[1];
+			my $destination = $line[3];
+			my $eventID = substr($line[6], 0, length($line[6] - 2));
+			#print $timeReceive . " " . $destination . " " . $eventID . "\n";
+			@receiveMsgs{$destination . ":" . $eventID} = $timeReceive;
+			@addresses{$destination} = $destination;
+			$numReceiveMsgs += 1;
+		    }
+
+		    if (index($nextLine, "RemoteSend") != -1 &&
+			index($nextLine, "lookup,") != -1) {
+			my $timeReceive = $line[1];
+			my $destination = $line[3];
+			my $eventID = substr($line[6], 0, length($line[6] - 2));
+			#print $timeReceive . " " . $destination . " " . $eventID . "\n";
+			@sendMsgs{$destination . ":" . $eventID} = $timeReceive;
+			$numSendMsgs += 1;
+		    }
+		}		
+	    }
+	}      	
+    }
+}
+
+# count messages sent and latency
+# percentage losses?
+# latency, local timestamps
+print "Finish processing $numReceiveMsgs $numSendMsgs\n";		
+
+my $key;
+my $failures = 0;
+foreach $key (keys (%sendMsgs)) { 
+    my @fields = split(/:/,  $key);    
+    if (@addresses{"$fields[0]:$fields[1]"} != undef && 
+	@receiveMsgs{$key} == undef) {
+	$failures += 1;
+    } else {
+	my $latency = @sendMsgs{$key} - @receiveMsgs{$key};
+    }
+}
+my $percentFailures = $failures / $numSendMsgs * 100;
+print "Number of failures: $failures $percentFailures\n";
+
