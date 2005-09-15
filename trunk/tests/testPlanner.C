f7fbf97a60febc8cffea12f0fbe9ada910346fd6
Boon Thau Loo <boonloo>
2005-09-15 21:15:28 +0000

    new planner test

---
+++ trunk/tests/testPlanner.C
@@ -0,0 +1,137 @@
+// -*- c-basic-offset: 2; related-file-name: "" -*-
+/*
+ * @(#)$Id$
+ *
+ * Copyright (c) 2004 Intel Corporation. All rights reserved.
+ *
+ * This file is distributed under the terms in the attached INTEL-LICENSE file.
+ * If you do not find these files, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ * 
+ * DESCRIPTION: Test harness for planner....
+ *
+ */
+
+#include <async.h>
+#include <arpc.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "ol_lexer.h"
+#include "ol_context.h"
+#include "eca_context.h"
+#include "rtr_confgen.h"
+#include "catalog.h"
+#include "udp.h"
+#include "planner.h"
+#include "ruleStrand.h"
+
+#include "dot.h"
+
+extern int ol_parser_debug;
+
+int main(int argc, char **argv)
+{
+  std::cout << "OVERLOG\n";
+  ref< OL_Context > ctxt = New refcounted< OL_Context>();
+  bool route = false;
+  bool builtin = false;
+  str filename("");
+
+  for( int i=1; i<argc; i++) { 
+    std::string arg(argv[i]);
+    
+    if (arg == "-d") { 
+      ol_parser_debug = 1;
+    } else if (arg == "-h") { 
+      std::cerr << "Usage: " << argv[0] << "{option|filename}+\n"
+		<< "\t-c: canonical form (used for builtin tests)\n"
+		<< "\t-d: turn on parser debugging\n"
+		<< "\t-r: try to instantiate a router config\n"
+                << "\t-g: produce a DOT graph spec\n"
+		<< "\t-h: print this help text\n"
+		<< "\t- : read from stdin\n";
+      exit(0);
+    } else if (arg == "-c") { 
+      builtin = true;
+    } else if (arg == "-r") {
+      route = true;
+      builtin = false;
+    } else if (arg == "-") {
+      ctxt->parse_stream(&std::cin);
+    } else if (arg == "-g") {
+      filename = argv[i+1];
+      std::ifstream istr(filename);
+      std::ofstream ostr(str(strbuf() << filename << ".dot"));
+      ctxt->parse_stream(&istr);
+      Router::ConfigurationRef conf = New refcounted< Router::Configuration >();
+      ref< Catalog > catalog = New refcounted< Catalog >();  
+      catalog->initTables(ctxt); 
+      ref< ECA_Context > ectxt = New refcounted< ECA_Context >(); 
+      ectxt->eca_rewrite(ctxt, catalog);
+      
+      ref< Planner > planner 
+	= New refcounted< Planner >(conf, catalog, false, "127.0.0.1:10000");  
+      ref< Udp > udp = New refcounted< Udp >("Udp", 10000);
+      std::vector<RuleStrand*> ruleStrands = planner->generateRuleStrands(ectxt);
+
+      planner->setupNetwork(udp);
+      planner->registerAllRuleStrands(ruleStrands);
+
+      toDot(&ostr, conf);
+      exit (0);
+    } else { 
+      filename = argv[i];
+      std::ifstream istr(argv[i]);
+      ctxt->parse_stream(&istr);
+    }
+  }
+
+  if (builtin) {
+    std::cerr << ctxt->toString();
+    std::cerr.flush();
+    std::cout << "OK\n";
+    std::cout.flush();
+  } 
+
+  std::cout << "Finish parsing (functors / tableInfos) " 
+	    << ctxt->getRules()->size() 
+	    << " " << ctxt->getTableInfos()->size() << "\n";
+
+  std::cout << ctxt->toString() << "\n";
+
+  if (route) {
+    Router::ConfigurationRef conf = New refcounted< Router::Configuration >();
+    ref< Catalog > catalog = New refcounted< Catalog >();  
+    catalog->initTables(ctxt); 
+    ref< ECA_Context > ectxt = New refcounted< ECA_Context >(); 
+    ectxt->eca_rewrite(ctxt, catalog);
+    
+    ref< Planner > planner 
+      = New refcounted< Planner >(conf, catalog, false, "127.0.0.1:10000");  
+    ref< Udp > udp = New refcounted< Udp >("Udp", 10000);
+    std::vector<RuleStrand*> ruleStrands = planner->generateRuleStrands(ectxt);
+    
+    for (unsigned k = 0; k < ruleStrands.size(); k++) {
+      std::cout << ruleStrands.at(k)->toString();
+    }
+    
+    planner->setupNetwork(udp);
+    planner->registerAllRuleStrands(ruleStrands);
+    std::cout << planner->getNetPlanner()->toString() << "\n";
+
+    LoggerI::Level level = LoggerI::NONE;
+    RouterRef router = New refcounted< Router >(conf, level);
+    if (router->initialize(router) == 0) {
+      std::cout << "Correctly initialized network of reachability flows.\n";
+    } else {
+      std::cout << "** Failed to initialize correct spec\n";
+    }    
+    std::cout << "Done.\n";
+  }
+
+  return 0;
+}
