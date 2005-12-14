dcb22e353c30c7b14d1b41b36a4820250a925a7b
Petros Maniatis <maniatis>
2005-12-14 23:09:49 +0000

    Event loop skeleton

---
+++ trunk/eventLoop/loop.C
@@ -0,0 +1,9 @@
+// -*- c-basic-offset: 2; related-file-name: "loop.h" -*-
+/*
+ * @(#)$Id$
+ *
+ */
+
+#include "loop.h"
+
+
---
+++ trunk/eventLoop/loop.h
@@ -0,0 +1,32 @@
+// -*- c-basic-offset: 2; related-file-name: "loop.C" -*-
+/*
+ * @(#)$Id$
+ *
+ * Event loop, inspired by core.C in libasync by David Mazieres.
+ * 
+ * Copyright (c) 2005 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software")
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ * 
+ * This file is distributed under the terms in the attached INTEL-LICENSE file.
+ * If you do not find these files, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ * 
+ * DESCRIPTION: Base class for P2 elements
+ *
+ */
+
+#ifndef __LOOP_H__
+#define __LOOP_H__
+
+
+#endif /* __LOOP_H_ */
