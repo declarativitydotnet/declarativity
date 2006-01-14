a1b4376fcee48a8596acd09508634765980498af
Petros Maniatis <maniatis>
2006-01-14 05:00:22 +0000

    Added elementary marshalField and unmarshalField test cases

--- trunk/unitTests/testMarshal.C
+++ trunk/unitTests/testMarshal.C
@@ -18,20 +18,27 @@ private:
   MarshalField mF;
   UnmarshalField uF;
   TuplePtr flat;
+  TuplePtr nested;
   
 public:
   testMarshal()
     : m("Marshal"),
       u("Unmarshal"),
-      mF("MarshalField", 0),
-      uF("UnmarshalField", 0)
+      mF("MarshalField", 1),
+      uF("UnmarshalField", 1)
   {
     // Create a straight tuple
     // The input tuple
     flat = Tuple::mk();
-    flat->append(Val_Str::mk("String"));
+    flat->append(Val_Str::mk("Flat"));
     flat->append(Val_UInt64::mk(13500975));
     flat->freeze();  
+
+    // Create a nested tuple
+    nested = Tuple::mk();
+    nested->append(Val_Str::mk("Nested"));
+    nested->append(Val_Tuple::mk(flat));
+    nested->freeze();
   }
 
   void
@@ -53,12 +60,41 @@ public:
     TuplePtr result = m.simple_action(flat);
     TuplePtr reFlat = u.simple_action(result);
 
-    BOOST_REQUIRE_MESSAGE(reFlat != NULL, "Didn't get a unmarshaling result");
+    BOOST_REQUIRE_MESSAGE(reFlat != NULL, "Didn't get an unmarshaling result");
     BOOST_CHECK_MESSAGE(reFlat->size() == flat->size(),
                         "Marshalled/unmarshalled tuple is different size");
     BOOST_CHECK_MESSAGE(reFlat->compareTo(flat) == 0,
                         "Marshalled/unmarshalled tuple does not match original");
   }
+
+
+  void
+  testMF()
+  {
+    // Marshal it
+    TuplePtr result = mF.simple_action(nested);
+    
+    BOOST_REQUIRE_MESSAGE(result != NULL, "Didn't get a field marshaling result");
+    BOOST_CHECK_MESSAGE(result->size() == 2, "Outer tuple has wrong size\n");
+    BOOST_CHECK_MESSAGE((*result)[1]->typeCode() == Value::OPAQUE,
+                        "Marshalled field is not OPAQUE 1\n");
+  }
+
+
+  void
+  testUF()
+  {
+    // Marshal it and unmarshal it
+    TuplePtr result = mF.simple_action(nested);
+    TuplePtr reNested = uF.simple_action(result);
+
+    BOOST_REQUIRE_MESSAGE(reNested != NULL, "Didn't get a field unmarshaling result");
+    BOOST_CHECK_MESSAGE(reNested->size() == nested->size(),
+                        "Field marshalled/unmarshalled tuple is different size");
+    BOOST_CHECK_MESSAGE(reNested->compareTo(nested) == 0,
+                        "Field marshalled/unmarshalled tuple does not match original");
+  }
+
 };
 
 class testMarshal_testSuite
@@ -72,5 +108,7 @@ public:
 
     add(BOOST_CLASS_TEST_CASE(&testMarshal::testM, instance));
     add(BOOST_CLASS_TEST_CASE(&testMarshal::testU, instance));
+    add(BOOST_CLASS_TEST_CASE(&testMarshal::testMF, instance));
+    add(BOOST_CLASS_TEST_CASE(&testMarshal::testUF, instance));
   }
 };
