160aca730d8a1a66984623e342ac1d94a1da401b
Petros Maniatis <maniatis>
2006-01-25 00:00:53 +0000

    Cleaned up license files

--- branches/Release0_1/eventLoop/loop.h
+++ branches/Release0_1/eventLoop/loop.h
@@ -2,26 +2,14 @@
 /*
  * @(#)$Id$
  *
- * Event loop, inspired by core.C in libasync by David Mazieres.
- * 
  * Copyright (c) 2005 Intel Corporation
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software")
- * to deal in the Software without restriction, subject to the conditions
- * listed in the Click LICENSE file. These conditions include: you must
- * preserve this copyright notice, and you cannot mention the copyright
- * holders in advertising related to the Software without their permission.
- * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
- * notice is a summary of the Click LICENSE file; the license in that file is
- * legally binding.
- * 
  * This file is distributed under the terms in the attached INTEL-LICENSE file.
  * If you do not find these files, copies can be found by writing to:
  * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
  * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
  * 
- * DESCRIPTION: Base class for P2 elements
+ * DESCRIPTION: Base class for P2 event loop
  *
  */
 
