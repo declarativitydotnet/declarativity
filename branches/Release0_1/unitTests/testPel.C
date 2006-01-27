2aa1b80f77675804cf5384d2286d1de709bc1550
Petros Maniatis <maniatis>
2006-01-27 20:04:45 +0000

    Integrated pel compiler tests into unit testing framework

--- branches/Release0_1/unitTests/testPel.C
+++ branches/Release0_1/unitTests/testPel.C
@@ -7,23 +7,10 @@
 
 #include "boost/test/unit_test.hpp"
 
-#include "fdbuf.h"
-#include "value.h"
-#include <rpc/xdr.h>
-#include "xdrbuf.h"
-
-
-#include "val_null.h"
-#include "val_str.h"
-#include "val_int32.h"
-#include "val_uint32.h"
-#include "val_int64.h"
-#include "val_uint64.h"
-#include "val_double.h"
-#include "val_opaque.h"
-#include "val_tuple.h"
-#include "val_time.h"
-#include "val_id.h"
+#include "pel_lexer.h"
+#include "pel_program.h"
+#include "pel_vm.h"
+#include <sstream>
 
 // The test macro
 #define TST(_e,_l,_s,_c) { _c;                                          \
@@ -63,24 +50,17 @@ private:
     /** Generated instructions */
     int	num_opcodes;
   };
+
+  static const CompilerTest ctests[];
   
-  const CompilerTest ctests[] = {
-    { "  ", "", 0, 0 },
-    { "1 2 3", "1 2 3 ", 3, 3},
-    { "1 1.2 \"String\" swap dup ", "1 1.2 \"String\" swap dup ", 3, 5},
-    { "NULL\t\n pop $2 $4  ->u32", "NULL pop $2 $4 ->u32 ", 1, 5},
-    { "1 ifpoptuple", "1 ifpoptuple ", 1, 2},
-    { "1 2 /* This is a comment */ pop pop", "1 2 pop pop ", 2, 4}
-  };
-  
-  const size_t num_ctests = sizeof(ctests) / sizeof(CompilerTest);
+  static const size_t num_ctests;
 
 public:
   
   void
   compilerTests()
   {
-    for(i = 0;
+    for(size_t i = 0;
         i < num_ctests;
         i++) {
       const CompilerTest *t = &ctests[i];
@@ -88,31 +68,31 @@ public:
       boost::shared_ptr<Pel_Program> prog = Pel_Lexer::compile(t->src);
 
       // Check opcodes left
-      std::ostrstream message1();
+      std::ostringstream message1;
       message1 << "Bad # opcodes for '"
                << t->src
                << "'; "
                << prog->ops.size()
                << " instead of expected "
                << t->num_opcodes;
-      BOOST_CHECK_MESSAGE(prog->ops.size() != (uint) t->num_opcodes,
-                          message1);
+      BOOST_CHECK_MESSAGE(prog->ops.size() == (uint) t->num_opcodes,
+                          message1.str().c_str());
 
       
       // Check constants in poll
-      std::ostrstream message2();
+      std::ostringstream message2;
       message2 << "Bad # consts for '"
                << t->src
                << "'; "
                << prog->const_pool.size()
                << " instead of expected "
                << t->num_consts;
-      BOOST_CHECK_MESSAGE(prog->const_pool.size() != (uint) t->num_consts,
-                          message2);
+      BOOST_CHECK_MESSAGE(prog->const_pool.size() == (uint) t->num_consts,
+                          message2.str().c_str());
 
       
       // Decompile and compare
-      std::ostrstream message3();
+      std::ostringstream message3;
       std::string dec = Pel_Lexer::decompile(*prog);
       message3 << "Bad disassembly for '"
                << t->src
@@ -120,13 +100,26 @@ public:
                << dec
                << "' instead of expected '"
                << t->disassembly;
-      BOOST_CHECK_MESSAGE(dec != t->disassembly,
-                          message3);
+      BOOST_CHECK_MESSAGE(dec == t->disassembly,
+                          message3.str().c_str());
     }  
   }
 };
 
 
+const testPel::CompilerTest
+testPel::ctests[] = {
+  { "  ", "", 0, 0 },
+  { "1 2 3", "1 2 3 ", 3, 3},
+  { "1 1.2 \"String\" swap dup ", "1 1.2 \"String\" swap dup ", 3, 5},
+  { "NULL\t\n pop $2 $4  ->u32", "NULL pop $2 $4 ->u32 ", 1, 5},
+  { "1 ifpoptuple", "1 ifpoptuple ", 1, 2},
+  { "1 2 /* This is a comment */ pop pop", "1 2 pop pop ", 2, 4}
+};
+  
+const size_t
+testPel::num_ctests = sizeof(testPel::ctests) /
+                  sizeof(testPel::CompilerTest);
 
 
 
