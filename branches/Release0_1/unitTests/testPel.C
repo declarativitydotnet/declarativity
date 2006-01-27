bbd5bffdfcf3f861e4f49f2a8b4e901a8e87073b
Petros Maniatis <maniatis>
2006-01-27 19:10:38 +0000

    Added

---
+++ branches/Release0_1/unitTests/testPel.C
@@ -0,0 +1,148 @@
+/* 
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
+ * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
+ */
+
+#include "boost/test/unit_test.hpp"
+
+#include "fdbuf.h"
+#include "value.h"
+#include <rpc/xdr.h>
+#include "xdrbuf.h"
+
+
+#include "val_null.h"
+#include "val_str.h"
+#include "val_int32.h"
+#include "val_uint32.h"
+#include "val_int64.h"
+#include "val_uint64.h"
+#include "val_double.h"
+#include "val_opaque.h"
+#include "val_tuple.h"
+#include "val_time.h"
+#include "val_id.h"
+
+// The test macro
+#define TST(_e,_l,_s,_c) { _c;                                          \
+    BOOST_CHECK_MESSAGE(fb.length() == _l, "Bad fdbuf length");         \
+    BOOST_CHECK_MESSAGE(fb.last_errno() == _e, "Bad errno");            \
+    BOOST_CHECK_MESSAGE(fb.str() == _s, "Bad fdbuf contents");          \
+}
+
+
+class testPel
+{
+private:
+
+
+public:
+  testPel()
+  {
+  }
+
+
+
+private:
+  ////////////////////////////////////////////////////////////
+  // Compiler Tests
+  ////////////////////////////////////////////////////////////
+  
+  struct CompilerTest {
+    /** Source code */
+    char *src; 
+
+    /** Expected output of disassembler */
+    char *disassembly;
+
+    /** Number of constants in the pool afterwards */
+    int num_consts;
+
+    /** Generated instructions */
+    int	num_opcodes;
+  };
+  
+  const CompilerTest ctests[] = {
+    { "  ", "", 0, 0 },
+    { "1 2 3", "1 2 3 ", 3, 3},
+    { "1 1.2 \"String\" swap dup ", "1 1.2 \"String\" swap dup ", 3, 5},
+    { "NULL\t\n pop $2 $4  ->u32", "NULL pop $2 $4 ->u32 ", 1, 5},
+    { "1 ifpoptuple", "1 ifpoptuple ", 1, 2},
+    { "1 2 /* This is a comment */ pop pop", "1 2 pop pop ", 2, 4}
+  };
+  
+  const size_t num_ctests = sizeof(ctests) / sizeof(CompilerTest);
+
+public:
+  
+  void
+  compilerTests()
+  {
+    for(i = 0;
+        i < num_ctests;
+        i++) {
+      const CompilerTest *t = &ctests[i];
+
+      boost::shared_ptr<Pel_Program> prog = Pel_Lexer::compile(t->src);
+
+      // Check opcodes left
+      std::ostrstream message1();
+      message1 << "Bad # opcodes for '"
+               << t->src
+               << "'; "
+               << prog->ops.size()
+               << " instead of expected "
+               << t->num_opcodes;
+      BOOST_CHECK_MESSAGE(prog->ops.size() != (uint) t->num_opcodes,
+                          message1);
+
+      
+      // Check constants in poll
+      std::ostrstream message2();
+      message2 << "Bad # consts for '"
+               << t->src
+               << "'; "
+               << prog->const_pool.size()
+               << " instead of expected "
+               << t->num_consts;
+      BOOST_CHECK_MESSAGE(prog->const_pool.size() != (uint) t->num_consts,
+                          message2);
+
+      
+      // Decompile and compare
+      std::ostrstream message3();
+      std::string dec = Pel_Lexer::decompile(*prog);
+      message3 << "Bad disassembly for '"
+               << t->src
+               << "'; '"
+               << dec
+               << "' instead of expected '"
+               << t->disassembly;
+      BOOST_CHECK_MESSAGE(dec != t->disassembly,
+                          message3);
+    }  
+  }
+};
+
+
+
+
+
+
+
+class testPel_testSuite
+  : public boost::unit_test_framework::test_suite
+{
+public:
+  testPel_testSuite()
+    : boost::unit_test_framework::test_suite("testPel")
+  {
+    boost::shared_ptr<testPel> instance(new testPel());
+    
+    
+    add(BOOST_CLASS_TEST_CASE(&testPel::compilerTests,
+                              instance));
+  }
+};
