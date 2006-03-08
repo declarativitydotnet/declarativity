51a1d8183765e8cf0b186d0d9fbd4fdd0721ab37
Petros Maniatis <maniatis>
2006-03-08 02:38:16 +0000

    Merged in release 0_1

--- trunk/eventLoop/p2Time.h
+++ trunk/eventLoop/p2Time.h
@@ -2,28 +2,15 @@
 /*
  * @(#)$Id$
  *
- * Event loop, inspired by core.C in libasync by David Mazieres.
- * 
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software")
- * to deal in the Software without restriction, subject to the conditions
- * listed in the Click LICENSE file. These conditions include: you must
- * preserve this copyright notice, and you cannot mention the copyright
- * holders in advertising related to the Software without their permission.
- * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
- * notice is a summary of the Click LICENSE file; the license in that file is
- * legally binding.
- * 
- * This file is distributed under the terms in the attached INTEL-LICENSE file.
- * If you do not find these files, copies can be found by writing to:
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
  * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300,
  * Berkeley, CA, 94704.  Attention:  Intel License Inquiry.
  * Or
  * UC Berkeley EECS Computer Science Division, 387 Soda Hall #1776, 
  * Berkeley, CA,  94707. Attention: P2 Group.
  * 
- * DESCRIPTION: Base class for P2 time utilities
+ * DESCRIPTION: P2 time utilities
  *
  */
 
