285a7f26bf1ef5782bea81ab9155c46fc6dc9e33
Joe Hellerstein <joe@trifacta.com>
2006-05-26 23:06:50 +0000

    add element wrappers for python debugger

---
+++ trunk/python/p2/debugger/ruleTracer.cpp
@@ -0,0 +1,14 @@
+#include <ruleTracer.h>
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+void export_ruleTracer()
+{
+  class_<RuleTracer, bases<Element>, boost::shared_ptr<RuleTracer>, boost::noncopyable>
+        ("RuleTracer", init<std::string, std::string, std::string, int, int, int, TablePtr, TablePtr>())
+    .def("class_name", &RuleTracer::class_name)
+    .def("processing", &RuleTracer::processing)
+    .def("flow_code",  &RuleTracer::flow_code)
+  ;
+}
