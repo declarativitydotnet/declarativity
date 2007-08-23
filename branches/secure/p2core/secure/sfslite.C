b0500dfe1faca1ba0721305d5ec24a5d41ae6cdf
princem <princem>
2007-08-23 07:59:46 +0000

    test cases for secure algo

--- branches/secure/p2core/secure/sfslite.C
+++ branches/secure/p2core/secure/sfslite.C
@@ -16,12 +16,7 @@ namespace compile {
       };
     };
     
-
-    const uint32_t RSA = 0, AES = 1;
-    const uint32_t RSAPriv = 2, RSAPub = 3, AESSecret = 4;
-    
-    inline void
-    b16xor (b16 *d, const b16 &s)
+    void b16xor (b16 *d, const b16 &s)
     {
       for (int i = 0; i < b16::nl; i++)
 	d->l[i] ^= s.l[i];
@@ -33,7 +28,7 @@ namespace compile {
 	d->l[i] = s1.l[i] ^ s2.l[i];
     }
     
-    
+    const int Sfslite::hashSize = 20;
     void cbcencrypt (aes *cp, void *_d, const void *_s, int len)
     {
       assert (!(len & 15));
@@ -78,12 +73,12 @@ namespace compile {
       char *qbuf = new char[sizeQ];
       mpz_get_raw (pbuf, sizeP, &priv->p);
       mpz_get_raw (qbuf, sizeQ, &priv->q);
-      serializedPriv->push_uint32(RSAPriv);
+      serializedPriv->push_uint32(Sfslite::RSAPriv);
       serializedPriv->push_uint32(sizeP);
-      serializedPriv->push_bytes(pbuf, sizeP);
       serializedPriv->push_uint32(sizeQ);
-      serializedPriv->push_bytes(qbuf, sizeQ);
       serializedPriv->push_uint64(priv->k);
+      serializedPriv->push_bytes(pbuf, sizeP);
+      serializedPriv->push_bytes(qbuf, sizeQ);
       delete pbuf;
       delete qbuf;
       return serializedPriv;
@@ -94,12 +89,12 @@ namespace compile {
     {
       //create a reader copy
       FdbufPtr serializedPriv(new Fdbuf(privBuf));
-      assert(serializedPriv->pop_uint32()==RSAPriv);
+      assert(serializedPriv->pop_uint32()==Sfslite::RSAPriv);
       uint32_t sizeP = serializedPriv->pop_uint32();
+      uint32_t sizeQ = serializedPriv->pop_uint32();
       bigint p, q;
       mpz_set_raw (&p, serializedPriv->raw_inline(), sizeP);
-      uint32_t sizeQ = serializedPriv->pop_uint32();
-      mpz_set_raw (&q, serializedPriv->raw_inline(), sizeQ);
+      mpz_set_raw (&q, serializedPriv->raw_inline() + sizeP, sizeQ);
       uint64_t k = serializedPriv->pop_uint64();
       
       return new esign_priv(p, q, k);
@@ -112,10 +107,10 @@ namespace compile {
       
       char *nbuf = new char[sizeN];
       mpz_get_raw (nbuf, sizeN, &pub->n);
-      serializedPub->push_uint32(RSAPub);
+      serializedPub->push_uint32(Sfslite::RSAPub);
       serializedPub->push_uint32(sizeN);
-      serializedPub->push_bytes(nbuf, sizeN);
       serializedPub->push_uint64(pub->k);
+      serializedPub->push_bytes(nbuf, sizeN);
       delete nbuf;
       return serializedPub;
     }
@@ -124,115 +119,185 @@ namespace compile {
     {
       //create a reader copy
       FdbufPtr serializedPub(new Fdbuf(pubBuf));
-      assert(serializedPub->pop_uint32()==RSAPub);
+      assert(serializedPub->pop_uint32()==Sfslite::RSAPub);
       uint32_t sizeN = serializedPub->pop_uint32();
+      uint64_t k = serializedPub->pop_uint64();
       bigint n;
       mpz_set_raw (&n, serializedPub->raw_inline(), sizeN);
-      uint64_t k = serializedPub->pop_uint64();
       
       return new esign_pub(n, k);    
     }
+    
+    FdbufPtr Sfslite::generatePrivKey(int len){
+      esign_priv priv = esign_keygen(len);
+      return serializePriv(&priv);
+    }
 
-      void msg2bigint (bigint *resp, const str &msg, int bits)
-      {
-	assert (bits);
-	bits--;
-	const size_t bytes = bits + 7 >> 3;
-	zcbuf buf (bytes);
-	sha1oracle ora (bytes, 1);
-	ora.update (msg.cstr (), msg.len ());
-	ora.final (reinterpret_cast<u_char *> (buf.base));
-	buf[bytes-1] &= 0xff >> (-bits & 7);
-	mpz_set_rawmag_le (resp, buf, bytes);
-      }
+    FdbufPtr Sfslite::getPubKey(FdbufPtr priv){
+      esign_priv* privKey = deserializePriv(priv);
+      FdbufPtr pubKey = serializePub(privKey);
+      delete privKey;
+      return pubKey;
+    }
+    
+    FdbufPtr Sfslite::generateAESKey(int len){
+      assert(!(len & 15));
+      wmstr wmsg (len);
+      rnd.getbytes (wmsg, len);
+      FdbufPtr key(new Fdbuf(len));
+      key->push_bytes(wmsg.cstr(), len);
+      return key;
+    }
 
-      FdbufPtr secureSignAES(FdbufPtr msg, FdbufPtr key){
-	aes ctx;
-	
-	ctx.setkey (key->raw_inline(), key->length());
-
-	bigint z;
-	str msgStr(msg->cstr(), msg->length());
-	msg2bigint(&z, msgStr, hashSize*8);
-	
-	uint32_t size = mpz_rawsize(&z);
-	//size rounded to 16 bytes
-	uint32_t size16 = (size+6>>4)<<4;
-	char* pbuf = new char[size16];
-	char* cbuf = new char[size16];
-	mpz_get_raw (pbuf, size, &z);
-	// fill the remaining bytes with 0s
-	memset(pbuf+size, 0, size16-size);
-
-	cbcencrypt(&ctx, cbuf, pbuf, size16);
-	FdbufPtr cipher(new Fdbuf(size16));
-	cipher->push_bytes(cbuf, size16);
-	
-	delete []cbuf;
-	delete []pbuf;
-	
-	return cipher;
-      }
+    void msg2bigint (bigint *resp, const str &msg, int bits)
+    {
+      assert (bits);
+      bits--;
+      const size_t bytes = bits + 7 >> 3;
+      zcbuf buf (bytes);
+      sha1oracle ora (bytes, 1);
+      ora.update (msg.cstr (), msg.len ());
+      ora.final (reinterpret_cast<u_char *> (buf.base));
+      buf[bytes-1] &= 0xff >> (-bits & 7);
+      mpz_set_rawmag_le (resp, buf, bytes);
+    }
+    
+    FdbufPtr Sfslite::secureSignAES(FdbufPtr msg, FdbufPtr key){
+      aes ctx;
       
-      FdbufPtr secureSignRSA(FdbufPtr msg, FdbufPtr key){
-	esign_priv* priv = deserializePriv(key);
-
-	bigint msgHash;
-	str msgStr(msg->cstr(), msg->length());
-	bigint m = priv->sign (msgStr);
-
-	uint32_t size = mpz_rawsize(&m);
-	FdbufPtr enc(new Fdbuf(size));
-	char* ebuf = new char[size];
-	mpz_get_raw (ebuf, size, &m);
-	enc->push_bytes(ebuf, size);
-	delete[] ebuf;
-	delete priv;
-	return enc;
-      }
+      ctx.setkey (key->raw_inline(), key->length());
       
-      bool secureVerifyAES(FdbufPtr msg, FdbufPtr key, FdbufPtr proof){	
-	aes ctx;
-	
-	ctx.setkey (key->raw_inline(), key->length());
-
-	bigint msgHash;
-	str msgStr(msg->cstr(), msg->length());
-	msg2bigint(&msgHash, msgStr, hashSize*8);
-
-	uint32_t size = mpz_rawsize(&msgHash);
-	//size rounded to 16 bytes
-	uint32_t size16 = (size+6>>4)<<4;
-	if(proof->length() != size16){
-	  return false;
-	}
-	char* pbuf = new char[size16];
-	char* decbuf = new char[size16];
-	mpz_get_raw (pbuf, size, &msgHash);
-	memset(pbuf+size, 0, size16-size);
-	
-	cbcdecrypt(&ctx, decbuf, proof->raw_inline(), size16);
-	bool res = memcmp(decbuf, pbuf, size16);
-	delete []decbuf;
-	delete []pbuf;
-
-	return res;
-      }
+      bigint z;
+      str msgStr(msg->cstr(), msg->length());
+      msg2bigint(&z, msgStr, hashSize*8);
       
-      bool secureVerifyRSA(FdbufPtr msg, FdbufPtr key, FdbufPtr proofbuf){
-	FdbufPtr proof(new Fdbuf(proofbuf));
-	esign_pub* pub = deserializePub(key);
-
-	bigint msgHash;
-	mpz_set_raw (&msgHash, proof->raw_inline(), proof->length());
-
-	str msgStr(msg->cstr(), msg->length());
-	bool res = pub->verify(msgStr, msgHash);
-
-	delete pub;
-	return res;
+      uint32_t size = mpz_rawsize(&z);
+      //size rounded to 16 bytes
+      uint32_t size16 = (size+6>>4)<<4;
+      char* pbuf = new char[size16];
+      char* cbuf = new char[size16];
+      mpz_get_raw (pbuf, size, &z);
+      // fill the remaining bytes with 0s
+      memset(pbuf+size, 0, size16-size);
+      
+      cbcencrypt(&ctx, cbuf, pbuf, size16);
+      FdbufPtr cipher(new Fdbuf(size16));
+      cipher->push_bytes(cbuf, size16);
+      
+      delete []cbuf;
+      delete []pbuf;
+      
+      return cipher;
+    }
+    
+    FdbufPtr Sfslite::secureSignRSA(FdbufPtr msg, FdbufPtr key){
+      esign_priv* priv = deserializePriv(key);
+      
+      bigint msgHash;
+      str msgStr(msg->cstr(), msg->length());
+      bigint m = priv->sign (msgStr);
+      
+      uint32_t size = mpz_rawsize(&m);
+      FdbufPtr enc(new Fdbuf(size));
+      char* ebuf = new char[size];
+      mpz_get_raw (ebuf, size, &m);
+      enc->push_bytes(ebuf, size);
+      delete[] ebuf;
+      delete priv;
+      return enc;
+    }
+    
+    bool Sfslite::secureVerifyAES(FdbufPtr msg, FdbufPtr key, FdbufPtr proof){	
+      aes ctx;
+      
+      ctx.setkey (key->raw_inline(), key->length());
+      
+      bigint msgHash;
+      str msgStr(msg->cstr(), msg->length());
+      msg2bigint(&msgHash, msgStr, hashSize*8);
+      
+      uint32_t size = mpz_rawsize(&msgHash);
+      //size rounded to 16 bytes
+      uint32_t size16 = (size+6>>4)<<4;
+      if(proof->length() != size16){
+	return false;
       }
+      char* pbuf = new char[size16];
+      char* decbuf = new char[size16];
+      mpz_get_raw (pbuf, size, &msgHash);
+      memset(pbuf+size, 0, size16-size);
+      
+      cbcdecrypt(&ctx, decbuf, proof->raw_inline(), size16);
+      bool res = memcmp(decbuf, pbuf, size16);
+      delete []decbuf;
+      delete []pbuf;
+      
+      return res;
+    }
+    
+    bool Sfslite::secureVerifyRSA(FdbufPtr msg, FdbufPtr key, FdbufPtr proofbuf){
+      FdbufPtr proof(new Fdbuf(proofbuf));
+      esign_pub* pub = deserializePub(key);
+      
+      bigint msgHash;
+      mpz_set_raw (&msgHash, proof->raw_inline(), proof->length());
+      
+      str msgStr(msg->cstr(), msg->length());
+      bool res = pub->verify(msgStr, msgHash);
+      
+      delete pub;
+      return res;
+    }
 
+    bool Sfslite::testAES(){
+     FdbufPtr key = generateAESKey(32);
+     size_t len = rnd.getword ()%256 + 256;
+     wmstr wmsg (len);
+     rnd.getbytes (wmsg, len);
+     FdbufPtr msg(new Fdbuf(len));
+     msg->push_bytes(wmsg.cstr(), len);
+     
+     FdbufPtr encrypted = secureSignAES(msg, key);
+     bool correct = secureVerifyAES(msg, key, encrypted);
+     return correct;
+    }
 
+    bool Sfslite::testRSA(){
+     FdbufPtr privBuf = generatePrivKey(424 + rnd.getword () % 256);
+     size_t len = rnd.getword ()%256 + 256;
+     wmstr wmsg (len);
+     rnd.getbytes (wmsg, len);
+     FdbufPtr msg(new Fdbuf(len));
+     msg->push_bytes(wmsg.cstr(), len);
+     
+     FdbufPtr encrypted = secureSignRSA(msg, privBuf);
+     FdbufPtr pubBuf = getPubKey(privBuf);
+     bool correct = secureVerifyRSA(msg, pubBuf, encrypted);
+     return correct;
+    }
+
+    void Sfslite::testSerialization(bool &res1, std::ostringstream &message1, bool &res2, std::ostringstream &message2){
+     esign_priv priv = esign_keygen (424 + rnd.getword () % 256);
+     FdbufPtr serializedPriv = serializePriv(&priv);
+     FdbufPtr serializedPub = serializePub(&priv);
+     esign_priv *dpriv = deserializePriv(serializedPriv);
+     esign_pub *dpub = deserializePub(serializedPub);
+     
+     message1<< "The original private key was: p: " <<  priv.p.cstr() 
+	     << "q: "<< priv.q.cstr() << "k: "<< priv.k 
+	     << " was reported incorrectly as " << dpriv->p.cstr() 
+	     << "q: "<< dpriv->q.cstr() << "k: "<< dpriv->k;
+     
+     res1 = (dpriv->p == priv.p) && (dpriv->q == priv.q) && (dpriv->k == priv.k);
+
+     message2<< "The original public key was: n: " <<  priv.n.cstr() 
+	     << "k: "<< priv.k 
+	     << " was reported incorrectly as " << dpub->n.cstr() 
+	     << "k: "<< dpub->k;
+     
+     res2 = (dpub->n == priv.n) && (dpub->k == priv.k);
+
+    }
+    
   }
 }
