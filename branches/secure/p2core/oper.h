5ac866713a4af3ade43901da998b51793f0b3375
prince <prince>
2007-10-02 06:34:16 +0000

    reverted the changes in p2core/oper.h

--- branches/secure/p2core/oper.h
+++ branches/secure/p2core/oper.h
@@ -236,11 +236,13 @@ namespace opr {
   public: 
     virtual bool _eq(const ValuePtr& v1, const ValuePtr& v2) const {
       ValuePtr c1 = T::mk(T::cast(v1));
-      return c1->compareTo(v2) == 0;
+      ValuePtr c2 = T::mk(T::cast(v2));
+      return c2->compareTo(c1) == 0;
     };
     virtual bool _neq (const ValuePtr& v1, const ValuePtr& v2) const {
       ValuePtr c1 = T::mk(T::cast(v1));
-      return c1->compareTo(v2) != 0;
+      ValuePtr c2 = T::mk(T::cast(v2));
+      return c2->compareTo(c1) != 0;
     };
     virtual bool _gt (const ValuePtr& v1, const ValuePtr& v2) const {
       ValuePtr c1 = T::mk(T::cast(v1));
