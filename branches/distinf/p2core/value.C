9d8a0664e0f3c55af260a6566a1e25dabb2e38cf
Stanislav Funiak <sfuniak>
2008-02-17 03:31:37 +0000

    fixed serialization for table factors

--- branches/distinf/p2core/value.C
+++ branches/distinf/p2core/value.C
@@ -29,7 +29,7 @@
 #include "val_vector.h"
 #include "val_matrix.h"
 #include "val_gaussian_factor.h"
-//#include "val_table_factor.h"
+#include "val_table_factor.h"
 
 typedef ValuePtr (*_unmarshal_fn)( XDR *);
 
@@ -47,8 +47,8 @@ static _unmarshal_fn jump_tab[] = {
   Val_List::xdr_unmarshal,
   Val_Vector::xdr_unmarshal,
   Val_Matrix::xdr_unmarshal,
-  Val_Gaussian_Factor::xdr_unmarshal
-  /* Val_Table_Factor::xdr_unmarshal */
+  Val_Gaussian_Factor::xdr_unmarshal,
+  Val_Table_Factor::xdr_unmarshal
 };
 
 #include <time.h>
