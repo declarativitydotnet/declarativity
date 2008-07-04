5e3764f17b1e352fceb77603dea5bcf8f1f6bae0
Tyson Condie <tcondie>
2008-07-04 03:40:34 +0000

    update

---
+++ java/p2/src/p2/types/basic/ValueList.java
@@ -0,0 +1,18 @@
+package p2.types.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValueList<T> 
+	extends ArrayList<T> implements Comparable<ValueList> {
+	
+	public int compareTo(ValueList o) {
+		if (o.equals(this)) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
+	}
+
+}
