d099a88326127b25ae0a5159f5b12471f789f680
Joe Hellerstein <joe@trifacta.com>
2008-07-15 19:58:20 +0000

    Index Join

---
+++ lincoln/ruby/lib/types/operator/index_join.rb
@@ -0,0 +1,26 @@
+class IndexJoin < Join
+	
+	def initialize (predicate, input, lookupKey, index)
+		super(predicate, input)
+		@lookupKey = lookupKey
+		@index = index
+	end
+	
+	def to_s
+		return "INDEX JOIN: PREDICATE[" + @predicate.to_s + "]"
+	end
+	
+	def evaluate(tuples)
+		result = TupleSet.new("ij_out", nil)
+		tuples.each do |outer|
+			@index.lookup_kt(@lookupKey, outer).each do |inner| 
+				if validate(outer, inner) then
+					inner.schema = @predicate.schema.clone
+					jointup = outer.join(inner)
+					result << jointup unless jointup.nil?
+				end
+			end
+		end
+		return result
+	end
+end
