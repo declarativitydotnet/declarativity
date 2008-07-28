85ee9de757766fe9430d6775b5ddf90810458d97
Joe Hellerstein <joe@trifacta.com>
2008-07-28 05:12:50 +0000

    forgot this

---
+++ lincoln/ruby/lib/lang/plan/native_expression.rb
@@ -0,0 +1,47 @@
+class NativeExpression < Expression
+
+  def initialize(oper, lhs, rhs)
+    @oper = oper
+    @lhs = lhs
+    @rhs = rhs
+  end
+
+  def expr_type
+    return lhs.class
+  end
+
+  def to_s
+    return "(" + @lhs.to_s + " " + @oper.to_s + " " + @rhs.to_s + ")"
+  end
+
+  def variables
+    variables = Array.new
+    @lhs.variables.each { |v| variables << v }
+    @rhs.variables.each { |v| variables << v}  unless (rhs.nil?) 
+    return variables;
+  end
+
+  def function
+    elam = lambda do |t|
+      lhs = @lhs.function.evaluate(t)
+      rhs = @rhs.function.evaluate(t) unless @rhs.nil?
+      return lhs.send(@oper, rhs) unless @rhs.nil?
+      return nil
+    end
+    
+    tlam = lambda do
+      return expr_type
+    end
+    
+    tmpClass = Class.new(TupleFunction)
+
+    tmpClass.send :define_method, :evaluate do |tuple|
+      return elam.call(tuple)
+    end
+    tmpClass.send :define_method, :returnType do 
+      return tlam 
+    end
+    retval = tmpClass.new
+    return retval
+  end
+end
