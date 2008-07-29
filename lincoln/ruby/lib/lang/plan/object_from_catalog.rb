d2393eedd22b8d7d338aeaeaf782d36521743444
Joe Hellerstein <joe@trifacta.com>
2008-07-29 00:50:36 +0000

    metaprogramming the catalog methods

---
+++ lincoln/ruby/lib/lang/plan/object_from_catalog.rb
@@ -0,0 +1,33 @@
+module ObjectFromCatalog
+
+  def camelize(str)
+    str = str.split('_')
+    retval = str[0]
+    str[1..str.length].each { |s| retval += s.to_s.capitalize }
+    return retval
+  end
+  
+  def constants
+    retval = Array.new
+    classname =self.class.to_s + "::Field"
+    consts = eval(classname).constants
+    consts.each do |c|
+      str = classname + "::" + c
+      retval << [c, eval(str)]
+    end
+    return retval
+  end
+
+  def insert_tup(tuple)
+    # needs to be done through eval since nested Field is class-dependent
+	  object_position = eval self.class.to_s + "::Field::OBJECT"
+    object = tuple.value(object_position)
+    raise UpdateException, "Object nil in catalog tuple" if object.nil?
+    constants.each do |c|
+      method = camelize(c[0].downcase) + "="
+      object.send method.to_sym, tuple.value(c[1]) unless c[0] == 'OBJECT'
+    end
+    return super(tuple)
+  end
+
+end
---
+++ lincoln/ruby/lib/types/exception/planner_exception.rb
@@ -0,0 +1,3 @@
+class PlannerException < StandardError
+
+end
