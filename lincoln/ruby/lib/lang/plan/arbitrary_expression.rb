12eab06cca4795bdaae1f4d1bbdd4ea79512ca1a
Peter Alvaro <palvaro>
2008-08-03 21:40:28 +0000

    clean

--- lincoln/ruby/lib/lang/plan/arbitrary_expression.rb
+++ lincoln/ruby/lib/lang/plan/arbitrary_expression.rb
@@ -2,7 +2,7 @@
 class ArbitraryExpression < Expression
 
   def initialize(expr,variables)
-	@expr = expr
+	@expr = expr.gsub(/\b([A-Z])/,'v\1')
 	@variables = Array.new
 	variables.each do |v|
 		@variables << v
@@ -27,31 +27,17 @@ class ArbitraryExpression < Expression
 
 	subexpr = ''
 	@variables.each do |v|	
-		#print "\tcheck "+v.to_s+"("+v.name+")\n"
 		if t.schema.contains(v) then
-			# uh-oh
-			#print "sub into ("+@expr+") <- "+t.value(v.name).to_s+"\n"
-			#regexp = "([^a-zA-Z0-9])"+v.name
-			#replace = '$1'+t.value(v.name).to_s
-			#@expr = @expr.gsub(regexp,replace)
-			#print "got expr "+@expr+"\n"
-			#print "reg "+regexp+"\n"
-
 			# substitution is stupid: how many times are we gonna parse this thing??
 			# instead, take advantage of rubiismo:
-			subexpr = subexpr + v.name + " = "+t.value(v.name).to_s+"\n"
+			subexpr = subexpr + "v"+v.name + " = "+t.value(v.name).to_s+"\n"
 		else
 			# I guess we have a problem if we have unbound variables...
 			raise
 		end	
-		# now, in rubyland, terms with first characters uppercase are constants.  the opposite is true in overlog.  who cares?
-		# well, because (I assume) of how constants are scoped, if we call evaluate() multiple times on a single object
-		# the interpreter is going to complain that we're redefining constants.		
-		#subexpr = subexpr + @expr
 	end
 
 	subexpr = subexpr + @expr
-	#print "expr is "+subexpr+"\n"
 	return eval(subexpr)
     end
     
