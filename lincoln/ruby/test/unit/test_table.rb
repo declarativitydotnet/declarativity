8fb5cf02418a59201bff022621f90675e7715a19
Peter Alvaro <palvaro>
2008-09-19 02:44:27 +0000

    index delete not working

--- lincoln/ruby/test/unit/test_table.rb
+++ lincoln/ruby/test/unit/test_table.rb
@@ -4,11 +4,12 @@ require "lib/types/table/key"
 require "lib/types/table/catalog"
 require "lib/types/table/hash_index"
 require "lib/types/table/callback"
+require "lib/core/system.rb"
 require "test/unit"
 require "rubygems"
 
 class TestTable < Test::Unit::TestCase
-  def default_test
+  def test_default
     $catalog=nil; $index=nil
     sys = System.new
     sys.init
@@ -31,5 +32,22 @@ class TestTable < Test::Unit::TestCase
     
     assert_raise(RuntimeError){t.insert_tup(Tuple.new(1, 'hi'))}
     assert_raise(RuntimeError){t.delete_tup(Tuple.new(1, 'hi'))}
+  end
+
+  def test_delete
+	tab = BasicTable.new("foo",10,Table::INFINITY,Key.new(0),[Integer,String])
+	tup = Tuple.new(1,"one")
+	tup1 = Tuple.new(2,"two")
+	tup2 = Tuple.new(3,"three")
+	ts = TupleSet.new("bar",tup,tup1,tup2)
+	indx = HashIndex.new(tab,Key.new(1),String)
+	tab.insert(ts,nil)
+	
+	assert_equal(3,tab.tuples.size)
+	tab.clear
+	assert_equal(0,tab.tuples.size)
+	res = indx.lookup(Tuple.new(nil,"two"))
+	assert_equal(0,res.size)
+	
   end
 end
