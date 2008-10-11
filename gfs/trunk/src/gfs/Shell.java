296b6a70da06d331f1fe4e8b30d288fbc312c37b
Neil Conway <neil.conway@gmail.com>
2008-10-11 01:57:49 +0000

    gfs: Checkpoint some more work on the toy GFS client.

--- gfs/trunk/src/gfs/Shell.java
+++ gfs/trunk/src/gfs/Shell.java
@@ -5,7 +5,13 @@ import java.util.List;
 
 import jol.core.Runtime;
 import jol.core.System;
+import jol.types.basic.Tuple;
+import jol.types.basic.TupleSet;
 import jol.types.exception.P2RuntimeException;
+import jol.types.exception.UpdateException;
+import jol.types.table.Table;
+import jol.types.table.TableName;
+import jol.types.table.Table.Callback;
 
 public class Shell {
 	private static System system;
@@ -17,13 +23,14 @@ public class Shell {
      *  (3) inject the appropriate inserts into P2; wait for the results to come back
      *  (4) return results to stdout
      */
-    public static void main(String[] args) throws P2RuntimeException {
+    public static void main(String[] args) throws P2RuntimeException, UpdateException {
         List<String> argList = Arrays.asList(args);
 
         if (argList.size() == 0)
             usage();
 
         system = Runtime.create(123);
+        system.install("gfs", ClassLoader.getSystemResource("nrc_gfs.olg"));
 
         String op = argList.remove(0);
 
@@ -37,19 +44,39 @@ public class Shell {
             usage();
     }
 
-    private static void doConcatenate(List<String> args)
+    private static void doConcatenate(List<String> args) throws UpdateException
     {
         if (args.size() == 0)
             usage();
-        
+
+        TableName tblName = new TableName("gfs", "cat_requests");
+        // Register callback to listen for responses
+        Table responseTbl = system.catalog().table(tblName);
+        responseTbl.register(new Callback(){
+			@Override
+			public void deletion(TupleSet tuples) {
+				;
+			}
+
+			@Override
+			public void insertion(TupleSet tuples) {
+				;
+			}        	
+        });
         /*
          * TODO:
-         * (1) For each file to list, add an entry into the "outgoing concat requests" table
+         * (1) For each file to list, add an entry into the "outgoing requests" table
+         *   => How do we identify an outgoing request, such that it is (a) distinct
+         *      from any other concurrent requests in the system (b) in the callback,
+         *      we can identify which response tuple is for which request
+         *   
          * (2) Register a callback on the "concat responses" table to listen for responses
          */
+        TupleSet reqs = new TupleSet(tblName);
         for (String file : args) {
-        	;
+        	reqs.add(new Tuple(1, file));
         }
+        system.schedule("gfs", tblName, reqs, null);
     }
 
     private static void doCreateDir(List<String> args)
