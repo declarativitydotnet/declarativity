8e4097960c5592f1a8073e1c78246515f72e38e4
Neil Conway <neil.conway@gmail.com>
2008-10-24 06:47:44 +0000

    jol: Checkpoint work on unit tests.

---
+++ lincoln/java/test/jol/test/PathTable.java
@@ -0,0 +1,28 @@
+package jol.test;
+
+import jol.types.basic.TypeList;
+import jol.types.table.Key;
+import jol.types.table.ObjectTable;
+import jol.types.table.TableName;
+
+public class PathTable extends ObjectTable {
+	public static final TableName TABLENAME = new TableName("path", "path");
+	
+	public static final Key PRIMARY_KEY = new Key(0, 1, 2);
+	
+	public enum Field {
+		SOURCE,
+		DESTINATION,
+		HOPS
+	};
+	
+	public static final Class[] SCHEMA = {
+		String.class,	// Source
+		String.class,   // Destination
+		Integer.class   // # of hops from source => dest on this path
+	};
+	
+	public PathTable(Runtime context) {
+		super(context, TABLENAME, PRIMARY_KEY, new TypeList(SCHEMA));
+	}
+}
--- lincoln/java/test/jol/test/PathTest.java
+++ lincoln/java/test/jol/test/PathTest.java
@@ -1,14 +1,19 @@
 package jol.test;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import jol.core.Runtime;
 import jol.core.System;
+import jol.test.PathTable;
+import jol.types.basic.Tuple;
+import jol.types.basic.TupleSet;
 import jol.types.exception.P2RuntimeException;
 import jol.types.exception.UpdateException;
+import jol.types.table.TableName;
+import jol.types.table.Table.Callback;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,7 +21,7 @@ public class PathTest {
     private System sys;
 
     @Before
-    public void setup() throws P2RuntimeException, MalformedURLException {
+    public void setup() throws P2RuntimeException, UpdateException {
         this.sys = Runtime.create(5000);
         URL u = ClassLoader.getSystemResource("jol/test/path.olg");
         this.sys.install("gfs", u);
@@ -31,11 +36,30 @@ public class PathTest {
     public void simplePathTest() throws UpdateException {
         /* Setup the initial links */
         TableName link_tbl = new TableName("path", "link");
-        TupleSet links = new TupleSet(link_tbl);
+        TupleSet links = new TupleSet();
         links.add(new Tuple("1", "2"));
         links.add(new Tuple("2", "3"));
         links.add(new Tuple("3", "4"));
+        
+        Callback cb = new Callback() {
+        	@Override
+        	public void deletion(TupleSet tuples) {
+        	}
+        	
+        	@Override
+        	public void insertion(TupleSet tuples) {
+        		for (Tuple t : tuples)
+        		{
+        			String src = (String) t.value(PathTable.Field.SOURCE.ordinal());
+        			String dest = (String) t.value(PathTable.Field.DESTINATION.ordinal());
+        			Integer hops = (Integer) t.value(PathTable.Field.HOPS.ordinal());
+        			
+        			Assert.assertFalse(src.equals(dest));
+        			Assert.assertEquals(hops.intValue(), 1);
+        		}
+        	}
+        };
 
-        system.schedule("path", link_tbl, links, null);
+        this.sys.schedule("path", link_tbl, links, null);
     }
 }
