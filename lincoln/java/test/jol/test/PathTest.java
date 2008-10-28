3c1256b87968d4134cab463762b2fcbb2684645e
Neil Conway <neil.conway@gmail.com>
2008-10-28 00:50:55 +0000

    jol: Change PathTest to make it less broken for recent Runtime evaluator changes. Still doesn't work, however.

--- lincoln/java/test/jol/test/PathTest.java
+++ lincoln/java/test/jol/test/PathTest.java
@@ -22,27 +22,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PathTest {
-	private static class PathTable extends ObjectTable {
-		public static final TableName TABLENAME = new TableName("path", "path");
-		
-		public static final Key PRIMARY_KEY = new Key(0, 1, 2);
-		
-		public enum Field {
-			SOURCE,
-			DESTINATION,
-			HOPS
-		};
-		
-		public static final Class[] SCHEMA = {
-			String.class,	// Source
-			String.class,   // Destination
-			Integer.class   // # of hops from source => dest on this path
-		};
-		
-		public PathTable(Runtime context) {
-			super(context, TABLENAME, PRIMARY_KEY, new TypeList(SCHEMA));
-		}
-	}
+    private static class PathTable extends ObjectTable {
+        public static final TableName TABLENAME = new TableName("path", "path");
+        
+        public static final Key PRIMARY_KEY = new Key(0, 1, 2);
+        
+        public enum Field {
+            SOURCE,
+            DESTINATION,
+            HOPS
+        };
+        
+        public static final Class[] SCHEMA = {
+            String.class,   // Source
+            String.class,   // Destination
+            Integer.class   // # of hops from source => dest on this path
+        };
+        
+        public PathTable(Runtime context) {
+            super(context, TABLENAME, PRIMARY_KEY, new TypeList(SCHEMA));
+        }
+    }
 
     private System sys;
 
@@ -62,30 +62,30 @@ public class PathTest {
     }
 
     @Test
-    public void simplePathTest() throws UpdateException, InterruptedException {
+        public void simplePathTest() throws UpdateException, InterruptedException, JolRuntimeException {
         /* Arrange to block until the callback tells us we're done */
         final SynchronousQueue<String> queue = new SynchronousQueue<String>();
 
         Callback cb = new Callback() {
             private int count = 0;
 
-        	@Override
-        	public void deletion(TupleSet tuples) {
-        	}
-        	
-        	@Override
-        	public void insertion(TupleSet tuples) {
+            @Override
+            public void deletion(TupleSet tuples) {
+            }
+            
+            @Override
+            public void insertion(TupleSet tuples) {
                 java.lang.System.err.println("insertion() cb invoked");
-        		for (Tuple t : tuples)
-        		{
-        			String src = (String) t.value(PathTable.Field.SOURCE.ordinal());
-        			String dest = (String) t.value(PathTable.Field.DESTINATION.ordinal());
-        			Integer hops = (Integer) t.value(PathTable.Field.HOPS.ordinal());
+                for (Tuple t : tuples)
+                {
+                    String src = (String) t.value(PathTable.Field.SOURCE.ordinal());
+                    String dest = (String) t.value(PathTable.Field.DESTINATION.ordinal());
+                    Integer hops = (Integer) t.value(PathTable.Field.HOPS.ordinal());
 
                     java.lang.System.err.println("Path from " + src + " => " + dest);
-        			Assert.assertFalse(src.equals(dest));
+                    Assert.assertFalse(src.equals(dest));
                     this.count++;
-        		}
+                }
 
                 try {
                     if (this.count == 6)
@@ -93,7 +93,7 @@ public class PathTest {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-        	}
+            }
         };
 
         /* Fire the callback for each change to the "paths" table */
@@ -108,8 +108,16 @@ public class PathTest {
 
         TableName link_name = new TableName("path", "link");
         this.sys.schedule("path", link_name, links, null);
-        
+        this.sys.start();
+
         /* Wait to be notified by the callback */
         queue.take();
+    }
+
+    public static void main(String[] args) throws Exception {
+        PathTest t = new PathTest();
+        t.setup();
+        t.simplePathTest();
+        t.shutdown();
     }
 }
