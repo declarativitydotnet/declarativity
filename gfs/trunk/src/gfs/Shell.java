66d57938a97952366899168621ca28af0eaada80
Neil Conway <neil.conway@gmail.com>
2008-11-11 18:37:53 +0000

    gfs: Unbreak build.

--- gfs/trunk/src/gfs/Shell.java
+++ gfs/trunk/src/gfs/Shell.java
@@ -59,7 +59,6 @@ public class Shell {
         this.responseQueue = new SynchronousQueue();
 
         this.system = Runtime.create(5501);
-        this.system.catalog().register(new MasterRequestTable((Runtime) this.system));
         this.system.catalog().register(new SelfTable((Runtime) this.system));
 
         this.system.install("gfs", ClassLoader.getSystemResource("gfs/gfs.olg"));
