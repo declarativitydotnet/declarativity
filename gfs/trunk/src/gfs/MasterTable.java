53d465e2dcc99f18043deeeb856fa61f46c26b4f
Peter Alvaro <palvaro>
2008-11-14 22:48:47 +0000

    timer

---
+++ gfs/trunk/src/gfs/MasterTable.java
@@ -0,0 +1,27 @@
+package gfs;
+
+import jol.core.Runtime;
+import jol.types.basic.TypeList;
+import jol.types.table.Key;
+import jol.types.table.ObjectTable;
+import jol.types.table.TableName;
+
+public class MasterTable extends ObjectTable {
+	public static final TableName TABLENAME = new TableName("gfs", "master_for_node");
+
+	public static final Key PRIMARY_KEY = new Key(0);
+
+	public enum Field {
+        ADDRESS,
+        MASTER
+	};
+
+	public static final Class<?>[] SCHEMA = {
+        String.class,    // Address of self
+        String.class    // Address of current master
+	};
+
+	MasterTable(Runtime context) {
+		super(context, TABLENAME, PRIMARY_KEY, new TypeList(SCHEMA));
+	}
+}
