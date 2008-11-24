bb84179193288c7cea364290542274778511a439
Neil Conway <neil.conway@gmail.com>
2008-11-24 23:02:09 +0000

    jol: Remove unused import.

--- lincoln/java/src/jol/types/basic/TupleSet.java
+++ lincoln/java/src/jol/types/basic/TupleSet.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
