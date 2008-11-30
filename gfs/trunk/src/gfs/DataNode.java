95a5450837c7a304697099d7bd86a7770d1631d7
Neil Conway <neil.conway@gmail.com>
2008-11-30 00:35:47 +0000

    gfs: Checkpoint some work on the data node.

--- gfs/trunk/src/gfs/DataNode.java
+++ gfs/trunk/src/gfs/DataNode.java
@@ -1,13 +1,18 @@
 package gfs;
 
+import java.io.File;
+
 import jol.core.System;
 import jol.core.Runtime;
+import jol.types.basic.Tuple;
+import jol.types.basic.TupleSet;
 import jol.types.exception.JolRuntimeException;
 import jol.types.exception.UpdateException;
+import jol.types.table.TableName;
 
 public class DataNode {
-    public static void main(String[] args) throws JolRuntimeException,UpdateException {
-        if (args.length != 1)
+    public static void main(String[] args) throws JolRuntimeException, UpdateException {
+        if (args.length != 2)
             usage();
 
         int dataNodeIdx = Integer.parseInt(args[0]);
@@ -16,39 +21,58 @@ public class DataNode {
             usage();
         }
 
-        DataNode dn = new DataNode(dataNodeIdx);
+        DataNode dn = new DataNode(dataNodeIdx, args[1]);
         dn.start();
     }
 
     private static void usage() {
-        java.lang.System.err.println("Usage: gfs.DataNode index");
+        java.lang.System.err.println("Usage: gfs.DataNode index dir_root");
         java.lang.System.err.println("    where 0 <= \"index\" <= " + (Conf.getNumDataNodes() - 1));
         java.lang.System.exit(1);
     }
 
     private int nodeId;
     private int port;
+    private String fsRoot;
     private DataServer dserver;
     private Thread serverThread;
     private System system;
 
-    DataNode(int nodeId) {
+    DataNode(int nodeId, String fsRoot) {
         this.nodeId = nodeId;
+        this.fsRoot = fsRoot;
         this.port = Conf.getDataNodeControlPort(nodeId);
         this.dserver = new DataServer(Conf.getDataNodeDataPort(nodeId));
         this.serverThread = new Thread(this.dserver);
         this.serverThread.start();
     }
 
-    public void start() throws JolRuntimeException,UpdateException {
+    public void start() throws JolRuntimeException, UpdateException {
+        File root = new File(fsRoot);
+        if (root.exists()) {
+            if (!root.isDirectory())
+                throw new RuntimeException("FS root is not a directory: " + root);
+        } else {
+            if (!root.mkdir())
+                throw new RuntimeException("Failed to create directory: " + root);
+            
+            java.lang.System.out.println("Created new root directory: " + root);
+        }
+
         /* Identify the address of the local node */
         Conf.setSelfAddress(Conf.getDataNodeAddress(this.nodeId));
 
         this.system = Runtime.create(this.port);
-
         this.system.install("gfs_global", ClassLoader.getSystemResource("gfs/files.olg"));
+        
+        /* Identify the data directory */
+        TableName tblName = new TableName("gfs", "datadir");
+        TupleSet datadir = new TupleSet(tblName);
+        datadir.add(new Tuple(Conf.getSelfAddress(), fsRoot));
+        this.system.schedule("gfs", tblName, datadir, null);
 
         this.system.start();
+
         java.lang.System.out.println("DataNode @ " + this.port + " (" +
                                      Conf.getDataNodeDataPort(this.nodeId) + ") ready!");
     }
