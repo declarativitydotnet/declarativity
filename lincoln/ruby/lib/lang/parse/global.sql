f7f2e5750f9da33851451cc81f65a6bc1fcf7f76
Joe Hellerstein <joe@trifacta.com>
2008-11-30 15:07:43 +0000

    global.sql

---
+++ lincoln/ruby/lib/lang/parse/global.sql
@@ -0,0 +1,14 @@
+table index (
+  +tablename TableName,
+  +key Key,
+  type TableType,
+  classname String,
+  object String
+)
+
+table operator (
+  program String,
+  rule String,
+  +id String,
+  selectivity Float
+)
