4e728ef2d1880b04a345133ccc0e4ef2b1fab5ce
Neil Conway <neil.conway@gmail.com>
2008-11-30 23:06:10 +0000

    jol: Tweak for JolRuntimeException -- make a variant of the constructor take Throwable, rather than Exception, for consistency with the Exception class itself.

--- lincoln/java/trunk/src/jol/types/exception/JolRuntimeException.java
+++ lincoln/java/trunk/src/jol/types/exception/JolRuntimeException.java
@@ -3,11 +3,11 @@ package jol.types.exception;
 public class JolRuntimeException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	public JolRuntimeException(String error) {
-		super(error);
+	public JolRuntimeException(String message) {
+		super(message);
 	}
 
-	public JolRuntimeException(String error, Exception e) {
-		super(error, e);
+	public JolRuntimeException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
