6c9e9342464074e2a3ba663d94d873d4026a5bd4
Neil Conway <neil.conway@gmail.com>
2008-12-03 21:18:22 +0000

    jol: Whitespace cleanup.

--- lincoln/java/trunk/src/jol/types/table/Index.java
+++ lincoln/java/trunk/src/jol/types/table/Index.java
@@ -11,12 +11,12 @@ import jol.types.exception.UpdateException;
 
 /**
  * The interface to an index structure.
- * 
+ *
  * An index is responsible for looking up tuples by some
  * subset of tuple values identified by the index key {@link Key}.
  */
 public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
-	
+
 	/**
 	 * A listener class that is used to registered with
 	 * the table being indexed. This listener informs the
@@ -38,33 +38,33 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 			}
 		}
 	}
-	
+
 	/**
 	 * The index table stores all indices defined in the Runtime.
 	 */
 	public static class IndexTable extends ObjectTable {
 		/** The runtime context. */
 		private Runtime context;
-		
+
 		/** The name of the index table. */
 		private static final TableName TABLENAME = new TableName(GLOBALSCOPE, "index");
-		
+
 		/** The primary key: <Tablename, Key> */
-		private static final Key PRIMARY_KEY = new Key(0,1); 
-		
+		private static final Key PRIMARY_KEY = new Key(0,1);
+
 		/** The field names. */
 		public enum Field {TABLENAME, KEY, TYPE, CLASSNAME, OBJECT};
-		
+
 		/** The field types. */
-		private static final Class[] SCHEMA = { 
+		private static final Class[] SCHEMA = {
 			TableName.class, // table name
 			Key.class,       // key
 			Type.class,      // the type
 			String.class,    // the class type
 			Index.class      // the object
 		};
-		
-		/** 
+
+		/**
 		 * Create a new index table (should be one per runtime).
 		 * @param context The runtime context.
 		 */
@@ -72,7 +72,7 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 			super(context, TABLENAME, PRIMARY_KEY, new TypeList(SCHEMA));
 			this.context = context;
 		}
-		
+
 		@Override
 		public boolean insert(Tuple tuple) throws UpdateException {
 			if (tuple.value(Field.OBJECT.ordinal()) == null) {
@@ -81,10 +81,10 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 					Constructor constructor = ctype.getConstructor(Table.class, Key.class, Type.class);
 					TableName name  = (TableName) tuple.value(Field.TABLENAME.ordinal());
 					Table table = context.catalog().table(name);
-					
+
 					/* Create the index object. */
 					Key key = (Key) tuple.value(Field.KEY.ordinal());
-					Type type = (Type)tuple.value(Field.TYPE.ordinal());
+					Type type = (Type) tuple.value(Field.TYPE.ordinal());
 					Index index = (Index) constructor.newInstance(table, key, type);
 					tuple.value(Field.OBJECT.ordinal(), index);
 				} catch (Exception e) {
@@ -100,19 +100,19 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 		}
 
 	}
-	
+
 	/** The index type enumeration. */
 	public enum Type{PRIMARY, SECONDARY};
-	
+
 	/** The table over which this index object is defined. */
 	private Table table;
-	
+
 	/** The index key referencing attribute positions in the indexed table. */
 	private Key key;
-	
+
 	/** The type of index. */
 	private Type type;
-	
+
 	/**
 	 * Create a new index. This constuctor will register the index
 	 * with the IndexTable. The index will register a listener with
@@ -142,10 +142,10 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 			this.table.register(new Listener());
 		}
 	}
-	
+
 	@Override
 	public abstract String toString();
-	
+
 	/**
 	 * Compares this index to another index using
 	 * the {@link Index#toString()} method.
@@ -153,15 +153,15 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 	public int compareTo(Index i) {
 		return toString().compareTo(i.toString());
 	}
-	
-	/** 
-	 * The table over which this index is defined. 
+
+	/**
+	 * The table over which this index is defined.
 	 * @return The table object.
 	 */
 	public Table table() {
 		return this.table;
 	}
-	
+
 	/**
 	 * The type of index.
 	 * @return The index type.
@@ -169,7 +169,7 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 	public Type type() {
 		return this.type;
 	}
-	
+
 	/**
 	 * The index key.
 	 * @return The key object.
@@ -177,19 +177,19 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 	public Key key() {
 		return key;
 	}
-	
+
 	/** An iterator over all tuple values. */
 	public abstract Iterator<Tuple> iterator();
-	
-	/** 
+
+	/**
 	 * Uses the index key as the lookup key and
-	 * the values from the given tuple. 
-	 * @param t A tuple to look up 
-	 * (assumed to be a pre-projected key) 
+	 * the values from the given tuple.
+	 * @param t A tuple to look up
+	 * (assumed to be a pre-projected key)
 	 * @return A set containing the lookup result.
 	 */
 	public abstract TupleSet lookupByKey(Tuple t) throws BadKeyException;
-	
+
 	/**
 	 * Quick way to lookup a set of values. The arity of the
 	 * values must match that of the index key.
@@ -198,14 +198,14 @@ public abstract class Index implements Comparable<Index>, Iterable<Tuple> {
 	 * @return A set containing the lookup result.
 	 * @throws BadKeyException If the argument values do not match the key.
 	 */
-	public TupleSet lookupByKey(Comparable... keyValues) throws BadKeyException 
+	public TupleSet lookupByKey(Comparable... keyValues) throws BadKeyException
 	{
 		return lookupByKey(new Tuple(keyValues));
 	}
-	
+
 	/** Index the given tuple. */
 	protected abstract void insert(Tuple t);
-	
+
 	/** Remove the tuple from the index. */
 	protected abstract void remove(Tuple t);
 
