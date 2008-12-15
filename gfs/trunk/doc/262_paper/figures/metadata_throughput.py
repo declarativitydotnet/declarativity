96066f2b79fd158f8bd14018740b173db5e01fca
Neil Conway <neil.conway@gmail.com>
2008-12-15 08:33:00 +0000

    Add matplotlib scripts for generating figures.

---
+++ gfs/trunk/doc/262_paper/figures/metadata_throughput.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(3)
+width = 0.35
+hdfs = (292.97, 304.49, 305.47)
+bfs = (614.57, 706.02, 847.44)
+
+plt.subplot(111)
+hdfs_rects = plt.bar(x, hdfs, width, color='b')
+bfs_rects = plt.bar(x + width, bfs, width, color='g')
+
+plt.xticks(x + width, ('100 ls ops', '100 touch ops', '100 0k copy ops'))
+plt.ylabel('Time (sec)')
+plt.title('Metadata Performance')
+plt.legend((hdfs_rects[0], bfs_rects[0]), ('HDFS', 'BoomFS'), loc='upper right')
+plt.ylim([0, 1000])
+plt.savefig('metadata_throughput.svg')
