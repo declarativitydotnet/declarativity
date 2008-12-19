f73b759e8ab316c3140c274a9b23a29d20ca8c79
Neil Conway <neil.conway@gmail.com>
2008-12-19 01:01:12 +0000

    Tweaks to make the figures more readable without color.
    Former-commit-id: e19b5bf1a05eb5625443633cac23c8335e691405

--- gfs/trunk/doc/262_paper/figures/metadata_throughput.py
+++ gfs/trunk/doc/262_paper/figures/metadata_throughput.py
@@ -9,11 +9,11 @@ bfs = (614.57, 706.02, 847.44)
 
 plt.subplot(111)
 hdfs_rects = plt.bar(x, hdfs, width, color='b')
-bfs_rects = plt.bar(x + width, bfs, width, color='g')
+bfs_rects = plt.bar(x + width, bfs, width, color='g', hatch='/')
 
 plt.xticks(x + width, ('100 ls ops', '100 touch ops', '100 0k copy ops'))
 plt.ylabel('Time (sec)')
 plt.title('Metadata Performance')
 plt.legend((hdfs_rects[0], bfs_rects[0]), ('HDFS', 'BoomFS'), loc='upper right')
-plt.ylim([0, 1000])
+plt.ylim([0, 1200])
 plt.savefig('metadata_throughput.eps')
