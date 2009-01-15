1c091ef8a531df308b172455b70d60731ef1d54f
Russell Sears <sears>
2009-01-15 06:34:34 +0000

    added test of count, with and without the star operator
    Former-commit-id: c6204d8e617a50ae5d50fac0d081f1ee43e6f2fb

---
+++ lincoln/java/trunk/test/jol/test/AggregationTest.java
@@ -0,0 +1,57 @@
+package jol.test;
+
+import jol.types.basic.Tuple;
+import jol.types.basic.TupleSet;
+import jol.types.exception.JolRuntimeException;
+import jol.types.exception.UpdateException;
+import jol.types.table.TableName;
+
+import org.junit.Test;
+
+public class AggregationTest extends LocalTestBase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setup() throws JolRuntimeException, UpdateException {
+		super.setup("jol/test/aggregation.olg");
+	}
+	@Test
+	public void countTest() throws Exception {
+		try {
+			TupleSet set = new TupleSet();
+	        set.add(new Tuple(1,"a"));
+	        set.add(new Tuple(1,"b"));
+	        set.add(new Tuple(2,"c"));
+	        sys.schedule("aggregation", new TableName("aggregation", "counted"), set, null);
+	        sys.evaluate();
+	        int n;
+	        n = 0;
+	        for(Tuple t: sys.catalog().table(new TableName("aggregation", "countStar")).tuples()) {
+	        	n++;
+	        	if(t.value(0).equals(1)) { assert(t.value(1).equals(2)); }
+	        	if(t.value(0).equals(2)) { assert(t.value(1).equals(1)); }
+	        }
+	        assert(n == 2);
+
+	        n = 0;
+	        for(Tuple t: sys.catalog().table(new TableName("aggregation", "countCol")).tuples()) {
+	        	n++;
+	        	if(t.value(0).equals(1)) { assert(t.value(1).equals(2)); }
+	        	if(t.value(0).equals(2)) { assert(t.value(1).equals(1)); }
+	        }
+	        assert(n == 2);
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+}
