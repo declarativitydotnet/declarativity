5a5b22cdfbf97cd3fe471a72f53cc610383f44f9
Neil Conway <neil.conway@gmail.com>
2009-02-01 04:38:29 +0000

    gfs: Code cleanup for tests. Silence some warnings.

--- gfs/trunk/test/gfs/test/GFSMM5Test.java
+++ gfs/trunk/test/gfs/test/GFSMM5Test.java
@@ -1,5 +1,7 @@
 package gfs.test;
 
+import java.util.LinkedList;
+
 import jol.types.basic.ValueList;
 import gfs.Master;
 import gfs.Shell;
@@ -9,9 +11,9 @@ import gfs.test.TestCommon;
 import org.junit.Test;
 
 public class GFSMM5Test extends TestCommon {
-  
+
   private void startOne() throws Exception {
-    this.masters = new ValueList<Master>();
+    this.masters = new LinkedList<Master>();
     Master m = new Master(0); // XXX
     m.start();
     this.masters.add(m);
@@ -19,15 +21,15 @@ public class GFSMM5Test extends TestCommon {
 
   @Test
   public void test4() {
-    try { 
+    try {
       startMany("localhost:5500","localhost:5502","localhost:5503");
-    //startMany("localhost:5500");
+      //startMany("localhost:5500");
       //startOne();
-    
+
       Shell longRun = new Shell();
 
       long agg = 0;
-        int count = 0;
+      int count = 0;
 
       for (int i=0; i < 100; i++) {
         String file = "XACT"+i;
@@ -37,32 +39,28 @@ public class GFSMM5Test extends TestCommon {
         count += 1;
         //Thread.sleep(100);
         //shellCreate(file);
-      }      
+      }
       longRun.shutdown();
 
-  
+
       java.lang.System.out.println("OK, good then\n");
 
-      long avg = agg / count;   
+      long avg = agg / count;
       java.lang.System.out.println("average time to create metadata on "+ Conf.getNumMasters() + " is "+ avg);
-    
+
         int cnt = shellLsCnt();
         java.lang.System.out.println("total files in ls: "+cnt);
 
       assertTrue(shellLs("XACT2","XACT18","XACT16","XACT12","XACT78","XACT59"));
       shutdown();
-
     } catch (Exception e) {
       java.lang.System.out.println("something went wrong: "+e);
       java.lang.System.exit(1);
     }
   }
-  
-
 
   public static void main(String[] args) throws Exception {
     GFSMM5Test t = new GFSMM5Test();
     t.test4();
   }
-
 }
