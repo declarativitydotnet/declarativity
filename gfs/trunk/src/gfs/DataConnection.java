e68c2c4d78373b287de06941b70f877e3caf2d4d
Neil Conway <neil.conway@gmail.com>
2009-02-02 07:01:13 +0000

    gfs: Add quick-and-dirty version of DataConnection class.
    Former-commit-id: f337d25078be5fce4a97d38a2328e38a5d82b479

---
+++ gfs/trunk/src/gfs/DataConnection.java
@@ -0,0 +1,87 @@
+package gfs;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+import java.util.List;
+
+/**
+ * This class represents the client side of a connection to a DataNode. It is
+ * used by both HDFS clients and DataNodes.
+ */
+public class DataConnection {
+    private SocketChannel channel;
+    private Socket socket;
+    private DataOutputStream dos;
+    private String remoteAddr;
+
+    DataConnection(String nodeAddr) {
+        setupStream(nodeAddr);
+    }
+
+    private void setupStream(String addr) {
+        String[] parts = addr.split(":");
+        String host = parts[1];
+        int controlPort = Integer.parseInt(parts[2]);
+        int dataPort = Conf.findDataNodeDataPort(host, controlPort);
+        this.remoteAddr = host + ":" + dataPort;
+
+        try {
+            SocketAddress sockAddr = new InetSocketAddress(host, dataPort);
+            this.channel = SocketChannel.open();
+            this.channel.configureBlocking(true);
+            this.channel.connect(sockAddr);
+            this.socket = this.channel.socket();
+            this.dos = new DataOutputStream(this.socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to setup DataConnection with " +
+                                       this.remoteAddr, e);
+        }
+    }
+
+    void sendRoutingData(int chunkId, List<String> path) {
+        try {
+            this.dos.writeByte(DataProtocol.WRITE_OPERATION);
+            this.dos.writeInt(chunkId);
+
+            int newSize = path.size();
+            if (newSize > Conf.getRepFactor() - 1) {
+                newSize = Conf.getRepFactor() - 1;
+            }
+
+            this.dos.writeInt(newSize);
+            for (int i = 0; i < newSize; i++) {
+                System.out.println("write " + path.get(i));
+                this.dos.writeChars(path.get(i));
+                this.dos.writeChar('|');
+            }
+            this.dos.writeChar(';');
+            // caller writes the actual data
+        } catch (IOException e) {
+            throw new RuntimeException("Exception writing chunk " + chunkId +
+                                       " to " + this.remoteAddr, e);
+        }
+    }
+
+    void sendChunkContent(FileChannel src) {
+        try {
+            src.transferTo(0, Conf.getChunkSize(), this.channel);
+        } catch (IOException e) {
+            throw new RuntimeException("Exception sending chunk content to " +
+                                       this.remoteAddr, e);
+        }
+    }
+
+    void close() {
+        try {
+            this.socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Exception closing data connection to " +
+                                       this.remoteAddr, e);
+        }
+    }
+}
