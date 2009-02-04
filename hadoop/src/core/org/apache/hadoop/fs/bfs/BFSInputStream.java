decf542e9914c6b59cd3f06140ee130ae98f9254
Neil Conway <neil.conway@gmail.com>
2009-02-04 06:42:48 +0000

    hadoop: Add stub implementations for BoomFS hadoop FileSystem implementation.

---
+++ hadoop/src/core/org/apache/hadoop/fs/bfs/BFSInputStream.java
@@ -0,0 +1,30 @@
+package org.apache.hadoop.fs.bfs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.FSInputStream;
+
+public class BFSInputStream extends FSInputStream {
+	@Override
+	public long getPos() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void seek(long pos) throws IOException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean seekToNewSource(long targetPos) throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
