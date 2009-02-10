84e138f80baceda6ea60d0028cf79758bf9c0237
Neil Conway <neil.conway@gmail.com>
2009-02-10 01:42:20 +0000

    bfs: Make BFSFileInfo serializable.

--- bfs/trunk/src/bfs/BFSFileInfo.java
+++ bfs/trunk/src/bfs/BFSFileInfo.java
@@ -1,6 +1,10 @@
 package bfs;
 
-public class BFSFileInfo {
+import java.io.Serializable;
+
+public class BFSFileInfo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private boolean isDirectory;
 	private String fileName;
 
