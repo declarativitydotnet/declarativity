f398ae601705d67848b3e8ad69a348a5783aba9a
Neil Conway <neil.conway@gmail.com>
2009-02-13 04:07:16 +0000

    hadoop: Checkpoint work on BFSInputStream.

--- hadoop/src/core/org/apache/hadoop/fs/bfs/BFSInputStream.java
+++ hadoop/src/core/org/apache/hadoop/fs/bfs/BFSInputStream.java
@@ -12,10 +12,11 @@ import bfs.BFSFileInfo;
 import bfs.Conf;
 
 public class BFSInputStream extends FSInputStream {
-	private String path;
 	private BFSClient bfs;
+	private String path;
 	private List<BFSChunkInfo> chunkList;
 	private boolean isClosed;
+	private boolean atEOF;
 
 	// Byte-wise offset into the logical file contents
 	private long position;
@@ -24,12 +25,14 @@ public class BFSInputStream extends FSInputStream {
 	// These fields are updated by updatePosition().
 	private int currentChunkIdx;
 	private int currentChunkOffset;
+	private BFSChunkInfo currentChunk;
 	private Set<String> chunkLocations;
 
 	public BFSInputStream(String path, BFSClient bfs) throws IOException {
-		this.path = path;
 		this.bfs = bfs;
+		this.path = path;
 		this.isClosed = false;
+		this.atEOF = false;
 		this.chunkList = bfs.getChunkList(path);
 		updatePosition(0);
 	}
@@ -54,9 +57,25 @@ public class BFSInputStream extends FSInputStream {
 		if (chunkNum > chunkList.size())
 			throw new IOException("cannot seek past end of file");
 
-		if (chunkNum != this.currentChunkIdx || this.chunkLocations == null) {
-			BFSChunkInfo chunk = this.chunkList.get(chunkNum);
-			this.chunkLocations = bfs.getChunkLocations(chunk.getId());
+		if (chunkList.isEmpty()) {
+			if (chunkOffset > 0)
+				throw new IOException("cannot seek past end of file");
+
+			this.atEOF = true;
+		} else {
+			if (chunkNum != this.currentChunkIdx || this.chunkLocations == null) {
+				this.currentChunk = this.chunkList.get(chunkNum);
+				this.chunkLocations = bfs.getChunkLocations(this.currentChunk.getId());
+			}
+
+			if (chunkOffset > this.currentChunk.getLength())
+				throw new IOException("cannot seek past end of file");
+
+			if (chunkOffset == this.currentChunk.getLength() &&
+				this.currentChunkIdx == this.chunkList.size())
+				this.atEOF = true;
+			else
+				this.atEOF = false;
 		}
 
 		this.currentChunkIdx = (int) chunkNum;
