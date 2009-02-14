4558b21f98e6c2bc837348fc23cc56d7d8c84585
Neil Conway <neil.conway@gmail.com>
2009-02-14 00:53:07 +0000

    hadoop: Checkpoint initial implementation of BFSInputStream.

--- hadoop/src/core/org/apache/hadoop/fs/bfs/BFSInputStream.java
+++ hadoop/src/core/org/apache/hadoop/fs/bfs/BFSInputStream.java
@@ -1,6 +1,13 @@
 package org.apache.hadoop.fs.bfs;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.util.List;
 import java.util.Set;
 
@@ -10,6 +17,7 @@ import bfs.BFSChunkInfo;
 import bfs.BFSClient;
 import bfs.BFSFileInfo;
 import bfs.Conf;
+import bfs.DataProtocol;
 
 public class BFSInputStream extends FSInputStream {
 	private BFSClient bfs;
@@ -27,6 +35,7 @@ public class BFSInputStream extends FSInputStream {
 	private int currentChunkOffset;
 	private BFSChunkInfo currentChunk;
 	private Set<String> chunkLocations;
+	private ByteBuffer buf;
 
 	public BFSInputStream(String path, BFSClient bfs) throws IOException {
 		this.bfs = bfs;
@@ -34,6 +43,7 @@ public class BFSInputStream extends FSInputStream {
 		this.isClosed = false;
 		this.atEOF = false;
 		this.chunkList = bfs.getChunkList(path);
+		this.buf = ByteBuffer.allocate(Conf.getChunkSize());
 		updatePosition(0);
 	}
 
@@ -64,6 +74,7 @@ public class BFSInputStream extends FSInputStream {
 			if (chunkNum != this.currentChunkIdx || this.currentChunk == null) {
 				this.currentChunk = this.chunkList.get(chunkNum);
 				this.chunkLocations = bfs.getChunkLocations(this.currentChunk.getId());
+				fetchChunkContent();
 			}
 
 			chunkLen = this.currentChunk.getLength();
@@ -82,24 +93,106 @@ public class BFSInputStream extends FSInputStream {
 		this.position = newPos;
 	}
 
+	/**
+	 * Read the content of "currentChunk" from one of the data nodes in
+	 * "chunkLocations".
+	 */
+	private void fetchChunkContent() throws IOException {
+		for (String loc : this.chunkLocations) {
+			try {
+				fetchChunkFromAddr(loc);
+			} catch (IOException e) {
+				continue;
+			}
+
+			// Read chunk successfully
+			return;
+		}
+
+		// Failed to read chunk: all DNs for this chunk are down
+		throw new IOException("failed to read chunk!");
+	}
+
+	private void fetchChunkFromAddr(String addr) throws IOException {
+        String[] parts = addr.split(":");
+        String host = parts[1];
+        int controlPort = Integer.parseInt(parts[2]);
+        int dataPort = Conf.findDataNodeDataPort(host, controlPort);
+
+        SocketAddress sockAddr = new InetSocketAddress(host, dataPort);
+        SocketChannel inChannel = SocketChannel.open();
+        inChannel.configureBlocking(true);
+        inChannel.connect(sockAddr);
+
+        Socket sock = inChannel.socket();
+        DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+        DataInputStream dis = new DataInputStream(sock.getInputStream());
+
+        dos.writeByte(DataProtocol.READ_OPERATION);
+        dos.writeInt(this.currentChunk.getId());
+        int length = dis.readInt();
+        if (length != this.currentChunk.getLength())
+        	throw new RuntimeException("expected length " +
+        			                   this.currentChunk.getLength() +
+        			                   ", data node copy's length is " +
+        			                   length + ", chunk " +
+        			                   this.currentChunk.getId());
+
+        this.buf.clear();
+        this.buf.limit(length);
+        while (this.buf.hasRemaining()) {
+        	int nread = inChannel.read(this.buf);
+        	if (nread == -1)
+        		throw new IOException("Unexpected EOF from data node");
+        }
+
+        sock.close();
+
+        // We successfully read a chunk, so prepare the buffer to
+        // be read from
+        this.buf.rewind();
+	}
+
 	@Override
 	public boolean seekToNewSource(long targetPos) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
+		// This is a somewhat fraudulent implementation of this API,
+		// but it should be sufficient
+		updatePosition(targetPos);
+		return true;
 	}
 
 	@Override
 	public int read() throws IOException {
-		if (this.isClosed)
-			throw new IOException("cannot read from closed file");
-		// TODO Auto-generated method stub
-		return 0;
+		byte[] tmpBuf = new byte[1];
+		int result = read(tmpBuf, 0, 1);
+		if (result == -1)
+			return result;
+		else
+			return (int) tmpBuf[0];
 	}
 
 	@Override
-	public int read(byte[] buf, int offset, int len) throws IOException {
-		// TODO Auto-generated method stub
-		return -1;
+	public int read(byte[] clientBuf, int offset, int len) throws IOException {
+		if (this.isClosed)
+			throw new IOException("cannot read from closed file");
+
+		if (this.atEOF)
+			return -1;
+
+		// If we get here, there must be some data left to read in
+		// the current chunk
+		if (!this.buf.hasRemaining())
+			throw new IllegalStateException();
+
+		// If there's not enough data in the buffer to completely satisfy
+		// the request, just read the remaining data. We then switch
+		// to the next chunk in time for the next read() call.
+		if (len > this.buf.remaining())
+			len = this.buf.remaining();
+
+		this.buf.get(clientBuf, offset, len);
+		updatePosition(this.position + len);
+		return len;
 	}
 
 	@Override
