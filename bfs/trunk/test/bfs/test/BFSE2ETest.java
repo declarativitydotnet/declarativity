540316bdfe93826894e0c0923061f56591a52f8d
Neil Conway <neil.conway@gmail.com>
2009-02-17 05:42:46 +0000

    bfs: Code cleanup.
    Former-commit-id: 66b856f85845bb5aac3a89b5f889d7752d1bb114

--- bfs/trunk/test/bfs/test/BFSE2ETest.java
+++ bfs/trunk/test/bfs/test/BFSE2ETest.java
@@ -5,16 +5,12 @@ import bfs.test.TestCommon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.DataOutputStream;
-import java.lang.Runnable;
-import java.util.List;
-import java.util.ArrayList;
 import java.io.IOException;
 
 import org.junit.Test;
 
 public class BFSE2ETest extends TestCommon {
-
-    private final String EXEC = "java -cp lib/jol.jar:dist/bfs.jar ";
+    private final static String EXEC = "java -cp lib/jol.jar:dist/bfs.jar ";
 
     @Test(timeout=12000)
     public void test1() throws Exception {
@@ -28,45 +24,41 @@ public class BFSE2ETest extends TestCommon {
         shutdown();
     }
 
-    
-
     public void appendCommand(String str) throws IOException {
         Process p = Runtime.getRuntime().exec(str);
         DataOutputStream dos = new DataOutputStream(p.getOutputStream());
         for (int i = 0; i < 10000; i++) {
             dos.writeChars("foobar\n");
         }
-        dos.close(); 
-
+        dos.close();
     }
+
     public void execCommand(String str) throws IOException, InterruptedException {
         Process p = Runtime.getRuntime().exec(str);
 
         p.waitFor();
         int eval = p.exitValue();
-        p.destroy(); 
+        p.destroy();
         safeAssert("command succeeds", eval == 0);
     }
 
     public void checkRead(String str) throws IOException, InterruptedException {
-
         Process p = Runtime.getRuntime().exec(str);
-
         BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line = "";
+        String line;
         int cnt = 0;
         while ((line=buf.readLine()) != null) {
-            // can't do this test: we are printing out crap to stdout... 
+            // can't do this test: we are printing out crap to stdout...
             //safeAssert("foobar != *"+line+"*", line == "foobar\n");
             cnt++;
-        } 
+        }
         // ditto for this one.
         //safeAssert("sought 10K foobars, found "+cnt, cnt == 10000);
         // we'll do a less strict one:
         safeAssert("sought >= 10K foobars, found "+cnt, cnt >= 10000);
         p.waitFor();
         buf.close();
-        p.destroy(); 
+        p.destroy();
     }
 
     public static void main(String[] args) throws Exception {
