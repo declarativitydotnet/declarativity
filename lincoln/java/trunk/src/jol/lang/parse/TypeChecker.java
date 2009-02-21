363270b22ac4ad9dc52543951d898909a1920c36
Russell Sears <sears>
2009-02-21 00:18:33 +0000

    workaround broken JavaHashtable

--- lincoln/java/trunk/src/jol/lang/parse/TypeChecker.java
+++ lincoln/java/trunk/src/jol/lang/parse/TypeChecker.java
@@ -423,7 +423,8 @@ public final class TypeChecker extends Visitor {
 				if(StasisTable.foundStasis) {
 					create = new LinearHashNTA(context, name, key, schema);
 				} else {
-					create = new JavaHashtable(context, name, key, schema);
+					//create = new JavaHashtable(context, name, key, schema);
+					create = new BasicTable(context, name, key, schema);
 				}
 			} catch (UpdateException e) {
 				// TODO Auto-generated catch block
