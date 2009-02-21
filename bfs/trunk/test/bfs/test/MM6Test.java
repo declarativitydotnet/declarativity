7fcaf442cc03f17ebd4e641bb8f40f205dee552c
Neil Conway <neil.conway@gmail.com>
2009-02-21 00:31:53 +0000

    bfs: Comment tweak.

--- bfs/trunk/test/bfs/test/MM6Test.java
+++ bfs/trunk/test/bfs/test/MM6Test.java
@@ -5,27 +5,28 @@ import bfs.test.TestCommon;
 import org.junit.Test;
 
 public class MM6Test extends TestCommon {
-    @Test(timeout=120000)
-    public void test() throws Exception {
-        startMany("localhost:5500", "localhost:5502", "localhost:5503");
+	@Test(timeout = 120000)
+	public void test() throws Exception {
+		startMany("localhost:5500", "localhost:5502", "localhost:5503");
 
-        shellCreate("/foo");
-        shellRm("/foo");
-        safeAssert("foo found??", !shellLs("/", "foo"));
+		shellCreate("/foo");
+		shellRm("/foo");
+		safeAssert("foo found??", !shellLs("/", "foo"));
 
-        /* kill the primary */
-        killMaster(0);
+		/* kill the primary */
+		killMaster(0);
 
-        /* we need to ensure that the deletion performed on the primary master
-           is reflected on the secondary after a master failure
-        */
-        safeAssert("foo found2??", !shellLs("/", "foo"));
+		/*
+		 * Check that the deletion performed on the primary master is reflected
+		 * on the secondary after a master failure
+		 */
+		safeAssert("foo found2??", !shellLs("/", "foo"));
 
-        shutdown();
-    }
+		shutdown();
+	}
 
-    public static void main(String[] args) throws Exception {
-        MM6Test t = new MM6Test();
-        t.test();
-    }
+	public static void main(String[] args) throws Exception {
+		MM6Test t = new MM6Test();
+		t.test();
+	}
 }
