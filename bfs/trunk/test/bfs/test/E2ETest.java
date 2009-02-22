11e3869574f8339974bb2db3539dd14b0949dde3
Neil Conway <neil.conway@gmail.com>
2009-02-22 05:06:28 +0000

    bfs: Rewrite E2ETest to avoid forking processes. It now passes on my machine.
    Former-commit-id: 0e3bbc3cda67f39ee12d6f4f25d58147c371f631

--- bfs/trunk/test/bfs/test/E2ETest.java
+++ bfs/trunk/test/bfs/test/E2ETest.java
@@ -1,65 +1,115 @@
 package bfs.test;
 
-import bfs.test.TestCommon;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import org.junit.Test;
 
+import bfs.BFSChunkInfo;
+import bfs.BFSClient;
+import bfs.BFSNewChunkInfo;
+import bfs.Conf;
+import bfs.DataConnection;
+import bfs.DataProtocol;
+
 public class E2ETest extends TestCommon {
-    private final static String EXEC = "java -cp lib/jol.jar:dist/bfs.jar ";
+	private final static String TEST_FILENAME = "/e2e_f1";
+	private final static int NUM_TEST_CHUNKS = 6;
 
-    @Test(timeout=12000)
-    public void test1() throws Exception {
-        startMany("localhost:5505");
-        startManyDataNodes("td1", "td2");
+	/* XXX: refactor this to avoid duplicated code */
+	@Test(timeout=12000)
+	public void test1() throws Exception {
+		startMany("localhost:5505");
+		startManyDataNodes("td1", "td2");
 
-        execCommand(EXEC + "bfs.Shell create /peter");
-        appendCommand(EXEC + "bfs.Shell append /peter");
-        checkRead(EXEC + "bfs.Shell read /peter");
+		BFSClient bfs = new BFSClient(10001);
+		safeAssert(bfs.createFile(TEST_FILENAME));
 
-        shutdown();
-    }
+		byte[] testData = new byte[NUM_TEST_CHUNKS * Conf.getChunkSize()];
+		Random r = new Random();
+		r.nextBytes(testData);
 
-    public void appendCommand(String str) throws IOException {
-        Process p = Runtime.getRuntime().exec(str);
-        DataOutputStream dos = new DataOutputStream(p.getOutputStream());
-        for (int i = 0; i < 10000; i++) {
-            dos.writeChars("foobar\n");
+		for (int i = 0; i < NUM_TEST_CHUNKS; i++) {
+			BFSNewChunkInfo newChunk = bfs.getNewChunk(TEST_FILENAME);
+
+			DataConnection dc = new DataConnection(newChunk.getCandidateNodes());
+			dc.sendRoutingData(newChunk.getChunkId());
+			dc.write(testData, i * Conf.getChunkSize(), Conf.getChunkSize());
+			dc.close();
+		}
+
+		/* Pause to let the DNs send HBs back to the master */
+		Thread.sleep(4000);
+
+		List<BFSChunkInfo> chunkList = bfs.getChunkList(TEST_FILENAME);
+		safeAssert(chunkList.size() == NUM_TEST_CHUNKS);
+		int i = 0;
+		byte[] readBuf = new byte[testData.length];
+		for (BFSChunkInfo chunk : chunkList) {
+			safeAssert(chunk.getLength() == Conf.getChunkSize());
+
+			Set<String> locs = bfs.getChunkLocations(chunk.getId());
+			for (String addr : locs) {
+				fetchChunkFromAddr(chunk, addr, readBuf, i * Conf.getChunkSize());
+				break;
+			}
+			i++;
+		}
+
+		for (int j = 0; j < testData.length; j++) {
+			if (testData[j] != readBuf[j])
+				throw new RuntimeException("Read buffer differs at offset: " + j);
+		}
+
+		shutdown();
+	}
+
+	private void fetchChunkFromAddr(BFSChunkInfo chunk, String addr, byte[] outBuf, int outOffset) throws IOException {
+        String[] parts = addr.split(":");
+        String host = parts[1];
+        int controlPort = Integer.parseInt(parts[2]);
+        int dataPort = Conf.findDataNodeDataPort(host, controlPort);
+
+        SocketAddress sockAddr = new InetSocketAddress(host, dataPort);
+        SocketChannel inChannel = SocketChannel.open();
+        inChannel.configureBlocking(true);
+        inChannel.connect(sockAddr);
+
+        Socket sock = inChannel.socket();
+        DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+        DataInputStream dis = new DataInputStream(sock.getInputStream());
+
+        dos.writeByte(DataProtocol.READ_OPERATION);
+        dos.writeInt(chunk.getId());
+        int length = dis.readInt();
+        if (length != chunk.getLength())
+        	throw new RuntimeException("expected length " +
+        			                   chunk.getLength() +
+        			                   ", data node copy's length is " +
+        			                   length + ", chunk " +
+        			                   chunk.getId());
+
+        ByteBuffer buf = ByteBuffer.allocate(length);
+        while (buf.hasRemaining()) {
+        	int nread = inChannel.read(buf);
+        	if (nread == -1)
+        		throw new IOException("Unexpected EOF from data node");
         }
-        dos.close();
-    }
 
-    public void execCommand(String str) throws IOException, InterruptedException {
-        Process p = Runtime.getRuntime().exec(str);
-
-        p.waitFor();
-        int eval = p.exitValue();
-        p.destroy();
-        //safeAssert("command succeeds", eval == 0);
-    }
-
-    public void checkRead(String str) throws IOException, InterruptedException {
-        Process p = Runtime.getRuntime().exec(str);
-        BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line;
-        int cnt = 0;
-        while ((line=buf.readLine()) != null) {
-            // can't do this test: we are printing out crap to stdout...
-            //safeAssert("foobar != *"+line+"*", line == "foobar\n");
-            cnt++;
-        }
-        // ditto for this one.
-        //safeAssert("sought 10K foobars, found "+cnt, cnt == 10000);
-        // we'll do a less strict one:
-        safeAssert("sought >= 10K foobars, found "+cnt, cnt >= 10000);
-        p.waitFor();
-        buf.close();
-        p.destroy();
-    }
+        sock.close();
+        buf.flip();
+        buf.get(outBuf, outOffset, length);
+	}
 
     public static void main(String[] args) throws Exception {
         E2ETest t = new E2ETest();
