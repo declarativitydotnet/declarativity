4eed9625062cce7e0f8258ee8959624ca58f5f60
Neil Conway <neil.conway@gmail.com>
2009-02-26 03:06:47 +0000

    jol: Whitespace tweak.

--- lincoln/java/trunk/src/jol/types/table/Table.java
+++ lincoln/java/trunk/src/jol/types/table/Table.java
@@ -14,19 +14,19 @@ import jol.types.table.Index.IndexTable;
 
 /**
  * The interface that all table types must extend.
- * 
+ *
  * A table is responsible for the storage of tuples
  * with a specific schema. Tables provide insertion
  * and deletion methods that are called by the
  * query processor. Insertion and deletion calls
  * store or remove the passed in tuples and return
- * those tuples that actually modified the state of 
+ * those tuples that actually modified the state of
  * the table as delta sets.
  */
 public abstract class Table implements Comparable<Table> {
 	/** The name of the global scope. */
 	public static final String  GLOBALSCOPE = "global";
-	
+
 	/**
 	 * Listeners can be established on tables that are
 	 * called whenever a set of tuples has been inserted
@@ -37,47 +37,47 @@ public abstract class Table implements Comparable<Table> {
 	public static abstract class Callback {
 		/** Callback identifier. */
 		private static long ids = 0L;
-		
+
 		private Long id = new Long(ids++);
 		@Override
 		public int hashCode() { return id.hashCode(); }
-		
+
 		/** The set of tuples that were inserted into the table. */
 		public abstract void insertion(TupleSet tuples);
-		
+
 		/** The set of tuples that were deleted from the table. */
 		public abstract void deletion(TupleSet tuples);
 	}
-	
+
 
 	/**
 	 * The system catalog.
 	 * This is a special table that stores references to all
 	 * other tables (including itself) created by the runtime
-	 * and the various user programs. 
+	 * and the various user programs.
 	 */
 	public static class Catalog extends ObjectTable {
 		/** The runtime context. */
 		private Runtime context;
-		
+
 		/** The index table. */
 		private IndexTable index;
-		
+
 		/** The primary key: The name of the table. */
 		private static final Key PRIMARY_KEY = new Key(0);
 
 		/** The fields that make up a table tuple. */
 		public enum Field {TABLENAME, TYPE, KEY, TYPES, OBJECT};
-		
+
 		/** The type of each field in a table tuple. */
-		private static final Class[] SCHEMA = { 
+		private static final Class[] SCHEMA = {
 			TableName.class, // Name
 			String.class,    // Table type
 			Key.class,       // The primary key
-			Class[].class,  // The type of each attribute
+			Class[].class,   // The type of each attribute
 			Table.class      // The table object
 		};
-		
+
 		/**
 		 * Create a new catalog.
 		 * @param context The runtime context.
@@ -87,7 +87,7 @@ public abstract class Table implements Comparable<Table> {
 			this.context = context;
 			this.index = null;
 		}
-		
+
 		@Override
 		protected boolean insert(Tuple tuple) throws UpdateException {
 			Table table = (Table) tuple.value(Field.OBJECT.ordinal());
@@ -104,13 +104,13 @@ public abstract class Table implements Comparable<Table> {
 					throw new UpdateException("Don't know how to create table type " + type);
 				}
 			}
-			
+
 			Object[] values = tuple.toArray();
 			values[Field.OBJECT.ordinal()] = table;
 			tuple = new Tuple(values);
 			return super.insert(tuple);
 		}
-		
+
 		/**
 		 * The index table.
 		 * @return The index table.
@@ -118,7 +118,7 @@ public abstract class Table implements Comparable<Table> {
 		IndexTable index() {
 			return this.index;
 		}
-		
+
 		/**
 		 * Set the index table.
 		 * @param index The index table.
@@ -126,7 +126,7 @@ public abstract class Table implements Comparable<Table> {
 		void index(IndexTable index) {
 			this.index = index;
 		}
-		
+
 		/**
 		 * Drop the table with the given name.
 		 * @param name The name of the table.
@@ -137,7 +137,7 @@ public abstract class Table implements Comparable<Table> {
 			try {
 				TupleSet tuples = primary().lookupByKey(name);
 				return delete(tuples).size() > 0;
-					
+
 			} catch (BadKeyException e) {
 				e.printStackTrace();
 				System.exit(0);
@@ -153,7 +153,7 @@ public abstract class Table implements Comparable<Table> {
 		public Table table(TableName name) {
 			try {
 				TupleSet table = primary().lookupByKey(name);
-				
+
 				if (table == null) {
 					return null;
 				}
@@ -170,7 +170,7 @@ public abstract class Table implements Comparable<Table> {
 			}
 			return null;
 		}
-		
+
 		/**
 		 * Register the given table with the catalog.
 		 * @param table The table to be registered.
@@ -188,36 +188,36 @@ public abstract class Table implements Comparable<Table> {
 			return false;
 		}
 	}
-	
-	/** The table type enumeration. 
+
+	/** The table type enumeration.
 	 * TABLE: A materialized table (stores tuples)
 	 * EVENT: Does not store the tuples inserted.
 	 * FUNCTION: A table function. (no tuple storage)
 	 * TIMER: Represents a wall-clock timer.
 	 */
 	public enum Type{TABLE, EVENT, FUNCTION, TIMER};
-	
+
 	/** The table type. */
 	protected Type type;
-	
+
 	/** The table name. */
 	protected TableName name;
-	
+
 	/** The type of each attribute in this table. */
 	protected Class[] attributeTypes;
-	
+
 	/** The primary for this table. */
 	protected Key key;
-	
+
 	/** The set of callbacks registered on this table. */
 	protected final Set<Callback> callbacks;
-	
+
 	/**
 	 * Constructs a new table object. (no catalog registration)
 	 * @param name The name of the table.
 	 * @param type The table type.
-	 * @param key The primary key of this table. 
-	 * @param attributeTypes The type of each attribute. 
+	 * @param key The primary key of this table.
+	 * @param attributeTypes The type of each attribute.
 	 */
 	protected Table(TableName name, Type type, Key key, Class[] attributeTypes) {
 		this.name = name;
@@ -226,7 +226,7 @@ public abstract class Table implements Comparable<Table> {
 		this.key = key;
 		this.callbacks = new HashSet<Callback>();
 	}
-	
+
 	/**
 	 * Called to initialize a new Catalog under the given runtime context.
 	 * @param context The runtime context.
@@ -239,21 +239,21 @@ public abstract class Table implements Comparable<Table> {
 		catalog.register(new OperatorTable(context));
 		return catalog;
 	}
-	
-	
+
+
 	@Override
 	public String toString() {
-		String value = name.toString() + ", " + attributeTypes.toString() + 
+		String value = name.toString() + ", " + attributeTypes.toString() +
 		               ", keys(" + key + "), {" + attributeTypes.toString() + "}";
 		return value;
 	}
-	
+
 	/**
 	 * The number of tuples stored in this table.
 	 * @return Number of stored tuples.
 	 */
 	public abstract Long cardinality();
-	
+
 	/**
 	 * The table type.
 	 * @return The table type.
@@ -266,7 +266,7 @@ public abstract class Table implements Comparable<Table> {
 	 * Register a new callback on table updates. Note that this is synchronized
 	 * because it might be invoked by a thread in the client program
 	 * concurrently with callbacks being invoked by JOL.
-	 * 
+	 *
 	 * @param callback The callback to register.
 	 */
 	public synchronized void register(Callback callback) {
@@ -277,40 +277,40 @@ public abstract class Table implements Comparable<Table> {
 	 * Unregister the given callback. Note that this is synchronized because it
 	 * might be invoked by a thread in the client program concurrently with
 	 * callbacks being invoked by JOL.
-	 * 
+	 *
 	 * @param callback The callback to unregister.
 	 */
 	public synchronized void unregister(Callback callback) {
 		this.callbacks.remove(callback);
 	}
-	
+
 	/**
 	 * Compare this table name to the other.
 	 */
 	public int compareTo(Table o) {
 		return name().compareTo(o.name());
 	}
-	
+
 	/**
 	 * Get the table name.
 	 * @return The name of this table. */
 	public TableName name() {
 		return this.name;
 	}
-	
+
 	/**
-	 * Get the type of each attribute (in schema order). 
+	 * Get the type of each attribute (in schema order).
 	 * @return The number of attributes associated with this table. */
 	public Class[] types() {
 		return attributeTypes;
 	}
-	
+
 	/**
 	 * The set of stored tuples.
 	 * @return A reference to the set of stored tuples.
 	 */
 	public abstract Iterable<Tuple> tuples();
-	
+
 	/**
 	 * The primary key.
 	 * @return The primary key object.
@@ -318,13 +318,13 @@ public abstract class Table implements Comparable<Table> {
 	public Key key() {
 		return this.key;
 	}
-	
+
 	/**
 	 * The primary index.
 	 * @return The primary index.
 	 */
 	public abstract Index primary();
-	
+
 	/**
 	 * A map containing all defined secondary indices.
 	 * @return All defined secondary indices.
@@ -344,7 +344,7 @@ public abstract class Table implements Comparable<Table> {
 		insert(insertion, conflicts);
 		delete(conflicts);
 	}
-	
+
 	/**
 	 * Insert a set of tuples into this table. Primary key conflicts
 	 * are not removed from the table but rather returned in the
@@ -352,13 +352,13 @@ public abstract class Table implements Comparable<Table> {
 	 * @param tuples The tuples to be inserted.
 	 * @param conflicts Any primary key conflicts that arise.
 	 * @return The delta set of tuples.
-	 * @throws UpdateException 
+	 * @throws UpdateException
 	 **/
 	public TupleSet insert(TupleSet tuples, TupleSet conflicts) throws UpdateException {
 		TupleSet delta = new TupleSet(name());
 		for (Tuple t : tuples) {
 			t = t.clone();
-			
+
 			Set<Tuple> oldvals = null;
 			try {
 				if (conflicts != null && primary() != null) {
@@ -375,7 +375,7 @@ public abstract class Table implements Comparable<Table> {
 				if (conflicts != null && primary() != null) {
 					conflicts.addAll(oldvals);
 				}
-				
+
 				/* Update the indices here so that primary key
 				 * conflicts from within the tupleset will show up
 				 * during this method call.  */
@@ -390,22 +390,22 @@ public abstract class Table implements Comparable<Table> {
 		}
 		return delta;
 	}
-	
+
 	/**
-	 * Remove the set of tuples from the table. 
-	 * @return The delta set of the delete tuples 
+	 * Remove the set of tuples from the table.
+	 * @return The delta set of the delete tuples
 	 * (a.k.a. the tuples that were actually deleted by this operation.)
-	 * @throws UpdateException 
+	 * @throws UpdateException
 	 **/
 	public TupleSet delete(Iterable<Tuple> tuples) throws UpdateException {
 		TupleSet delta = new TupleSet(name());
-		
+
 		for (Tuple t : tuples) {
 			if (delete(t)) {
 				delta.add(t);
 			}
 		}
-		
+
 		if (!delta.isEmpty()) {
 		    synchronized (this) {
 		        for (Callback callback : this.callbacks) {
@@ -413,7 +413,7 @@ public abstract class Table implements Comparable<Table> {
 		        }
 		    }
 		}
-		
+
 		return delta;
 	}
 
@@ -424,12 +424,12 @@ public abstract class Table implements Comparable<Table> {
 	 * @exception UpdateException Any problems that occurred.
 	 */
 	protected abstract boolean insert(Tuple t) throws UpdateException;
-	
+
 	/**
 	 * Signal to table below to actually perform the deletion.
 	 * @param t Tuple to be deleted.
 	 * @return true if the deletion occurred, false otherwise.
-	 * @throws UpdateException 
+	 * @throws UpdateException
 	 */
 	protected abstract boolean delete(Tuple t) throws UpdateException;
 }
