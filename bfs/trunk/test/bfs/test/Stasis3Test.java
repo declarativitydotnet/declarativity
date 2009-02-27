cf284b08896085c08f114260063c2b4d34946f32
Neil Conway <neil.conway@gmail.com>
2009-02-27 00:44:54 +0000

    bfs: Remove a duplicated test, rename some tests.
    Former-commit-id: af45f3f46595b6f7badf4ab734d2041e3234615f

--- bfs/trunk/test/bfs/test/Stasis3Test.java
+++ bfs/trunk/test/bfs/test/Stasis3Test.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import org.junit.Test;
 
-public class BFSStasis2Test extends TestCommon {
+public class Stasis3Test extends TestCommon {
 	/**
 	 * Test multi-master stasis setups w/ shutdown, but no catch-up
 	 * @throws Exception
@@ -18,24 +18,24 @@ public class BFSStasis2Test extends TestCommon {
 
         shellCreate("/foo");
         assertTrue(shellLs("/", "foo"));
-        
+
         // XXX Do not call shutdown(); not handling data nodes yet.
 	    this.killMaster(0);
 	    this.killMaster(1);
 	    this.killMaster(2);
-        
+
         System.out.println("--------------------------------------------------------------------------------------------------------\n");
 
         startMany("localhost:5509", "localhost:5510", "localhost:5511");
 
 	    Thread.sleep(1000);
-	    
+
 	    assertTrue(shellLs("/", "foo"));
 
 	    this.killMaster(0);
 
 	    shellCreate("/bar");
-	    
+
 	    assertTrue(shellLs("/", "foo"));
 	    assertTrue(shellLs("/", "bar"));
 
@@ -44,12 +44,27 @@ public class BFSStasis2Test extends TestCommon {
 	    assertTrue(shellLs("/", "foo"));
 	    assertTrue(shellLs("/", "bar"));
 
-	    
+	    this.killMaster(2);
+
+	    startMany("localhost:5509", "localhost:5510", "localhost:5511");
+	    shellCreate("/baz"); // hopefully this will kick over to the master, and block until it's done catching up.
+
+	    assertTrue(shellLs("/", "foo"));
+	    assertTrue(shellLs("/", "bar"));
+	    assertTrue(shellLs("/", "baz"));
+
+	    this.killMaster(2);
+	    this.killMaster(1);
+
+	    assertTrue(shellLs("/", "foo"));
+	    assertTrue(shellLs("/", "bar"));
+	    assertTrue(shellLs("/", "baz"));
+
 	    shutdown();
     }
 
     public static void main(String[] args) throws Exception {
-        BFSStasis2Test t = new BFSStasis2Test();
+        Stasis3Test t = new Stasis3Test();
         t.test1();
     }
 }
