64ee5addc94c8af86deab54d69cb1d0892ae48e9
Neil Conway <neil.conway@gmail.com>
2009-03-01 00:15:46 +0000

    hadoop: Checkpoint work on scripts.

--- hadoop/exec/hadoop-daemon.sh
+++ hadoop/exec/hadoop-daemon.sh
@@ -115,12 +115,65 @@ case $startStop in
     echo starting $command, logging to $log
     cd "$HADOOP_HOME"
     if [[ ("$command" = "bfsmaster") || ("$command" = "bfsdatanode") ]]; then
+      # CLASSPATH initially contains $HADOOP_CONF_DIR
+      CLASSPATH="${HADOOP_CONF_DIR}"
+      CLASSPATH=${CLASSPATH}:$JAVA_HOME/lib/tools.jar
+
+      # for developers, add Hadoop classes to CLASSPATH
+      if [ -d "$HADOOP_HOME/build/classes" ]; then
+        CLASSPATH=${CLASSPATH}:$HADOOP_HOME/build/classes
+      fi
+      if [ -d "$HADOOP_HOME/build/webapps" ]; then
+        CLASSPATH=${CLASSPATH}:$HADOOP_HOME/build
+      fi
+      if [ -d "$HADOOP_HOME/build/test/classes" ]; then
+        CLASSPATH=${CLASSPATH}:$HADOOP_HOME/build/test/classes
+      fi
+
+      # so that filenames w/ spaces are handled correctly in loops below
+      IFS=
+
+      # for releases, add core hadoop jar & webapps to CLASSPATH
+      if [ -d "$HADOOP_HOME/webapps" ]; then
+        CLASSPATH=${CLASSPATH}:$HADOOP_HOME
+      fi
+      for f in $HADOOP_HOME/hadoop-*-core.jar; do
+        CLASSPATH=${CLASSPATH}:$f;
+      done
+
+      # add libs to CLASSPATH
+      for f in $HADOOP_HOME/lib/*.jar; do
+        CLASSPATH=${CLASSPATH}:$f;
+      done
+
+      for f in $HADOOP_HOME/lib/jetty-ext/*.jar; do
+        CLASSPATH=${CLASSPATH}:$f;
+      done
+
+      # add user-specified CLASSPATH last
+      if [ "$HADOOP_CLASSPATH" != "" ]; then
+        CLASSPATH=${CLASSPATH}:${HADOOP_CLASSPATH}
+      fi
+
+      export MASTERFILE=$HADOOP_CONF_DIR/masters
+      export SLAVEFILE=$HADOOP_CONF_DIR/slaves
+
+      if [ "$command" = "bfsdatanode" ]; then
+        BFS_DATA_DIR=/tmp/bfs_data
+        # XXX: for now, we just remove the old data directory on startup
+        echo "Removing data directory $BFS_DATA_DIR"
+        rm -r $BFS_DATA_DIR
+        nohup $JAVA -cp "$CLASSPATH" bfs.DataNode $BFS_DATA_DIR > "$log" 2>&1 < /dev/null &
+      else
+        nohup $JAVA -cp "$CLASSPATH" bfs.Master > "$log" 2>&1 < /dev/null &
+      fi
+    else
+      nohup nice -n $HADOOP_NICENESS "$HADOOP_HOME"/bin/hadoop --config $HADOOP_CONF_DIR $command "$@" > "$log" 2>&1 < /dev/null &
     fi
-    nohup nice -n $HADOOP_NICENESS "$HADOOP_HOME"/bin/hadoop --config $HADOOP_CONF_DIR $command "$@" > "$log" 2>&1 < /dev/null &
     echo $! > $pid
     sleep 1; head "$log"
     ;;
-          
+
   (stop)
 
     if [ -f $pid ]; then
