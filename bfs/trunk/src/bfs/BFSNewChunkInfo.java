663b0774ce9c3893e0126f755bacccd1d9cb162b
Neil Conway <neil.conway@gmail.com>
2009-03-01 06:01:38 +0000

    bfs: Return at most Conf.getRepFactor() candidate data nodes in a NewChunk response, chosen at random from the set of candidates. We do this in Java, because it is apparently too expensive to do in Overlog.
    Former-commit-id: 07ea14735731949febf78e678fa1280c2951fdf0

--- bfs/trunk/src/bfs/BFSNewChunkInfo.java
+++ bfs/trunk/src/bfs/BFSNewChunkInfo.java
@@ -1,18 +1,22 @@
 package bfs;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class BFSNewChunkInfo implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	private static final Random random = new Random();
 
 	private final int chunkId;
 	private final List<String> nodeAddrs;
 
 	public BFSNewChunkInfo(int chunkId, List<String> nodeList) {
 		this.chunkId = chunkId;
-		this.nodeAddrs = Collections.unmodifiableList(nodeList);
+		this.nodeAddrs = Collections.unmodifiableList(BFSNewChunkInfo.randomSelection(nodeList, Conf.getRepFactor()));
 	}
 
 	public int getChunkId() {
@@ -26,5 +30,21 @@ public class BFSNewChunkInfo implements Serializable {
 	@Override
 	public String toString() {
 		return "BFSNewChunkInfo[#" + chunkId + "; " + nodeAddrs + "]";
+	}
+
+	private static List<String> randomSelection(List<String> inList, int k) {
+		if (inList.size() <= k)
+			return inList;
+
+		List<String> result = new ArrayList<String>(k);
+		// java.util.Random#nextInt() might not be thread-safe, so be paranoid
+		synchronized (random) {
+			while (result.size() < k) {
+				String candidate = inList.get(random.nextInt(inList.size()));
+				if (!result.contains(candidate))
+					result.add(candidate);
+			}
+		}
+		return result;
 	}
 }
