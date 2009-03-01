e888637b0fecbaa383d6d922185c19c177279b92
Neil Conway <neil.conway@gmail.com>
2009-03-01 01:57:05 +0000

    hadoop: Script tweaks.

--- hadoop/exec/stop-bfs-datanode.sh
+++ hadoop/exec/stop-bfs-datanode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Stop the BFS data node running on the local host.
+# Stop the BFS data node running on all the addresses in the "masters" file.
 
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
@@ -8,10 +8,6 @@ bin=`cd "$bin"; pwd`
 
 if [ -f "${HADOOP_CONF_DIR}/hadoop-env.sh" ]; then
   . "${HADOOP_CONF_DIR}/hadoop-env.sh"
-fi
-
-if [ "$HADOOP_PID_DIR" = "" ]; then
-  HADOOP_PID_DIR=/tmp
 fi
 
 "$bin"/hadoop-daemons.sh --config $HADOOP_CONF_DIR stop bfsdatanode
