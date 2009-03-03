f515daa84108e4139eb343e5b50201dba9dbce06
Neil Conway <neil.conway@gmail.com>
2009-03-03 21:28:12 +0000

    jol: Remove unused import.
    Former-commit-id: c2b661e64072add7d8075c367931fee26a9cb150

--- lincoln/java/trunk/src/jol/net/NetworkBuffer.java
+++ lincoln/java/trunk/src/jol/net/NetworkBuffer.java
@@ -1,7 +1,6 @@
 package jol.net;
 
 import jol.types.basic.Tuple;
-import jol.types.basic.BasicTupleSet;
 import jol.types.basic.TupleSet;
 import jol.types.exception.UpdateException;
 import jol.types.table.Key;
@@ -10,9 +9,9 @@ import jol.types.table.TableName;
 import jol.core.Runtime;
 
 /**
- * The network buffer holds all messages that are 
+ * The network buffer holds all messages that are
  * sent via the network.
- * 
+ *
  * <p>
  * This buffer table holds all messages regardless of
  * protocol. This allows protocol implementation to layer
@@ -21,34 +20,33 @@ import jol.core.Runtime;
  * be the network layer, implemented by {@link Network} manager.
  */
 public class NetworkBuffer extends ObjectTable {
-	
+
 	public static final Key PRIMARY_KEY = new Key(2,3);
-	
+
 	public enum Field{PROTOCOL, DIRECTION, ADDRESS, MESSAGE};
 	public static final Class[] SCHEMA = {
 		String.class,   // Protocol
 		String.class,   // Direction
-		Address.class,  // Destination address 
+		Address.class,  // Destination address
 		Message.class   // Message
 	};
-	
+
 	public NetworkBuffer(Runtime context) {
 		super(context, new TableName("network", "buffer"), PRIMARY_KEY, SCHEMA);
 	}
-	
+
 	@Override
 	public TupleSet insert(TupleSet tuples, TupleSet conflicts) throws UpdateException {
 		return super.insert(tuples, conflicts);
 	}
-	
+
 	@Override
 	public boolean insert(Tuple tuple) throws UpdateException {
 		return super.insert(tuple);
 	}
-	
+
 	@Override
 	public boolean delete(Tuple tuple) throws UpdateException {
 		return super.delete(tuple);
 	}
-
 }
