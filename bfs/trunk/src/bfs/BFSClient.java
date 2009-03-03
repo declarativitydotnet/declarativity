14856d8cffcff595a391be7875bdd5c6b85b0bde
Neil Conway <neil.conway@gmail.com>
2009-03-03 19:02:59 +0000

    bfs: Remove installation of chunks_global.olg from BFSClient -- not needed, and inconsistent with Shell.
    Former-commit-id: 9aba0bfd957cb432ad0c5becaf1f99d0fe5c673c

--- bfs/trunk/src/bfs/BFSClient.java
+++ bfs/trunk/src/bfs/BFSClient.java
@@ -38,8 +38,6 @@ public class BFSClient {
 
 	        this.system.install("bfs_global", ClassLoader.getSystemResource("bfs/bfs_global.olg"));
 	        this.system.evaluate();
-	        this.system.install("bfs", ClassLoader.getSystemResource("bfs/chunks_global.olg"));
-	        this.system.evaluate();
             this.system.install("bfs_global", ClassLoader.getSystemResource("bfs/heartbeats.olg"));
             this.system.evaluate();
             this.system.install("bfs_global", ClassLoader.getSystemResource("bfs/chunks.olg"));
