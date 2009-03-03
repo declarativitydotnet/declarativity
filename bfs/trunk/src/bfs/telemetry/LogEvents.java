f56a8f5edb7de673decaa4e5113d383481a9b3fc
Russell Sears <sears>
2009-03-03 09:39:30 +0000

    Add code to synchronously detect new events; still need to get it to callback into JOL.
    Former-commit-id: fc2d5e7df5b595e83d764a3abdec434979de1af2

---
+++ bfs/trunk/src/bfs/telemetry/LogEvents.java
@@ -0,0 +1,79 @@
+package bfs.telemetry;
+
+import java.io.BufferedReader;
+import java.io.CharArrayReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class LogEvents {
+	private final File name;
+	private class LogThread extends Thread {
+		public boolean done = false;
+		@Override
+        public void run() { 
+			
+			int oldlen = 0;
+			int lastNewline = -1;
+			try {
+				while(!done) {
+					try{
+						int curlen = (int)name.length();
+						if(curlen > oldlen) {
+							System.out.println("curlen == " + curlen);
+							char[] buf = new char[(int)curlen - (lastNewline+1)];
+							FileReader in = new FileReader(name);
+							in.skip(lastNewline+1);
+							in.read(buf, 0, buf.length);
+							in.close();
+	
+							int i = buf.length-1;
+							while(i >= 0 && buf[i] != '\n') { i--; }
+							if(i >= 0 && buf[i] == '\n') lastNewline = lastNewline + 1 + i;
+	
+							if(i >= 0) {
+								BufferedReader r = new BufferedReader(new CharArrayReader(buf, 0, i));
+								String line;
+								while(null != (line = r.readLine())) {
+									System.out.println("Found a line!! " + line + "<---");
+								}
+							}
+							oldlen = curlen;
+						} else if(curlen < oldlen) {
+							// file was truncated?!?
+							oldlen = 0;
+							lastNewline = -1;
+						} else {
+							Thread.sleep(250);
+						}
+					}catch (InterruptedException e) {
+						// ignore.
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		public LogThread(File f) {
+			super("Log monitor for " + f);
+		}
+	}
+	private final LogThread logThread;
+
+	final boolean done = false;
+	public LogEvents(File name) {
+		this.name = name;
+		logThread = new LogThread(name);
+		logThread.run();
+	}
+	public void shutdown() {
+		logThread.done = true;
+	}
+	public static void main(String [] args) { 
+		LogEvents e = new LogEvents(new File(args[0]));
+		while (e != null) {
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException ex) {}
+		}
+	}
+}
