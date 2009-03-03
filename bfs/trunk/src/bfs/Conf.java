92adb8bc5b2880d1b38171a074998f8c15c3b359
Neil Conway <neil.conway@gmail.com>
2009-03-03 00:27:29 +0000

    bfs: Adjust default listing timeout to 20 seconds, and set default replication factor to 3.

--- bfs/trunk/src/bfs/Conf.java
+++ bfs/trunk/src/bfs/Conf.java
@@ -43,13 +43,13 @@ public class Conf {
     };
 
     private static final long fileOpTimeout = 20000;
-    private static final long listingTimeout = 10000;
+    private static final long listingTimeout = 20000;
 
     private static final int chunkSize = 256 * 1024;
 
     private static final long heartbeatRetention = 12000L;
 
-    private static int replicationFactor = 1;
+    private static int replicationFactor = 3;
 
     private static boolean tap = false;
 
