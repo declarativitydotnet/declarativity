4d9f87d8f9142b9b258b4a953eab0dc81ac8d58f
Neil Conway <neil.conway@gmail.com>
2009-03-04 01:15:55 +0000

    jol: Whitespace cleanup.
    Former-commit-id: 9f9f90f17f5e406219c967f485add6244cdf4a42

--- lincoln/java/trunk/src/jol/net/Message.java
+++ lincoln/java/trunk/src/jol/net/Message.java
@@ -7,20 +7,20 @@ import java.io.Serializable;
  */
 public abstract class Message implements Comparable<Message>, Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
-	
+
 	private String layer;
-	
+
 	public Message() {
 		this.id = 0L;
 	}
-	
+
 	public Message(Long id, String layer) {
 		this.id = id;
 		this.layer = layer;
 	}
-	
+
 	@Override
 	public String toString() {
 		return layer + ":" + id.toString();
@@ -29,13 +29,12 @@ public abstract class Message implements Comparable<Message>, Serializable {
 	public int compareTo(Message o) {
 		return toString().compareTo(o.toString());
 	}
-	
+
 	public Long id() {
 		return this.id;
 	}
-	
+
 	public String layer() {
 		return this.layer;
 	}
-
 }
