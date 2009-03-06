527a1ddbcd5a48a3105e92a4cc6d81240a36b07f
Neil Conway <neil.conway@gmail.com>
2009-03-06 00:08:26 +0000

    jol: Code cleanup. Make some fields final.

--- lincoln/java/trunk/src/jol/net/IP.java
+++ lincoln/java/trunk/src/jol/net/IP.java
@@ -4,21 +4,18 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class IP extends Address {
-	
-	private InetAddress address;
-	
-	private Integer port;
-	
+	private final InetAddress address;
+	private final Integer port;
+
 	public IP(String address) {
 		try {
 			this.address = InetAddress.getByName(address.substring(0, address.indexOf(':')));
 			this.port    = Integer.parseInt(address.substring(address.indexOf(':') + 1));
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public IP(InetAddress address, Integer port) {
 		this.address = address;
 		this.port = port;
@@ -28,11 +25,11 @@ public class IP extends Address {
 	public String toString() {
 		return address.toString() + ":" + port;
 	}
-	
+
 	public InetAddress address() {
 		return this.address;
 	}
-	
+
 	public Integer port() {
 		return this.port;
 	}
