bf70ef858bd928c324ecbc8250ce261d35bb3918
Russell Sears <sears>
2009-03-27 19:20:56 +0000

    oops; created a trunk/ under the branch...
    Former-commit-id: 9d22c7d2913db0c7c18e1d31c52f447d5058c4fa

--- lincoln/java/branches/stable-sp09/trunk/src/jol/types/exception/PlannerException.java
+++
@@ -1,9 +0,0 @@
-package jol.types.exception;
-
-public class PlannerException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public PlannerException(String error) {
-		super(error);
-	}
-}
