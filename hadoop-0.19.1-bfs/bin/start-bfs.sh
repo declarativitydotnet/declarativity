eb7a0d9c4326e4594109f766b971590a9b560305
Neil Conway <neil.conway@gmail.com>
2009-04-10 21:00:14 +0000

    Add BFS scripts.
    Former-commit-id: c25cbdab226281ec90f4a634c77db6e6c178a18f

---
+++ hadoop-0.19.1-bfs/bin/start-bfs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Start a BFS master node on all the addresses in the "masters" file.
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+. "$bin"/hadoop-config.sh
+
+if [ -f "${HADOOP_CONF_DIR}/hadoop-env.sh" ]; then
+  . "${HADOOP_CONF_DIR}/hadoop-env.sh"
+fi
+
+"$bin"/hadoop-masters.sh --config $HADOOP_CONF_DIR start bfsmaster
+
