751ac01a571847d234ea577a096640bbec4923bf
Neil Conway <neil.conway@gmail.com>
2009-04-21 23:18:03 +0000

    jol: Remove needless "TODO" comments; current implementation is fine for what BKE currently does.
    Former-commit-id: 24c9a97e4c13bdb2c6ce1dcd72d7cb684e20c952

--- lincoln/java/trunk/src/jol/types/exception/BadKeyException.java
+++ lincoln/java/trunk/src/jol/types/exception/BadKeyException.java
@@ -9,16 +9,13 @@ public class BadKeyException extends Exception {
 
 	public BadKeyException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public BadKeyException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public BadKeyException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 }
