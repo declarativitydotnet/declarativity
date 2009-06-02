529a29a17f4886135234c2426ef6a88625cc6362
Tyson Condie <tcondie>
2009-06-02 18:46:17 +0000

    update
    Former-commit-id: 8f9f8d5ea6484990e5ace20ad96929814e1d4b4b

--- hadoop/src/mapred/org/apache/hadoop/mapred/declarative/master/JobTrackerImpl.java
+++ hadoop/src/mapred/org/apache/hadoop/mapred/declarative/master/JobTrackerImpl.java
@@ -194,7 +194,7 @@ public class JobTrackerImpl extends JobTracker {
 		this.context().catalog().register(new TaskTable(context));
 		this.context().catalog().register(new TaskTrackerActionTable(context));
 		this.context().catalog().register(new TaskTrackerErrorTable(context));
-		this.context().catalog().register(new TaskTrackerTable(context));
+		this.context().catalog().register(new TaskTrackerTable(context, this));
 		this.context().catalog().register(new NetworkTopologyTable(context, this));
 		
 		for (String program : programs) {
