f50f64b54d8035f0d8b16873bcb9f0d5172448a2
Neil Conway <neil.conway@gmail.com>
2009-08-16 00:06:41 +0000

    Add comment.
    Former-commit-id: b188e37d3c82bd8f16d66a02619a47e756a9799e

--- hadoop/src/core/org/apache/hadoop/bufmanager/ReduceOutputCollector.java
+++ hadoop/src/core/org/apache/hadoop/bufmanager/ReduceOutputCollector.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import org.apache.hadoop.io.DataInputBuffer;
 import org.apache.hadoop.mapred.OutputCollector;
 
+/*
+ * This is an optimized version of the OutputCollector interface used for reduce
+ * output. The difference is that the arguments to collect are still in their
+ * marshalled representation.
+ */
 public interface ReduceOutputCollector<K extends Object, V extends Object>
 		extends OutputCollector<K, V> {
-	
-	  void collect(DataInputBuffer key, DataInputBuffer value) throws IOException;
+	void collect(DataInputBuffer key, DataInputBuffer value) throws IOException;
 }
