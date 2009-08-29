f980670c5822062ff4a991b3cbdff584606b3083
Tyson Condie <tcondie>
2009-08-29 11:40:55 +0000

    update

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/BufferController.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/BufferController.java
@@ -188,6 +188,7 @@ public class BufferController extends Thread implements BufferUmbilicalProtocol 
 	
 	private void register(BufferRequest request) throws IOException {
 		synchronized(this) {
+			System.err.println("Register " + request);
 			if (!this.requests.containsKey(request.taskid())) {
 				this.requests.put(request.taskid(), new TreeSet<BufferRequest>());
 			}
