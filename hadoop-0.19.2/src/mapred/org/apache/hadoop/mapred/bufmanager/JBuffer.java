7f3154f0281b1c1607e21717473ed2c2cbccac77
Tyson Condie <tcondie>
2009-09-01 00:40:36 +0000

    update

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/JBuffer.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/JBuffer.java
@@ -1103,6 +1103,7 @@ public class JBuffer<K extends Object, V extends Object>  implements ReduceOutpu
 
 			numFlush = numSpills;
 			numSpills++;
+		}
 
 			long finalOutFileSize = 0;
 			long finalIndexFileSize = 0;
@@ -1219,7 +1220,6 @@ public class JBuffer<K extends Object, V extends Object>  implements ReduceOutpu
 					localFs.delete(indexFileName[i], true);
 				}
 			}
-		}
 	}
 
 	private void writeIndexRecord(FSDataOutputStream indexOut, 
