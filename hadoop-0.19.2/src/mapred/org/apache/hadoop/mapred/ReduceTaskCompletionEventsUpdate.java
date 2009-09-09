4251ee808e123a0815ab7653bc15e51f47f1b741
Tyson Condie <tcondie>
2009-09-09 00:57:20 +0000

    update

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/ReduceTaskCompletionEventsUpdate.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/ReduceTaskCompletionEventsUpdate.java
@@ -1,9 +1,23 @@
 package org.apache.hadoop.mapred;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 class ReduceTaskCompletionEventsUpdate extends MapTaskCompletionEventsUpdate {
+
+	public ReduceTaskCompletionEventsUpdate() {};
 
 	public ReduceTaskCompletionEventsUpdate(TaskCompletionEvent[] reduceEvents, boolean b) {
 		super(reduceEvents, b);
 	}
 
+	public void write(DataOutput out) throws IOException {
+		super.write(out);
+	}
+
+	public void readFields(DataInput in) throws IOException {
+		super.readFields(in);
+
+	}
 }
