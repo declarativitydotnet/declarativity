ad4761ecebe568fce8a5f346d19a053efdf2ef2e
Tyson Condie <tcondie>
2009-09-09 22:41:57 +0000

    update
    Former-commit-id: cee70c72bd7a7aa6b0d8a27c6a67f4a489e53ea1

---
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/PipelineReduceTask.java
@@ -0,0 +1,72 @@
+package org.apache.hadoop.mapred;
+
+import java.io.IOException;
+
+import org.apache.hadoop.mapred.bufmanager.BufferUmbilicalProtocol;
+import org.apache.hadoop.mapred.bufmanager.JBuffer;
+import org.apache.hadoop.mapred.bufmanager.ValuesIterator;
+import org.apache.hadoop.util.ReflectionUtils;
+
+public class PipelineReduceTask extends ReduceTask {
+	
+	private BufferUmbilicalProtocol bufferUmbilical;
+	
+	public PipelineReduceTask() {
+		super();
+	}
+
+	public PipelineReduceTask(String jobFile, TaskAttemptID taskId,
+			int partition, int numMaps) {
+		super(jobFile, taskId, partition, numMaps);
+	}
+	
+	public boolean isPipeline() {
+		return true;
+	}
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public void run(JobConf job, final TaskUmbilicalProtocol umbilical, final BufferUmbilicalProtocol bufferUmbilical)
+	throws IOException {
+		this.bufferUmbilical = bufferUmbilical;
+		super.run(job, umbilical, bufferUmbilical);
+	}
+	
+	@Override
+	public void reduce(JobConf job, final Reporter reporter, JBuffer buffer) throws IOException {
+		setPhase(TaskStatus.Phase.REDUCE); 
+		
+		JBuffer output = new JBuffer(bufferUmbilical, getTaskID(), job, reporter);
+
+		Reducer reducer = (Reducer)ReflectionUtils.newInstance(job.getReducerClass(), job);
+		// apply reduce function
+		try {
+		      Class keyClass = job.getMapOutputKeyClass();
+		      Class valClass = job.getMapOutputValueClass();
+		      
+		      buffer.flush();
+		      ValuesIterator values = buffer.iterator();
+		      while (values.more()) {
+		        reducer.reduce(values.getKey(), values, output, reporter);
+		        values.nextKey();
+		        reporter.progress();
+		      }
+		      
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			throw ioe;
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		finally {
+			//Clean up: repeated in catch block below
+			try {
+				reducer.close();
+				buffer.close();
+			} catch (IOException ignored) {}
+		}
+		
+		bufferUmbilical.commit(getTaskID());
+	}
+	
+}
