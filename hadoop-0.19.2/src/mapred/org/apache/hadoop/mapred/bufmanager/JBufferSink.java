4c5ce353d69f26f7a2ed2c754bb6b9bac9c4e70f
Tyson Condie <tcondie>
2009-09-16 01:01:24 +0000

    update

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/JBufferSink.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/JBufferSink.java
@@ -328,7 +328,6 @@ public class JBufferSink<K extends Object, V extends Object> {
 				} catch (IOException e) { }
 			}
 		};
-		acceptor.setPriority(Thread.MAX_PRIORITY);
 		acceptor.start();
 	}
 	
