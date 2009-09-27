322aac0395d1a4d4d42fc9f13ba37f855b012493
Tyson Condie <tcondie>
2009-09-27 18:39:19 +0000

    update

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/BufferRequest.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/BufferRequest.java
@@ -313,7 +313,7 @@ public class BufferRequest<K extends Object, V extends Object> implements Compar
 		}
 	}
 	
-	public IFile.Writer<K, V> force(DataInputBuffer key, DataInputBuffer value, IFile.Writer<K, V> writer) throws IOException {
+	public IFile.Writer<K, V> force(DataInputBuffer key, DataInputBuffer value, IFile.Writer<K, V> writer, long length) throws IOException {
 		synchronized (this) {
 			if (!isOpen()) throw new IOException("BufferRequest is closed!");
 
@@ -328,7 +328,7 @@ public class BufferRequest<K extends Object, V extends Object> implements Compar
 				Class <K> keyClass = (Class<K>)conf.getMapOutputKeyClass();
 				Class <V> valClass = (Class<V>)conf.getMapOutputValueClass();
 				
-				out.writeLong(1);
+				out.writeLong(length);
 				out.writeFloat(0.1f);
 				writer = new IFile.Writer<K, V>(conf, out,  keyClass, valClass, codec);
 			}
