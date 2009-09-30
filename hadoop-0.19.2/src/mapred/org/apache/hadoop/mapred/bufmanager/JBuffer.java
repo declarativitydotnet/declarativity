21bf3c0bd311e33428fb1c7636b01bb82787701e
Tyson Condie <tcondie>
2009-09-30 21:01:31 +0000

    update

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/JBuffer.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/bufmanager/JBuffer.java
@@ -519,6 +519,7 @@ public class JBuffer<K extends Object, V extends Object>  implements JBufferColl
 		
 		this.mergeThread = new MergeThread();
 		this.mergeThread.setDaemon(true);
+		this.mergeThread.setPriority(Thread.MAX_PRIORITY);
 		if (!taskid.isMap()) {
 			this.mergeThread.start();
 		}
