823e19a71fd3d0dfa1fc1a63b099bb06ff6fa2b1
Tyson Condie <tcondie>
2009-10-17 21:09:48 +0000

    update
    Former-commit-id: af7238ede369b96a831cd22497618f0d93269d9c

--- hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/ReduceTask.java
+++ hadoop-0.19.2/src/mapred/org/apache/hadoop/mapred/ReduceTask.java
@@ -369,8 +369,9 @@ public class ReduceTask extends Task {
 		
 		long begin = System.currentTimeMillis();
 		try {
-			buffer.close();
+			OutputFile finalOutput = buffer.close();
 			buffer.setProgress(reducePhase);
+			if (reducePipeline) buffer.input(finalOutput, true);
 			reduce(job, reporter, buffer);
 		} finally {
 			reducePhase.complete();
