c287609b80d78e71b713bc7059a9f5896ee47cae
Tyson Condie <tcondie>
2010-06-23 00:03:24 +0000

    Former-commit-id: 21ce447015c2d95dd08263b7abd913e8e1b351a0

--- thesis/materials/docs/sigmod2006/compile.sh
+++
@@ -1,7 +0,0 @@
-make clean
-make pdf
-cp dnsigmod06.pdf ~/public_html/research/sigmod2006/dnsigmod06.pdf
-cp dnsigmod06.pdf 114.pdf
-cp dnsigmod06.pdf ~/public_html/research/sigmod2006/114.pdf
-cp *.tex ~/public_html/research/sigmod2006/tex/.
-cp *.bib ~/public_html/research/sigmod2006/tex/.
