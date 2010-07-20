c29f960fc39fc21ea8ae6f33e286f683440392c4
Joe Hellerstein <joe@trifacta.com>
2010-07-20 02:17:26 +0000

    BSD license
    Former-commit-id: 227ab3266b65dd618dac33471188aca422cf848a
    Former-commit-id: 955c92eb2f1d6f0674328e8bfa142f14a1935cd3

--- bloomrb/examples/basic.rb
+++ bloomrb/examples/basic.rb
@@ -1,4 +1,5 @@
-require 'bloom'
+require 'rubygems'
+require 'lib/bloom'
 
 class Basic < Bloom
 
@@ -10,15 +11,16 @@ class Basic < Bloom
   
   def declaration
     strata[0] = rules {
-      r << [1, 1]
-      r << [2, 1]
-      r << [2, 2]
-      r << [3, 1]
-      r << [3, 2]
-      s << [1, 1]
-      s << [2, 2]
-      s << [3, 1]
-      s << [3, 2]
+      r << ['a', 1]
+      r << ['a', 2]
+      r << ['b', 1]
+      r << ['b', 2]
+      r << ['c', 1]
+      r << ['c', 2]
+      s << ['a', 1]
+      s << ['b', 2]
+      s << ['c', 1]
+      s << ['c', 2]
       j = join(r,s)
       out <= j.map do |t1,t2|
         [t1.x, t1.y, t2.y] if t1.x == t2.x
@@ -29,5 +31,6 @@ end
 
 program = Basic.new('localhost', 12345)
 program.tick
+puts 'we should see 1 a, 2 b\'s and 4 c\'s'
 o = (program.out.map {|o| o}).sort
 o.map {|o| puts o.inspect}
