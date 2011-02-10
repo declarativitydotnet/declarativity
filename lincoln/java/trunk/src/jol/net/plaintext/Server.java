9bce1a6c77e587d99e6e9441cf674a9c73858603
Russell Sears <sears>
2011-02-10 19:02:03 +0000

    change plaintext server to read from stdin; improve error messages
    Former-commit-id: 4a14d2badd495f77a3745211909974a94fb7e1da

--- lincoln/java/trunk/src/jol/net/plaintext/Server.java
+++ lincoln/java/trunk/src/jol/net/plaintext/Server.java
@@ -4,14 +4,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jol.core.Runtime;
 import jol.types.basic.BasicTupleSet;
 import jol.types.basic.Tuple;
 import jol.types.basic.TupleSet;
+import jol.types.table.Table;
 import jol.types.table.TableName;
 
 public class Server {
@@ -34,64 +36,86 @@ public class Server {
 			return;
 		}
 		
-		while(true) {
+//		while(true) {
 			try {
-				Socket ss = sock.accept();
-				BufferedReader ins = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+				long num_processed = 0;
+//				Socket ss = sock.accept();
+//				BufferedReader ins = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+				BufferedReader ins = new BufferedReader(new InputStreamReader(System.in));
 				String in;
-
+				Set<String> unknownTables = new HashSet<String>();
 				while(null != (in = ins.readLine())) {
-					// TODO Call the jol parser, instead of using this crazy hack?
-					String[] tok = in.split("\\s*[()]\\s*");
-					String[] tok2 = tok[1].split("\\s*[,]\\s*");
-					
-					TableName name = new TableName("trace", tok[0]);
-					@SuppressWarnings("rawtypes")
-					Class[] c = j.catalog().table(name).types();
-					List<Object> l = new ArrayList<Object>();
-					
-					for (int i = 0; i < tok2.length; i++) {
-						if(c[i] == String.class) {
-							// XXX what about strings that contain ','?
-							l.add(tok2[i]);
-						} else if(c[i] == Integer.class) {
-							l.add(Integer.parseInt(tok2[i]));
-						} else if(c[i] == Long.class) {
-							l.add(Long.parseLong(tok2[i]));
-						} else if(c[i] == Short.class) {
-							l.add(Short.parseShort(tok2[i]));
-						} else if(c[i] == Boolean.class) {
-							l.add(Boolean.parseBoolean(tok2[i]));
-						} else if(c[i] == Character.class) {
-							if(tok2[i].length() == 1) {
-								l.add(tok2[i].charAt(0));
-							} else {
-								throw new UnsupportedOperationException("Could not convert '" + tok2[i] + "' to a char, as it is more than one character long");
-							}
-						} else if(c[i] == Byte.class) {
-							if(tok2[i].length() == 1) {
-								l.add((byte)tok2[i].charAt(0));
-							} else {
-								// XXX what about UTF16 chars greater than 255?
-								throw new UnsupportedOperationException("Could not convert '" + tok2[i] + "' to a byte, as it is more than one character long");
-							}
-						} else if(c[i] == Float.class) {
-							l.add(Float.parseFloat(tok2[i]));
-						} else if(c[i] == Double.class) {
-							l.add(Double.parseDouble(tok2[i]));
+					num_processed ++;
+//					if(num_processed % 100 == 0) {
+						System.err.print("                                                                             \rLine " + num_processed + ": " + in + " ");
+//					}
+					try {
+//						System.out.println(in);
+						// TODO Call the jol parser, instead of using this crazy hack?
+						String[] tok = in.split("\\s*[()]\\s*");
+						String[] tok2;
+						if(tok.length > 1) {
+							tok2 = tok[1].split("\\s*[,]\\s*");
 						} else {
-							throw new UnsupportedOperationException("Unknown column type encountered in schema.  I only understand the JOL basic types (String, Integer, Long, Short, Boolean, Character, Byte, Float, and Double).  Also, String and Byte support is buggy.");
+							tok2 = new String[0];
 						}
+						
+						TableName name = new TableName("trace", tok[0]);
+						Table t = j.catalog().table(name);
+						if(null != t) {
+							@SuppressWarnings("rawtypes")
+							Class[] c = t.types();
+							List<Object> l = new ArrayList<Object>();
+							
+							for (int i = 0; i < tok2.length; i++) {
+								if(c[i] == String.class) {
+									// XXX what about strings that contain ','?
+									l.add(tok2[i]);
+								} else if(c[i] == Integer.class) {
+									l.add(Integer.parseInt(tok2[i]));
+								} else if(c[i] == Long.class) {
+									l.add(Long.parseLong(tok2[i]));
+								} else if(c[i] == Short.class) {
+									l.add(Short.parseShort(tok2[i]));
+								} else if(c[i] == Boolean.class) {
+									l.add(Boolean.parseBoolean(tok2[i]));
+								} else if(c[i] == Character.class) {
+									if(tok2[i].length() == 1) {
+										l.add(tok2[i].charAt(0));
+									} else {
+										throw new UnsupportedOperationException("Could not convert '" + tok2[i] + "' to a char, as it is more than one character long");
+									}
+								} else if(c[i] == Byte.class) {
+									if(tok2[i].length() == 1) {
+										l.add((byte)tok2[i].charAt(0));
+									} else {
+										// XXX what about UTF16 chars greater than 255?
+										throw new UnsupportedOperationException("Could not convert '" + tok2[i] + "' to a byte, as it is more than one character long");
+									}
+								} else if(c[i] == Float.class) {
+									l.add(Float.parseFloat(tok2[i]));
+								} else if(c[i] == Double.class) {
+									l.add(Double.parseDouble(tok2[i]));
+								} else {
+									throw new UnsupportedOperationException("Unknown column type encountered in schema.  I only understand the JOL basic types (String, Integer, Long, Short, Boolean, Character, Byte, Float, and Double).  Also, String and Byte support is buggy.");
+								}
+							}
+							Tuple tup = new Tuple(l);
+							TupleSet insertions = new BasicTupleSet(tup);
+							
+							j.schedule("trace", name, insertions, null);
+							j.evaluate();
+						} else {
+				//			if(unknownTables.add(tok[0])) { System.err.println("\nWARNING: Unknown table.  Tuple was " + in); }
+						}
+					} catch (Exception e) {
+						System.err.println("\nERROR Can't process tuple ->" + in + "<-");
+						throw e;
 					}
-					Tuple tup = new Tuple(l);
-					TupleSet insertions = new BasicTupleSet(tup);
-					
-					j.schedule("trace", name, insertions, null);
-					j.evaluate();
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
+//		}
 	}
 }
