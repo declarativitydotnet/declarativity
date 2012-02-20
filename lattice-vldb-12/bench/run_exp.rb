974bf3ee84d6747639b30e87fbcc85c881b27b27
Neil Conway <neil.conway@gmail.com>
2012-02-20 22:07:11 +0000

    Rework benchmark script to spawn a new process per program variant.
    GC was interferring with the reproducibility of the results.
    
    
    
    
    Former-commit-id: 5442929b93d891b85628b7ba9523edc45247fa1a

--- lattice-vldb-12/bench/run_exp.rb
+++ lattice-vldb-12/bench/run_exp.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 nruns = 4
-sizes = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
-data_file = "perf.data"
+#sizes = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+sizes = [10,12,14,16,18]
+variants = ["bloom", "seminaive-lat", "naive-lat"]
+data_files = {}
+variants.each {|v| data_files[v] = "#{v}.data"}
 log_file = "exp_log"
 
 `rm -f #{log_file}`
-`echo "#NNodes NLinks NPaths Bloom LatticeSemi LatticeNaive" | cat > #{data_file}`
+data_files.each {|v,f| `echo "#Variant: #{v}\n#NNodes NLinks NPaths Runtime" | cat > #{f}`}
 
 sizes.each do |s|
   puts "Running benchmark; size = #{s}..."
-  `ruby bench.rb #{s} #{nruns} >> #{log_file} 2>>#{data_file}`
+  variants.each do |v|
+    `ruby bench.rb #{s} #{nruns} #{v} >> #{log_file} 2>>#{data_files[v]}`
+  end
 end
