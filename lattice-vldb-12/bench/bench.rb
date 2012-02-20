ddd5abf290373736374cb0754537bd69fd7f1731
Neil Conway <neil.conway@gmail.com>
2012-02-20 23:16:06 +0000

    Include stddev and standard error of the mean in experiment results.
    Former-commit-id: 144b35b82eed3fefa36ebf116d40d4ebeb079699
    Former-commit-id: 6dcafbb0012d71fff65f7cc1bca4467bcc4954dd

--- lattice-vldb-12/bench/bench.rb
+++ lattice-vldb-12/bench/bench.rb
@@ -56,13 +56,13 @@ end
 def lattice_bench(data, nruns, use_naive=false)
   l = AllPathsL.new(:disable_lattice_semi_naive => use_naive)
   l.link <+ [data]
-  total_time = 0.0
+  times = []
   npaths = nil
   nruns.times do |i|
     t = Benchmark.realtime do
       l.tick
     end
-    total_time += t
+    times << t
     new_npaths = l.path.current_value.reveal.length
     puts "#{use_naive ? "Naive" : "SN"} lattice done #{i+1}/#{nruns}; #{t} seconds. # of paths: #{new_npaths} (RSS size: #{report_mem})"
     if npaths != nil && npaths != new_npaths
@@ -71,32 +71,52 @@ def lattice_bench(data, nruns, use_naive=false)
     npaths = new_npaths
   end
   l.stop
-  [total_time / nruns, npaths]
+  [times, npaths]
 end
 
 def bloom_bench(data, nruns)
   b = AllPathsB.new
   b.link <+ data
-  total_time = 0.0
+  times = []
   npaths = nil
   nruns.times do |i|
     t = Benchmark.realtime do
       b.tick
     end
-    total_time += t
+    times << t
     puts "Bloom done #{i+1}/#{nruns}; #{t} seconds. # of paths: #{b.path.to_a.length} (RSS size: #{report_mem})"
     if npaths != nil && npaths != b.path.to_a.length
       raise "Non-deterministic results: old = #{npaths}, new = #{b.path.to_a.length}"
     end
     npaths = b.path.to_a.length
   end
-  
+
   b.stop
-  [total_time / nruns, npaths]
+  [times, npaths]
 end
 
 def report_mem
   `ps -o rss= -p #{Process.pid}`.to_i
+end
+
+module Enumerable
+  def sum
+    self.inject(0){|accum, i| accum + i }
+  end
+
+  def mean
+    self.sum/self.length.to_f
+  end
+
+  def sample_variance
+    m = self.mean
+    sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+    sum/(self.length - 1).to_f
+  end
+
+  def standard_deviation
+    return Math.sqrt(self.sample_variance)
+  end
 end
 
 def bench(size, nruns, variant)
@@ -107,19 +127,28 @@ def bench(size, nruns, variant)
 
   case variant
   when "bloom"
-    t, npaths = bloom_bench(data, nruns)
+    times, npaths = bloom_bench(data, nruns)
   when "seminaive-lat"
-    t, npaths = lattice_bench(data, nruns)
+    times, npaths = lattice_bench(data, nruns)
   when "naive-lat"
     # Don't try to use naive evaluation for large graphs
-    t, npaths = lattice_bench(data, nruns, true) if size <= 18
+    times, npaths = lattice_bench(data, nruns, true) if size <= 18
   else
     raise "Unrecognized variant: #{variant}"
   end
 
-  unless t.nil?
-    puts "Results: avg #{variant} = #{t}"
-    $stderr.printf("%d %d %d %.6f\n", size, data.length, npaths, t)
+  if times
+    raise unless times.length == nruns
+    mean_t = times.mean
+    if times.length > 1
+      stddev_t = times.standard_deviation
+      std_err = stddev_t / Math.sqrt(times.length.to_f)
+    else
+      stddev_t, std_err = [0, 0]
+    end
+    printf("Results: avg %s = %.6f (std dev: %.6f)\n", variant, mean_t, stddev_t)
+    $stderr.printf("%d %d %d %.6f %0.6f %0.6f\n",
+                   size, data.length, npaths, mean_t, stddev_t, std_err)
   end
 end
 
