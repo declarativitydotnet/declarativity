75d5f8ce70f8cfdaff9eaa0faf4bddccb0695335
Neil Conway <neil.conway@gmail.com>
2012-02-24 18:52:54 +0000

    Add VC example code.
    Former-commit-id: edb22f760d397ba68f932c904d423f57e4619151

---
+++ lattice-vldb-12/code/vc.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'bud'
+
+class VectorClock
+  include Bud
+
+  state do
+    lmap :my_vc
+    lmap :next_vc
+    interface input, :in_msg, [:addr, :payload, :clock]
+    interface input, :out_msg, [:addr, :payload]
+    interface output, :out_msg_vc, [:addr, :payload, :clock]
+  end
+
+  bootstrap do
+    my_vc <= [ {ip_port => Bud::MaxLattice.new(0)} ]
+  end
+
+  bloom do
+    next_vc <= my_vc
+    next_vc <= out_msg { {ip_port => my_vc.at(ip_port) + 1} }
+    next_vc <= in_msg  { {ip_port => my_vc.at(ip_port) + 1} }
+    next_vc <= in_msg  {|m| m.clock}
+    my_vc <+ next_vc
+
+    out_msg_vc <= out_msg {|m| [m.addr, m.payload, next_vc]}
+  end
+end
