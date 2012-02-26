6f242f57a8add5c93a099643d6b925f3a120ce55
Neil Conway <neil.conway@gmail.com>
2012-02-26 21:13:52 +0000

    Tweaks.
    Former-commit-id: 8d6f3217ba721d3e4a72b0661a2fa1c2a96567b7

--- lattice-vldb-12/bench/run_exp.rb
+++ lattice-vldb-12/bench/run_exp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 nruns = 5
-sizes = [20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120]
+sizes = [24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120]
 variants = ["bloom", "seminaive-lat", "naive-lat"]
 data_files = {}
 variants.each {|v| data_files[v] = "#{v}.data"}
