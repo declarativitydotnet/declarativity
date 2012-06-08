b18773098435e10ad1a2c2a8e620b840c1765a13
Neil Conway <neil.conway@gmail.com>
2012-06-08 00:37:19 +0000

    Design notes.
    Former-commit-id: 4018562e23d4e40b61e905425a363804783f5a14
    Former-commit-id: 5c181bef7c1e7b631115edffeed9e9c550d0ec3b

--- lattice-socc-12/code/kvs/kvs.rb
+++ lattice-socc-12/code/kvs/kvs.rb
@@ -9,6 +9,30 @@ module KvsProtocol
   end
 end
 
+# Design notes:
+#
+# Inspired by Dynamo. Each key is associated with a <vector-clock, value>
+# pair. A read returns this pair.
+#
+# To issue a write, a client must supply a <vector-clock, value> pair. The
+# vector-clock represents the most recent version of this key read by the
+# client, if any (if the client is doing a "blind write", they can use the empty
+# clock). The write is received by a replica, which _coordinates_ applying the
+# write to the rest of the system. First, the coordinator increments its own
+# position in the client-submitted vector clock. It then merges the resulting
+# <vector-clock, value> pair into its own database, according to this logic:
+# versions are preferred according to the partial order over VCs, and for
+# incomparable VCs (concurrent versions) a user-supplied merge method is
+# invoked; the resulting merged value is assigned the merged VC of the two input
+# values. Since we do conflict resolution at the server side, each replica
+# stores at most one <vector-clock, value> associated with any key (unlike in
+# Dynamo).
+#
+# Writes are propagated to between replicas via the same logic, except that a
+# replica accepting a write from another replica does not need to bump its own
+# position in the write's vector clock. Write progagation could either be done
+# actively (coordinator doesn't return "success" to the client until W replicas
+# have been written) or passively (replicas perioidically merge their databases).
 class MergeMapKvsReplica
   include Bud
   include KvsProtocol
