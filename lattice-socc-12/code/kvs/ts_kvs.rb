f7f7f9bee4d90592c5c6d678aa658ee876c6a21b
Neil Conway <neil.conway@gmail.com>
2012-06-09 06:14:47 +0000

    Add test case for lpair<lset, lset>.
    Former-commit-id: a69d4031af58cbc3403686340605f9bf321695ed
    Former-commit-id: d8f41055c19d8c669906f2b4614748b8c3783722

--- lattice-socc-12/code/kvs/ts_kvs.rb
+++ lattice-socc-12/code/kvs/ts_kvs.rb
@@ -24,6 +24,10 @@ class TestPair < MiniTest::Unit::TestCase
     Bud::MaxLattice.new(x)
   end
 
+  def set(*x)
+    Bud::SetLattice.new(x)
+  end
+
   def unwrap_pair(i, sym)
     val = i.send(sym).current_value.reveal
     [val.first.reveal, val.last.reveal]
@@ -35,5 +39,27 @@ class TestPair < MiniTest::Unit::TestCase
     i.p3 <+ PairLattice.new([max(4), max(10)])
     i.tick
     assert_equal([5, 0], unwrap_pair(i, :p1))
+  end
+
+  def test_pair_set
+    i = SimplePair.new
+    i.p2 <+ PairLattice.new([set(1, 2, 3), set(4, 5, 6)])
+    i.p3 <+ PairLattice.new([set(1, 2), set(7, 8, 9)])
+    i.tick
+    first, last = unwrap_pair(i, :p1)
+    assert_equal([1, 2, 3], first.sort)
+    assert_equal([4, 5, 6], last.sort)
+
+    i.p3 <+ PairLattice.new([set(1, 2, 3, 4), set(10)])
+    i.tick
+    first, last = unwrap_pair(i, :p1)
+    assert_equal([1, 2, 3, 4], first.sort)
+    assert_equal([10], last.sort)
+
+    i.p3 <+ PairLattice.new([set(1, 2, 3, 4, 5), set()])
+    i.tick
+    first, last = unwrap_pair(i, :p1)
+    assert_equal([1, 2, 3, 4, 5], first.sort)
+    assert_equal([], last.sort)
   end
 end
