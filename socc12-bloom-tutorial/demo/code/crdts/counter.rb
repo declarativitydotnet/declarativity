ecd02a0eaab4d86ba8325729a0bd0b765a0873e1
Peter Alvaro <palvaro>
2012-09-24 21:36:11 +0000

    more code
    Former-commit-id: f53ae36e1d975d939dddfde40e0bacff57ce8a9e
    Former-commit-id: 00a0f9ecc8ef4e6cbe4b3e74078be07a5d621376

--- socc12-bloom-tutorial/demo/code/crdts/counter.rb
+++ socc12-bloom-tutorial/demo/code/crdts/counter.rb
@@ -1,11 +1,42 @@
 require 'rubygems'
 require 'bud'
 
-module BCounter
+module CounterPreds
   state do
     interfaces :input, [:msg]
-
     channel :msg, [:@site, :content]
+  end
+end
+
+module NaiveCounterOne
+  include CounterPreds
+  state do
+    table :cnt, [:value]
+  end
+  bootstrap do
+    cnt << [0]
+  end
+  bloom do
+    cnt <+ (cnt * msg).lefts{|c| [c.value + 1]}
+    cnt <- (cnt * msg).lefts
+  end
+end
+
+module NaiveCounterTwo
+  include CounterPreds
+  state do
+    table :msgs, [:content]
+    scratch :cnt, [:cnt]
+  end
+  bloom do
+    msgs <= msg{|m| [m.content]}
+    cnt <= msgs.group([], count)
+  end
+end
+
+module BCounter
+  include CounterPreds
+  state do
     lset :msgs
     lmax :cnt
   end
@@ -25,6 +56,16 @@ module QueryPreds
   end
 end
 
+module GuardedQueryPreds
+  include QueryPreds
+  state do
+    table :qguard, query.schema
+  end
+  bloom do
+    qguard <= query
+  end
+end
+
 module QueryableBCounter
   include BCounter
   include QueryPreds
@@ -36,14 +77,34 @@ end
 
 module GuardedQueryableBCounter
   include BCounter
-  include QueryPreds
-
-  state do
-    table :qguard, query.schema
-  end
+  include GuardedQueryPreds
 
   bloom do
-    qguard <= query
     query_resp <~ qguard{|q| [q.return_add, cnt.reveal]}
+  end
+end
+
+
+module QueryableNC1
+  include NaiveCounterOne
+  include QueryPreds
+  bloom do
+    query_resp <~ (query * cnt).pairs{|q, c| [q.return_add, c.cnt]}
+  end
+end
+
+module GuardedQueryableNC1
+  include NaiveCounterOne
+  include GuardedQueryPreds
+  bloom do
+    query_resp <~ (qguard * cnt).pairs{|q, c| [q.return_add, c.cnt]}
+  end
+end
+
+module GuardedQueryableNC2
+  include NaiveCounterTwo
+  include GuardedQueryPreds
+  bloom do
+    query_resp <~ (qguard * cnt).pairs{|q, c| [q.return_add, c.value]}
   end
 end
