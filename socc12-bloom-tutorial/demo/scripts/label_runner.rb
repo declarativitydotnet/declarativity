c76c532a3d676cec91d38183a43b00812f4eed20
Peter Alvaro <palvaro>
2012-09-29 20:47:24 +0000

    shuffle libs
    Former-commit-id: 8b89b87e8544b51216cf7d239a6022ca452bc3f2
    Former-commit-id: dc7cd9bec0a815957a404a8bb7267121cffdc13c

--- socc12-bloom-tutorial/demo/scripts/label_runner.rb
+++ socc12-bloom-tutorial/demo/scripts/label_runner.rb
@@ -2,19 +2,42 @@
 LPATH = "~/code/path-labeling"
 CXT = "./code"
 
+flags = ARGV[0]
+flags = "P" if flags.nil?
 
+def table_row(cols)
+  return "<tr>" + cols.map{|c| "<td>#{c}</td>"}.join(" ")  + "</tr>"
+end
+
+def ahref(name, link)
+  "<a href=\"#{link}\">#{name}</a>"
+end
+
+def imgsrc(link)
+  "<img src=\"#{link}\" width=\"82\">"
+  #"<img src=\"#{link}\">"
+end
+
+
+index = File.open("index.html", "w")
+index.puts "<table border=1>"
 fp = File.open("scripts/mods.cfg", "r")
 while (str = fp.gets)
   file, mod = str.split(/\s+/)
   dn = File.dirname(file)
-  exe = "ruby -I . -I #{CXT} -I #{dn} -I #{LPATH} #{LPATH}/bin/label -r #{file} -i #{mod} -P"
-  #puts "EXE #{exe}"
-  #res = `#{exe}`
+  exe = "budlabel -p #{CXT} -r #{file} -i #{mod} -#{flags} -O png"
+  puts "EXE #{exe}"
   IO::popen(exe) do |io|
+    lines = []
     while (ln = io.gets)
       next if ln =~ /^Warning/
       puts "#{file}::#{mod}\t#{ln}"
+      lines << ln
     end
+    #index.puts table_row([ahref(file, file), mod, lines.join("<br>"), ahref("#{mod}.pdf", "./#{mod}.pdf"), exe])
+    index.puts table_row([ahref(file, file), mod, lines.join("<br>"), imgsrc("#{mod}.png"), exe])
   end
 end
 fp.close
+index.puts "</table>"
+index.close
